f190ce191e8634f06ff968505fbb8be731169aea
Dennis Sen <108349707+warm-coolguy@users.noreply.github.com>
2023-11-15 16:40:40 +0100

    Update packages/plugins/Gfi/src/store/actions.ts
    apply suggestion "future-proof comment"
    
    Co-authored-by: Pascal Röhling <73653210+dopenguin@users.noreply.github.com>

--- packages/plugins/Gfi/src/store/actions.ts
+++ packages/plugins/Gfi/src/store/actions.ts
@@ -173,7 +173,7 @@ const actions: PolarActionTree<GfiState, GfiGetters> = {
       // render feature geometries to help layer
       geometryLayerKeys
         .filter((key) => Array.isArray(featuresByLayerId[key]))
-        // @ts-expect-error | Might be fixed through having all the types in the action. Otherwise: It works properly, as all the symbols are filtered in line 168.
+        // @ts-expect-error | Might be fixed through having all the types in the action. Otherwise: It works properly, as all the symbols are filtered in before calling forEach
         .forEach((key) => featuresByLayerId[key].forEach(addFeature))
     },
     50
