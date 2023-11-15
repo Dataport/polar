91ec01b24dcb5e5b8e7f51b090b01805c16b5992
Pascal Roehling <pas.roehling@gmail.com>
2023-11-15 17:30:18 +0100

    Focus first element after successful search

--- packages/plugins/AddressSearch/src/components/Results.vue
+++ packages/plugins/AddressSearch/src/components/Results.vue
@@ -124,6 +124,18 @@ export default Vue.extend({
     },
   },
   watch: {
+    featuresAvailable(): void {
+      this.$nextTick(() => {
+        const firstElement =
+          // @ts-expect-error | Type conversion is fine here as the querySelector method is monkeyPatched in core/createMap
+          (document.querySelector('[data-app]') as ShadowRoot).getElementById(
+            'polar-plugin-address-search-results-feature-0-0'
+          )
+        if (firstElement) {
+          firstElement.focus()
+        }
+      })
+    },
     /* reset opened categories on group change */
     selectedGroupId(): void {
       this.openCategories = []
