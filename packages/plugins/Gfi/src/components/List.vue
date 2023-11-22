c0488fc9d7b4b9ec75a7bfc596c831d087723502
Dennis Sen <dennis.sen@dataport.de>
2023-11-22 06:07:41 +0100

    add max width for gfi list

--- packages/plugins/Gfi/src/components/List.vue
+++ packages/plugins/Gfi/src/components/List.vue
@@ -50,6 +50,11 @@ export default Vue.extend({
 </script>
 
 <style scoped lang="scss">
+#polar-plugin-gfi:not(.polar-plugin-gfi-move-handle) .plugin-gfi-list {
+  /* magic number; leaves minimal space to minimal size AddressSearch in IconMenu mode */
+  max-width: 420px;
+}
+
 .plugin-gfi-list {
   .v-card__title {
     display: flex;
