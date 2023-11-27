2f0a6586f951dd11a268fbeec22ba2a2119903b3
Dennis Sen <dennis.sen@dataport.de>
2023-11-27 05:56:05 +0100

    add types

--- packages/plugins/Gfi/src/utils/requestGfiGeoJson.ts
+++ packages/plugins/Gfi/src/utils/requestGfiGeoJson.ts
@@ -1,16 +1,32 @@
 import { Feature as GeoJsonFeature } from 'geojson'
+import { Feature, Map } from 'ol'
 import { GeoJSON } from 'ol/format'
+import { Layer } from 'ol/layer'
 
 const writer = new GeoJSON()
 
 /**
  * Returns features from GeoJSON layer as GeoJSON.
  */
-export default ({ map, coordinate, layer }): Promise<GeoJsonFeature[]> =>
+export default ({
+  map,
+  coordinate,
+  layer,
+}: {
+  map: Map
+  coordinate: [number, number]
+  layer: Layer
+}): Promise<GeoJsonFeature[]> =>
   Promise.resolve(
     map
       .getFeaturesAtPixel(map.getPixelFromCoordinate(coordinate), {
         layerFilter: (candidate) => candidate === layer,
       })
-      .map((feature) => JSON.parse(writer.writeFeature(feature)))
+      .map((feature) =>
+        feature instanceof Feature
+          ? JSON.parse(writer.writeFeature(feature))
+          : false
+      )
+      // remove FeatureLikes
+      .filter((x) => x)
   )
