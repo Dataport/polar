0e33f6c9d4f21e5e8e8e8fa62d8e48ed4b78a908
Dennis Sen <dennis.sen@dataport.de>
2023-12-11 06:16:09 +0100

    add missing type

--- packages/core/src/utils/resolveClusterClick.ts
+++ packages/core/src/utils/resolveClusterClick.ts
@@ -2,11 +2,11 @@ import { Feature, Map } from 'ol'
 import { createEmpty, extend } from 'ol/extent'
 
 export const resolveClusterClick = (map: Map, feature: Feature) => {
-  const features = feature.get('features')
+  const features: Feature[] = feature.get('features')
 
   const extent = createEmpty()
   features.forEach((feature) =>
-    extend(extent, feature.getGeometry().getExtent())
+    extend(extent, feature.getGeometry()?.getExtent() || [])
   )
 
   map.getView().fit(extent, {
