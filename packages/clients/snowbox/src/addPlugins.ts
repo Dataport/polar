16f61022ed0d85e9809c1e8bd4667db71da4ba02
Pascal Roehling <pas.roehling@gmail.com>
2023-12-13 14:57:49 +0100

    Update icons of layerChooser to be clearer for the content behind it

--- packages/clients/snowbox/src/addPlugins.ts
+++ packages/clients/snowbox/src/addPlugins.ts
@@ -29,7 +29,7 @@ export const addPlugins = (core) => {
     menus: [
       {
         plugin: LayerChooser({}),
-        icon: 'fa-book-atlas',
+        icon: 'fa-layer-group',
         id: 'layerChooser',
       },
       {
