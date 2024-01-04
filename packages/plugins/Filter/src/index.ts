eee80d3d8b9548a3218cec9187ddc92c478e576c
Dennis Sen <dennis.sen@dataport.de>
2024-01-04 08:21:20 +0100

    make Filter plugin SPA ready
    Relates: #66

--- packages/plugins/Filter/src/index.ts
+++ packages/plugins/Filter/src/index.ts
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import { FilterConfiguration } from '@polar/lib-custom-types'
 import { Filter } from './components'
 import language from './language'
-import storeModule from './store'
+import { makeStoreModule } from './store'
 
 export default (options: FilterConfiguration) => (instance: Vue) =>
   instance.$store.dispatch('addComponent', {
     name: 'filter',
     plugin: Filter,
     language,
-    storeModule,
+    storeModule: makeStoreModule(),
     options,
   })
--- packages/plugins/Filter/src/store/index.ts
+++ packages/plugins/Filter/src/store/index.ts
@@ -21,186 +21,197 @@ const getInitialState = (): FilterState => ({
   time: {},
 })
 
-const storeModule: PolarModule<FilterState, FilterGetters> = {
-  namespaced: true,
-  state: getInitialState(),
-  actions: {
-    setupModule({ getters: { filterConfiguration }, commit }): void {
-      Object.entries(filterConfiguration.layers).forEach(
-        ([layerId, { categories, time }]) => {
-          if (categories) {
-            categories.forEach(({ targetProperty, knownValues }) =>
+// OK for module creation
+// eslint-disable-next-line max-lines-per-function
+export const makeStoreModule = () => {
+  const storeModule: PolarModule<FilterState, FilterGetters> = {
+    namespaced: true,
+    state: getInitialState(),
+    actions: {
+      setupModule({ getters: { filterConfiguration }, commit }): void {
+        Object.entries(filterConfiguration.layers).forEach(
+          ([layerId, { categories, time }]) => {
+            if (categories) {
+              categories.forEach(({ targetProperty, knownValues }) =>
+                commit('setupState', {
+                  path: ['category', layerId, targetProperty],
+                  value: knownValues.reduce((accumulator, current) => {
+                    accumulator[current] = true
+                    return accumulator
+                  }, {}),
+                })
+              )
+            }
+            if (time) {
+              const { targetProperty, freeSelection, pattern } = time
               commit('setupState', {
-                path: ['category', layerId, targetProperty],
-                value: knownValues.reduce((accumulator, current) => {
-                  accumulator[current] = true
-                  return accumulator
-                }, {}),
-              })
-            )
-          }
-          if (time) {
-            const { targetProperty, freeSelection, pattern } = time
-            commit('setupState', {
-              path: ['time', layerId],
-              value: {
-                targetProperty,
-                pattern: pattern || 'YYYY-MM-DD',
-                radioId: 0,
-                freeSelection: freeSelection ? [] : null,
-              },
-            })
-          }
-        }
-      )
-    },
-    toggleCategory(
-      { getters, commit, dispatch },
-      payload: {
-        layerId: LayerId
-        targetProperty: TargetProperty
-        knownValue: KnownValue
-      }
-    ) {
-      const value = !getters.getActiveCategory(payload)
-      commit('setCategory', { ...payload, value })
-      dispatch('updateFeatureVisibility', payload.layerId)
-    },
-    toggleCategoryAll(
-      { getters, commit, dispatch },
-      payload: {
-        layerId: LayerId
-        targetProperty: TargetProperty
-      }
-    ) {
-      // 'indeterminate' to false intentionally (something had to be decided)
-      const value = !getters.getActiveCategoryAll(payload)
-      const { layerId } = payload
-      // @ts-expect-error | this call only happens if structures exist (generation in .vue)
-      getters
-        .getCategories(layerId)
-        .find(({ targetProperty }) => targetProperty === payload.targetProperty)
-        .knownValues.forEach((knownValue) => {
-          commit('setCategory', { ...payload, knownValue, value })
-        })
-      dispatch('updateFeatureVisibility', payload.layerId)
-    },
-    changeTimeRadio(
-      { commit, dispatch },
-      payload: { layerId: LayerId; radioId: number }
-    ) {
-      commit('setTimeRadio', payload)
-      dispatch('updateFeatureVisibility', payload.layerId)
-    },
-    changeFreeSelection(
-      { commit, dispatch },
-      { layerId, freeSelection }: { layerId: LayerId; freeSelection: Date[] }
-    ) {
-      commit('setFreeSelection', { layerId, freeSelection })
-      dispatch('updateFeatureVisibility', layerId)
-    },
-    updateFeatureVisibility({ state, rootGetters, getters }, layerId: LayerId) {
-      updateFeatureVisibility({
-        map: rootGetters.map,
-        layerId,
-        categories: getters.getCategories(layerId),
-        timeOptions: getters.getTimeOptions(layerId),
-        state: JSON.parse(JSON.stringify(state)),
-      })
-    },
-  },
-  mutations: {
-    ...generateSimpleMutations(getInitialState()),
-    setCategory(
-      state,
-      {
-        layerId,
-        targetProperty,
-        knownValue,
-        value,
-      }: {
-        layerId: LayerId
-        targetProperty: TargetProperty
-        knownValue: KnownValue
-        value: boolean
-      }
-    ) {
-      state.category[layerId][targetProperty][knownValue] = value
-    },
-    setupState(state, { path, value }: { path: string[]; value: boolean }) {
-      setState(state, path, value)
-    },
-    setTimeRadio(
-      state,
-      { layerId, radioId }: { layerId: LayerId; radioId: number }
-    ) {
-      state.time[layerId].radioId = radioId
-    },
-    setFreeSelection(
-      state,
-      { layerId, freeSelection }: { layerId: LayerId; freeSelection: Date[] }
-    ) {
-      state.time[layerId].freeSelection = freeSelection
-    },
-  },
-  getters: {
-    ...generateSimpleGetters(getInitialState()),
-    filterConfiguration(_, __, ___, rootGetters): FilterConfiguration {
-      return rootGetters.configuration?.filter || { layers: {} }
-    },
-    getActiveCategory:
-      (state) =>
-      ({ layerId, targetProperty, knownValue }) =>
-        state.category[layerId][targetProperty][knownValue],
-    getActiveCategoryAll:
-      (state) =>
-      ({ layerId, targetProperty }) => {
-        const allValues = Object.values(state.category[layerId][targetProperty])
-        if (arrayOnlyContains(allValues, true)) {
-          return true
-        }
-        if (arrayOnlyContains(allValues, false)) {
-          return false
-        }
-        return 'indeterminate'
-      },
-    getActiveTime:
-      (state) =>
-      ({ layerId }) =>
-        state.time[layerId].radioId,
-    getCategories: (_, getters) => (layerId) =>
-      getters.filterConfiguration.layers[layerId]?.categories || [],
-    getTimeConfig:
-      (_, { filterConfiguration }) =>
-      (layerId) =>
-        filterConfiguration.layers[layerId]?.time || null,
-    getTimeOptions:
-      (_, { getTimeConfig }) =>
-      (layerId) => {
-        const timeConfig = getTimeConfig(layerId)
-        if (!timeConfig) {
-          return []
-        }
-        return [
-          ...(timeConfig.last || []).map(parseTimeOption('last')).flat(1),
-          ...(timeConfig.next || []).map(parseTimeOption('next')).flat(1),
-          ...(!timeConfig.freeSelection
-            ? []
-            : [
-                {
-                  label: 'common:plugins.filter.time.chooseTimeFrame.label',
-                  component: ChooseTimeFrame,
-                  amount: null,
-                  unit: timeConfig.freeSelection.unit || 'days',
-                  now: timeConfig.freeSelection.now,
-                  type: 'freeSelection',
+                path: ['time', layerId],
+                value: {
+                  targetProperty,
+                  pattern: pattern || 'YYYY-MM-DD',
+                  radioId: 0,
+                  freeSelection: freeSelection ? [] : null,
                 },
-              ]),
-        ]
+              })
+            }
+          }
+        )
       },
-    getFreeSelection: (state) => (layerId: string) =>
-      state.time[layerId].freeSelection,
-  },
-}
+      toggleCategory(
+        { getters, commit, dispatch },
+        payload: {
+          layerId: LayerId
+          targetProperty: TargetProperty
+          knownValue: KnownValue
+        }
+      ) {
+        const value = !getters.getActiveCategory(payload)
+        commit('setCategory', { ...payload, value })
+        dispatch('updateFeatureVisibility', payload.layerId)
+      },
+      toggleCategoryAll(
+        { getters, commit, dispatch },
+        payload: {
+          layerId: LayerId
+          targetProperty: TargetProperty
+        }
+      ) {
+        // 'indeterminate' to false intentionally (something had to be decided)
+        const value = !getters.getActiveCategoryAll(payload)
+        const { layerId } = payload
+        // @ts-expect-error | this call only happens if structures exist (generation in .vue)
+        getters
+          .getCategories(layerId)
+          .find(
+            ({ targetProperty }) => targetProperty === payload.targetProperty
+          )
+          .knownValues.forEach((knownValue) => {
+            commit('setCategory', { ...payload, knownValue, value })
+          })
+        dispatch('updateFeatureVisibility', payload.layerId)
+      },
+      changeTimeRadio(
+        { commit, dispatch },
+        payload: { layerId: LayerId; radioId: number }
+      ) {
+        commit('setTimeRadio', payload)
+        dispatch('updateFeatureVisibility', payload.layerId)
+      },
+      changeFreeSelection(
+        { commit, dispatch },
+        { layerId, freeSelection }: { layerId: LayerId; freeSelection: Date[] }
+      ) {
+        commit('setFreeSelection', { layerId, freeSelection })
+        dispatch('updateFeatureVisibility', layerId)
+      },
+      updateFeatureVisibility(
+        { state, rootGetters, getters },
+        layerId: LayerId
+      ) {
+        updateFeatureVisibility({
+          map: rootGetters.map,
+          layerId,
+          categories: getters.getCategories(layerId),
+          timeOptions: getters.getTimeOptions(layerId),
+          state: JSON.parse(JSON.stringify(state)),
+        })
+      },
+    },
+    mutations: {
+      ...generateSimpleMutations(getInitialState()),
+      setCategory(
+        state,
+        {
+          layerId,
+          targetProperty,
+          knownValue,
+          value,
+        }: {
+          layerId: LayerId
+          targetProperty: TargetProperty
+          knownValue: KnownValue
+          value: boolean
+        }
+      ) {
+        state.category[layerId][targetProperty][knownValue] = value
+      },
+      setupState(state, { path, value }: { path: string[]; value: boolean }) {
+        setState(state, path, value)
+      },
+      setTimeRadio(
+        state,
+        { layerId, radioId }: { layerId: LayerId; radioId: number }
+      ) {
+        state.time[layerId].radioId = radioId
+      },
+      setFreeSelection(
+        state,
+        { layerId, freeSelection }: { layerId: LayerId; freeSelection: Date[] }
+      ) {
+        state.time[layerId].freeSelection = freeSelection
+      },
+    },
+    getters: {
+      ...generateSimpleGetters(getInitialState()),
+      filterConfiguration(_, __, ___, rootGetters): FilterConfiguration {
+        return rootGetters.configuration?.filter || { layers: {} }
+      },
+      getActiveCategory:
+        (state) =>
+        ({ layerId, targetProperty, knownValue }) =>
+          state.category[layerId][targetProperty][knownValue],
+      getActiveCategoryAll:
+        (state) =>
+        ({ layerId, targetProperty }) => {
+          const allValues = Object.values(
+            state.category[layerId][targetProperty]
+          )
+          if (arrayOnlyContains(allValues, true)) {
+            return true
+          }
+          if (arrayOnlyContains(allValues, false)) {
+            return false
+          }
+          return 'indeterminate'
+        },
+      getActiveTime:
+        (state) =>
+        ({ layerId }) =>
+          state.time[layerId].radioId,
+      getCategories: (_, getters) => (layerId) =>
+        getters.filterConfiguration.layers[layerId]?.categories || [],
+      getTimeConfig:
+        (_, { filterConfiguration }) =>
+        (layerId) =>
+          filterConfiguration.layers[layerId]?.time || null,
+      getTimeOptions:
+        (_, { getTimeConfig }) =>
+        (layerId) => {
+          const timeConfig = getTimeConfig(layerId)
+          if (!timeConfig) {
+            return []
+          }
+          return [
+            ...(timeConfig.last || []).map(parseTimeOption('last')).flat(1),
+            ...(timeConfig.next || []).map(parseTimeOption('next')).flat(1),
+            ...(!timeConfig.freeSelection
+              ? []
+              : [
+                  {
+                    label: 'common:plugins.filter.time.chooseTimeFrame.label',
+                    component: ChooseTimeFrame,
+                    amount: null,
+                    unit: timeConfig.freeSelection.unit || 'days',
+                    now: timeConfig.freeSelection.now,
+                    type: 'freeSelection',
+                  },
+                ]),
+          ]
+        },
+      getFreeSelection: (state) => (layerId: string) =>
+        state.time[layerId].freeSelection,
+    },
+  }
 
-export default storeModule
+  return storeModule
+}
