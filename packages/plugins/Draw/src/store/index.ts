8b82fe06f717915f314c3ca1ca3ecc007d2ae198
Dennis Sen <dennis.sen@dataport.de>
2024-01-04 08:23:17 +0100

    make Draw plugin SPA ready
    Relates: #66

--- packages/plugins/Draw/src/store/index.ts
+++ packages/plugins/Draw/src/store/index.ts
@@ -6,10 +6,7 @@ import { Feature } from 'geojson'
 import { PolarModule } from '@polar/lib-custom-types'
 import noop from '@repositoryname/noop'
 import { DrawGetters, DrawMutations, DrawState } from '../types'
-import actions, { drawSource } from './actions'
-
-// NOTE hack to keep complex objects out of vuex
-let selectedFeature = null
+import { makeActions } from './actions'
 
 const getInitialState = (): DrawState => ({
   mode: 'none',
@@ -23,110 +20,122 @@ const getInitialState = (): DrawState => ({
   selectedFeature: 1,
 })
 
-const storeModule: PolarModule<DrawState, DrawGetters> = {
-  namespaced: true,
-  state: getInitialState(),
-  actions,
-  getters: {
-    ...generateSimpleGetters(getInitialState()),
-    selectableDrawModes(_, { configuration }) {
-      /* eslint-disable @typescript-eslint/naming-convention */
-      // NOTE: Keys are directly used as technical keys for ol/interaction/Draw and are allowed to differ from the naming scheme.
-      const allSelectableDrawModes = {
-        Circle: 'common:plugins.draw.drawMode.circle',
-        LineString: 'common:plugins.draw.drawMode.lineString',
-        Point: 'common:plugins.draw.drawMode.point',
-        Polygon: 'common:plugins.draw.drawMode.polygon',
-        Text: 'common:plugins.draw.drawMode.text',
-      }
-      /* eslint-enable @typescript-eslint/naming-convention */
+// OK for module creation
+// eslint-disable-next-line max-lines-per-function
+export const makeStoreModule = () => {
+  // NOTE hack to keep complex objects out of vuex
+  let selectedFeature = null
 
-      // TODO DO SOMETHING DIFFERENT THAN ESLINT IGNORE
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const { Text, ...defaultSelectableDrawModes } = allSelectableDrawModes
+  const { actions, drawSource } = makeActions()
 
-      return configuration?.selectableDrawModes
-        ? configuration.selectableDrawModes.reduce(
-            (acc, curr) => ({ ...acc, [curr]: allSelectableDrawModes[curr] }),
-            {}
-          )
-        : defaultSelectableDrawModes
-    },
-    selectedFeature: (state) => {
-      noop(state.selectedFeature)
-      return selectedFeature
-    },
-    selectableModes(_, { configuration }) {
-      const includesWrite = configuration?.selectableDrawModes?.includes('Text')
-      const selectableModesDraw = {
-        none: 'common:plugins.draw.mode.none',
-        draw: includesWrite
-          ? 'common:plugins.draw.mode.write'
-          : 'common:plugins.draw.mode.draw',
-        edit: 'common:plugins.draw.mode.edit',
-        delete: 'common:plugins.draw.mode.delete',
-      }
-      return selectableModesDraw
-    },
-    showTextInput({ drawMode, mode }, { selectedFeature }) {
-      return (
-        (drawMode === 'Text' && mode === 'draw') ||
-        (mode === 'edit' &&
-          selectedFeature &&
-          typeof selectedFeature.get('text') === 'string')
-      )
-    },
-    configuration(_, __, ___, rootGetters) {
-      return rootGetters.configuration?.draw || {}
-    },
-    fontSizes(_, { configuration }) {
-      const { textStyle } = configuration
-      if (textStyle?.font) {
-        if (typeof textStyle.font === 'string') {
-          return []
+  const storeModule: PolarModule<DrawState, DrawGetters> = {
+    namespaced: true,
+    state: getInitialState(),
+    actions,
+    getters: {
+      ...generateSimpleGetters(getInitialState()),
+      selectableDrawModes(_, { configuration }) {
+        /* eslint-disable @typescript-eslint/naming-convention */
+        // NOTE: Keys are directly used as technical keys for ol/interaction/Draw and are allowed to differ from the naming scheme.
+        const allSelectableDrawModes = {
+          Circle: 'common:plugins.draw.drawMode.circle',
+          LineString: 'common:plugins.draw.drawMode.lineString',
+          Point: 'common:plugins.draw.drawMode.point',
+          Polygon: 'common:plugins.draw.drawMode.polygon',
+          Text: 'common:plugins.draw.drawMode.text',
         }
-        return textStyle.font.size ? textStyle.font.size.slice(0, 5) : [10]
-      }
-      return []
-    },
-    showSizeSlider(_, { fontSizes, showTextInput }) {
-      return showTextInput && fontSizes.length > 1
-    },
-    textSize(_, { fontSizes, selectedSize }) {
-      return fontSizes[selectedSize]
-    },
-  },
-  mutations: {
-    ...generateSimpleMutations(getInitialState()),
-    updateFeatures(state) {
-      const features = drawSource.getFeatures().map((feature) => {
-        const geometry = feature.getGeometry()
-        const type = geometry.getType()
-        const isCircle = type === 'Circle'
-        const jsonFeature: Feature = {
-          type: 'Feature',
-          properties: feature.get('text') ? { text: feature.get('text') } : {},
-          geometry: {
-            type: isCircle ? 'Point' : type,
-            coordinates: isCircle
-              ? geometry.getCenter()
-              : geometry.getCoordinates(),
-          },
+        /* eslint-enable @typescript-eslint/naming-convention */
+
+        // TODO DO SOMETHING DIFFERENT THAN ESLINT IGNORE
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const { Text, ...defaultSelectableDrawModes } = allSelectableDrawModes
+
+        return configuration?.selectableDrawModes
+          ? configuration.selectableDrawModes.reduce(
+              (acc, curr) => ({ ...acc, [curr]: allSelectableDrawModes[curr] }),
+              {}
+            )
+          : defaultSelectableDrawModes
+      },
+      selectedFeature: (state) => {
+        noop(state.selectedFeature)
+        return selectedFeature
+      },
+      selectableModes(_, { configuration }) {
+        const includesWrite =
+          configuration?.selectableDrawModes?.includes('Text')
+        const selectableModesDraw = {
+          none: 'common:plugins.draw.mode.none',
+          draw: includesWrite
+            ? 'common:plugins.draw.mode.write'
+            : 'common:plugins.draw.mode.draw',
+          edit: 'common:plugins.draw.mode.edit',
+          delete: 'common:plugins.draw.mode.delete',
         }
-        // NOTE: If one is checking if properties exists (which it clearly does), TS complains
-        // "TS2531: Object is possibly 'null'.". This is due to the structure of the type GeoJsonProperties.
-        if (isCircle && jsonFeature.properties) {
-          jsonFeature.properties.radius = geometry.getRadius()
+        return selectableModesDraw
+      },
+      showTextInput({ drawMode, mode }, { selectedFeature }) {
+        return (
+          (drawMode === 'Text' && mode === 'draw') ||
+          (mode === 'edit' &&
+            selectedFeature &&
+            typeof selectedFeature.get('text') === 'string')
+        )
+      },
+      configuration(_, __, ___, rootGetters) {
+        return rootGetters.configuration?.draw || {}
+      },
+      fontSizes(_, { configuration }) {
+        const { textStyle } = configuration
+        if (textStyle?.font) {
+          if (typeof textStyle.font === 'string') {
+            return []
+          }
+          return textStyle.font.size ? textStyle.font.size.slice(0, 5) : [10]
         }
-        return jsonFeature
-      })
-      state.featureCollection = { ...state.featureCollection, features }
+        return []
+      },
+      showSizeSlider(_, { fontSizes, showTextInput }) {
+        return showTextInput && fontSizes.length > 1
+      },
+      textSize(_, { fontSizes, selectedSize }) {
+        return fontSizes[selectedSize]
+      },
     },
-    setSelectedFeature: (state, payload) => {
-      selectedFeature = payload
-      state.selectedFeature = state.selectedFeature + 1
-    },
-  } as DrawMutations,
+    mutations: {
+      ...generateSimpleMutations(getInitialState()),
+      updateFeatures(state) {
+        const features = drawSource.getFeatures().map((feature) => {
+          const geometry = feature.getGeometry()
+          const type = geometry.getType()
+          const isCircle = type === 'Circle'
+          const jsonFeature: Feature = {
+            type: 'Feature',
+            properties: feature.get('text')
+              ? { text: feature.get('text') }
+              : {},
+            geometry: {
+              type: isCircle ? 'Point' : type,
+              coordinates: isCircle
+                ? geometry.getCenter()
+                : geometry.getCoordinates(),
+            },
+          }
+          // NOTE: If one is checking if properties exists (which it clearly does), TS complains
+          // "TS2531: Object is possibly 'null'.". This is due to the structure of the type GeoJsonProperties.
+          if (isCircle && jsonFeature.properties) {
+            jsonFeature.properties.radius = geometry.getRadius()
+          }
+          return jsonFeature
+        })
+        state.featureCollection = { ...state.featureCollection, features }
+      },
+      setSelectedFeature: (state, payload) => {
+        selectedFeature = payload
+        state.selectedFeature = state.selectedFeature + 1
+      },
+    } as DrawMutations,
+  }
+
+  return storeModule
 }
-
-export default storeModule
