c230ce87437664925545b0950d03ba13db2d9dd9
Dennis Sen <dennis.sen@dataport.de>
2024-02-05 11:03:58 +0100

    release: test

--- scripts/publishPackages.js
+++ scripts/publishPackages.js
@@ -33,14 +33,15 @@ for (const path of packages) {
     if (nextVersion) {
       const context = { cwd: path, stdio: 'inherit' }
       tags.push(`${getPackageName(path)}@${nextVersion}`)
-      cp.execSync('npm config set loglevel warn')
-      cp.execSync('npm version ' + nextVersion, context)
+
+      cp.execSync('npm version ' + nextVersion + ' --silent', context)
       cp.execSync(
         'npm set //registry.npmjs.org/:_authToken ' +
-          process.env.NODE_AUTH_TOKEN,
+          process.env.NODE_AUTH_TOKEN +
+          ' --silent',
         { cwd: path }
       )
-      cp.execSync('npm publish --access=public --dry-run', context)
+      cp.execSync('npm publish --access=public --dry-run --silent', context)
     }
   } catch (e) {
     console.error(e)
