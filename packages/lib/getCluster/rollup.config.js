34c2d9f844a3462c1ccabc6d81f9298a0b55e08b
Pascal Roehling <pas.roehling@gmail.com>
2024-02-19 12:13:53 +0100

    Migrate @polar/lib-get-cluster to the new rollup setup

---
+++ packages/lib/getCluster/rollup.config.js
@@ -0,0 +1,21 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
+import typescript from 'rollup-plugin-typescript2'
+import { createFilter } from '@rollup/pluginutils'
+
+const external = createFilter(['ol/**'], null, { resolve: false })
+
+export default {
+  input: 'index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      name: '@polar/lib-get-cluster',
+      format: 'es',
+      sourcemap: 'inline',
+    },
+  ],
+  external,
+  plugins: [typescript({ check: true }), commonjs(), resolve(), terser()],
+}
