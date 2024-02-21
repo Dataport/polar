7ab1443deda04fcd67dcd67f29df75ce736909f1
Pascal Roehling <pas.roehling@gmail.com>
2024-02-21 19:58:49 +0100

    Properly document dependencies fo @polar/lib-passes-boundary-check

--- packages/lib/passesBoundaryCheck/rollup.config.js
+++ packages/lib/passesBoundaryCheck/rollup.config.js
@@ -1,5 +1,10 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
 import terser from '@rollup/plugin-terser'
 import typescript from 'rollup-plugin-typescript2'
+import { createFilter } from '@rollup/pluginutils'
+
+const external = createFilter(['ol/**'], null, { resolve: false })
 
 export default {
   input: 'index.ts',
@@ -11,5 +16,6 @@ export default {
       sourcemap: 'inline',
     },
   ],
-  plugins: [typescript({ check: true }), terser()],
+  external,
+  plugins: [typescript({ check: true }), commonjs(), resolve(), terser()],
 }
