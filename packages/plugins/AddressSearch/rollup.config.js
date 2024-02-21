f85892ab7046daaf873f395f77f912b308de93fa
Pascal Roehling <pas.roehling@gmail.com>
2024-02-21 20:02:07 +0100

    Remove unused peerDependency of @polar/plugin-address-search

--- packages/plugins/AddressSearch/rollup.config.js
+++ packages/plugins/AddressSearch/rollup.config.js
@@ -12,7 +12,6 @@ const external = createFilter(
     '@masterportal/masterportalapi/**',
     '@polar/**',
     '@repositoryname/vuex-generators',
-    'i18next',
     'lodash.debounce',
     'ol/**',
     'vue',
