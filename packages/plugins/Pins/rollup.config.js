901890c5204d07a8586e99924c206c9c29d991f7
Pascal Roehling <pas.roehling@gmail.com>
2024-02-22 22:09:43 +0100

    Remove style imports from non-component providing packages
    Also, move vue dependency in Pins to only be a devDependency as no
    vue component is provided by the package.

--- packages/plugins/Pins/rollup.config.js
+++ packages/plugins/Pins/rollup.config.js
@@ -1,10 +1,8 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
-import scss from 'rollup-plugin-scss'
 import svg from 'rollup-plugin-svg'
 import terser from '@rollup/plugin-terser'
 import typescript from 'rollup-plugin-typescript2'
-import vue from 'rollup-plugin-vue'
 import { createFilter } from '@rollup/pluginutils'
 
 const external = createFilter(
@@ -12,7 +10,6 @@ const external = createFilter(
     '@polar/lib-passes-boundary-check',
     '@repositoryname/vuex-generators',
     'ol/**',
-    'vue',
   ],
   null,
   { resolve: false }
@@ -30,11 +27,6 @@ export default {
   ],
   external,
   plugins: [
-    vue({
-      defaultLang: { script: 'ts', style: 'scss' },
-      css: false,
-    }),
-    scss({ fileName: 'index.css', sourceMap: true }),
     // Types are not checked as typechecking does not work for vue-files with the current version, might be fixable with an upgrade of vue and vuex; see https://github.com/vuejs/vuex/pull/1121.
     typescript({ check: false }),
     commonjs(),
