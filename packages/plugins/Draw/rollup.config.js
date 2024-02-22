667659836db367c6dbcaf3e6eeacdd82835345a4
Pascal Roehling <pas.roehling@gmail.com>
2024-02-22 22:24:05 +0100

    Migrate @polar/plugin-draw to the new rollup setup
    Also, import the styles from the package in all clients.

---
+++ packages/plugins/Draw/rollup.config.js
@@ -0,0 +1,38 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import scss from 'rollup-plugin-scss'
+import terser from '@rollup/plugin-terser'
+import typescript from 'rollup-plugin-typescript2'
+import vue from 'rollup-plugin-vue'
+import { createFilter } from '@rollup/pluginutils'
+
+const external = createFilter(
+  ['@repositoryname/**', 'ol/**', 'vue', 'vuex'],
+  null,
+  { resolve: false }
+)
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      name: '@polar/plugin-draw',
+      format: 'es',
+      sourcemap: 'inline',
+    },
+  ],
+  external,
+  plugins: [
+    vue({
+      defaultLang: { script: 'ts', style: 'scss' },
+      css: false,
+    }),
+    scss({ fileName: 'index.css', sourceMap: true }),
+    // Types are not checked as typechecking does not work for vue-files with the current version, might be fixable with an upgrade of vue and vuex; see https://github.com/vuejs/vuex/pull/1121.
+    typescript({ check: false }),
+    commonjs(),
+    resolve(),
+    terser(),
+  ],
+}
