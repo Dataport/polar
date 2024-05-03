70817d45920ad84c1ac42226215a1ba3ec11674b
Pascal Roehling <pas.roehling@gmail.com>
2024-05-03 11:48:30 +0200

    Correctly describe config optionality of @polar/plugin-layer-chooser
    Also, fix some outdated or false documentation texts and add missing
    parameters.

--- packages/types/custom/core.ts
+++ packages/types/custom/core.ts
@@ -460,13 +460,19 @@ export type LayerType = 'mask' | 'background'
 
 export interface LayerConfigurationOptionLayers {
   /**
+   * Legend image to be used for sub-layer. If false, no image is displayed.
+   * If true, it is assumed an image exists in the layer's GetCapabilities, and
+   * that will be used. If Record, it maps the layer name to a linked image.
+   */
+  legend?: boolean | Record<string, string>
+  /**
    * Comma-separated re-ordering of service layer's 'layer' specification.
    * Layer's not specified in service definition, but in order, are initially
    * invisible. Layers not specified in order, but in service definition, are
    * always visible. Layers specified in both are initially visible. Layers
    * specified in neither are always invisible.
    */
-  order: string
+  order?: string
   /**
    * Title to be displayed for sub-layer. If false, layer name itself will
    * be used as given in service description 'layers' field. If true, it is
@@ -474,20 +480,14 @@ export interface LayerConfigurationOptionLayers {
    * used. If Record, it maps the layer name to an arbitrary display name given
    * by the configuration.
    */
-  title: boolean | Record<string, string>
-  /**
-   * Legend image to be used for sub-layer. If false, no image is displayed.
-   * If true, it is assumed an image exists in the layer's GetCapabilities, and
-   * that will be used. If Record, it maps the layer name to a linked image.
-   */
-  legend: boolean | Record<string, string>
+  title?: boolean | Record<string, string>
 }
 
 export interface LayerConfigurationOptions {
   /**
    * Named matching OGC-specification of a WMS layer's layers.
    */
-  layers?: LayerConfigurationOptionLayers
+  layers?: boolean | LayerConfigurationOptionLayers
   // NOT IMPLEMENTED
   // transparency: boolean
 }
@@ -499,20 +499,16 @@ export interface LayerConfiguration {
   name: string
   /** Whether the layer is a background layer or a feature layer with specific information */
   type: LayerType
-  /** Whether the layer should be rendered; defaults to false */
-  visibility?: boolean
-  /** Whether the layer should be hidden from the LayerChooser selection menu */
+  /** Whether the mask-layer should be hidden from the LayerChooser selection menu */
   hideInMenu?: boolean
   /** The minimum zoom level the layer will be rendered in; defaults to 0 */
   minZoom?: number
   /** The maximum zoom level the layer will be rendered in; defaults to Number.MAX_SAFE_INTEGER */
   maxZoom?: number
-  /** Url to use to proxy the request with */
-  proxyUrl?: string
-  // TODO: Is the property 'loadingStrategy' still in use?
-  loadingStrategy?: StrategyOptions
   /** Enables a configuration feature for the layer in its selection. */
   options?: LayerConfigurationOptions
+  /** Whether the layer should be rendered; defaults to false */
+  visibility?: boolean
 }
 
 export interface PolarMapOptions {
