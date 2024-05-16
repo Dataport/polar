0918a8270e05cd9fca2c579b8d74afec72f130c2
Dennis Sen <dennis.sen@dataport.de>
2024-05-16 06:08:28 +0200

    fix keyboard interaction (tabbing fired buttons)

--- packages/clients/textLocator/src/plugins/GeometrySearch/components/Action.vue
+++ packages/clients/textLocator/src/plugins/GeometrySearch/components/Action.vue
@@ -11,7 +11,7 @@
         v-bind="attrs"
         v-on="on"
         @click="action(item)"
-        @keydown="action(item)"
+        @keydown.enter="action(item)"
       >
         <v-icon>{{ icon }}</v-icon>
       </v-btn>
