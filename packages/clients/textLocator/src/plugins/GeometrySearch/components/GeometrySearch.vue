85286b4d756d8dc67e3a802f8ffdfdf705030e69
Dennis Sen <dennis.sen@dataport.de>
2024-05-16 06:02:52 +0200

    refactor: split component

--- packages/clients/textLocator/src/plugins/GeometrySearch/components/GeometrySearch.vue
+++ packages/clients/textLocator/src/plugins/GeometrySearch/components/GeometrySearch.vue
@@ -6,65 +6,7 @@
     </v-card-title>
     <v-card-text>
       <ViewToggle />
-      <v-treeview
-        v-if="treeViewItems.length"
-        dense
-        hoverable
-        color="info"
-        :items="treeViewItems"
-      >
-        <template #label="{ item }">
-          <v-badge color="info" inline left class="text-locator-result-badge">
-            <template #badge>
-              <v-tooltip left>
-                <template #activator="{ on }">
-                  <span v-on="on">{{ item.count }}</span>
-                </template>
-                <span>{{ $t(tooltipKey(item)) }}</span>
-              </v-tooltip>
-            </template>
-            {{ item.name }}
-            <template v-if="item.type === 'toponym' && item.feature">
-              &nbsp;
-              <ResultInfo :feature="item.feature" direction="left"></ResultInfo>
-            </template>
-          </v-badge>
-        </template>
-        <template #append="{ item }">
-          <Action
-            v-if="item.type === 'toponym' || item.children"
-            :item="item"
-            :aria-label-key="`plugins.geometrySearch.tooltip.highlight.${
-              item?.children?.length && item.type !== 'toponym'
-                ? 'heat'
-                : 'cold'
-            }`"
-            :action="changeActiveData"
-            icon="fa-map-location-dot"
-            :tooltip-key="`plugins.geometrySearch.tooltip.highlight.${
-              item?.children?.length && item.type !== 'toponym'
-                ? 'heat'
-                : 'cold'
-            }`"
-          ></Action>
-          <Action
-            v-if="item.type === 'toponym'"
-            :item="item"
-            aria-label-key="plugins.geometrySearch.tooltip.focusSearch"
-            :action="fullSearchOnToponym"
-            icon="fa-magnifying-glass-arrow-right"
-            tooltip-key="plugins.geometrySearch.tooltip.focusSearch"
-          ></Action>
-          <Action
-            v-if="item.type === 'text'"
-            :item="item"
-            aria-label-key="plugins.geometrySearch.tooltip.textSearch"
-            :action="fullSearchLiterature"
-            icon="fa-magnifying-glass-arrow-right"
-            tooltip-key="plugins.geometrySearch.tooltip.textSearch"
-          ></Action>
-        </template>
-      </v-treeview>
+      <TreeView v-if="treeViewItems.length" />
       <v-card-text v-else>
         {{ $t('common:plugins.geometrySearch.results.none') }}
       </v-card-text>
@@ -74,41 +16,20 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { mapGetters, mapActions } from 'vuex'
-import ResultInfo from '../../../components/ResultInfo.vue'
-import { TreeViewItem } from '../types'
+import { mapGetters } from 'vuex'
 import DrawMode from './DrawMode.vue'
 import ViewToggle from './ViewToggle.vue'
-import Action from './Action.vue'
+import TreeView from './Tree.vue'
 
 export default Vue.extend({
   name: 'GeometrySearchPlugin',
   components: {
     DrawMode,
-    Action,
+    TreeView,
     ViewToggle,
-    ResultInfo,
   },
   computed: {
     ...mapGetters('plugin/geometrySearch', ['treeViewItems']),
-  },
-  methods: {
-    ...mapActions('plugin/geometrySearch', [
-      'changeActiveData',
-      'fullSearchOnToponym',
-      'fullSearchLiterature',
-    ]),
-    tooltipKey(item: TreeViewItem) {
-      return `plugins.geometrySearch.tooltip.badge.${
-        item.type === 'toponym' && item.children
-          ? 'toponymToText'
-          : item.type === 'toponym' && !item.children
-          ? 'toponymInText'
-          : item.children
-          ? 'textToToponym'
-          : 'textInToponym'
-      }`
-    },
   },
 })
 </script>
@@ -121,15 +42,6 @@ export default Vue.extend({
   pointer-events: all;
   min-width: 400px;
   white-space: normal;
-
-  .text-locator-result-badge {
-    cursor: pointer;
-    max-width: 500px;
-    width: 500px;
-    white-space: normal;
-    justify-content: left;
-    margin: 0.5em 0;
-  }
 }
 </style>
 
