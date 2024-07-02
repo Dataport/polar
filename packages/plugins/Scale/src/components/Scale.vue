ab5c0499dfabfedc3553626d77126b39739c2e7b
bruehlca <carina.bruehl@dataport.de>
2024-07-02 16:51:55 +0200

    add scale switcher to scale plugin

--- packages/plugins/Scale/src/components/Scale.vue
+++ packages/plugins/Scale/src/components/Scale.vue
@@ -4,7 +4,17 @@
     :title="$t('common:plugins.scale.label')"
     :aria-label="$t('common:plugins.scale.label')"
   >
-    <span class="scale-as-a-ratio">
+    <select
+      v-if="scales.length > 0"
+      v-model="scale"
+      class="scale-as-a-ratio"
+      @change="setResolutionByIndex($event.target.selectedIndex)"
+    >
+      <option v-for="(scaleOption, i) in scales" :key="i" :value="scaleOption">
+        {{ scalesToOne(scaleOption.scale) }}
+      </option>
+    </select>
+    <span v-else class="scale-as-a-ratio">
       {{ scaleToOne }}
     </span>
     <span class="scale-line">
@@ -15,12 +25,61 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { mapGetters } from 'vuex'
+import { mapGetters, mapMutations, mapActions } from 'vuex'
+import { mapConfiguration } from '../../../../clients/dish/src/mapConfig'
+import thousandsSeparator from '../utils/thousandsSeperator'
 
 export default Vue.extend({
   name: 'PolarScale',
+  data() {
+    return {
+      scales: [] as { scale: number; zoomLevel: number }[],
+    }
+  },
   computed: {
-    ...mapGetters('plugin/scale', ['scaleToOne', 'scaleWithUnit']),
+    ...mapGetters({
+      scaleToOne: 'plugin/scale/scaleToOne',
+      scaleValue: 'plugin/scale/scaleValue',
+      scaleWithUnit: 'plugin/scale/scaleWithUnit',
+      getMapView: 'map/getMapView',
+    }),
+    scalesToOne() {
+      return (scale: number) => '1 : ' + thousandsSeparator(scale)
+    },
+    scale: {
+      get() {
+        let scaleToCompare = this.scaleValue
+
+        if (scaleToCompare > 10000) {
+          scaleToCompare = Math.round(scaleToCompare / 500) * 500
+        } else if (scaleToCompare > 1000) {
+          scaleToCompare = Math.round(scaleToCompare / 50) * 50
+        }
+        return this.scales[
+          this.scales.findIndex((s) => s.scale === scaleToCompare)
+        ]
+      },
+      set(value: number) {
+        this.setScaleValue(value)
+      },
+    },
+  },
+  created() {
+    this.scales = mapConfiguration.options.map((option) => ({
+      scale: option.scale,
+      zoomLevel: option.zoomLevel,
+    }))
+  },
+  methods: {
+    ...mapMutations({
+      setScaleValue: 'plugin/scale/setScaleValue',
+    }),
+    ...mapActions({
+      setZoomLevel: 'plugin/zoom/setZoomLevel',
+    }),
+    setResolutionByIndex(index: number) {
+      this.setZoomLevel(this.scales[index].zoomLevel)
+    },
   },
 })
 </script>
@@ -35,7 +94,8 @@ export default Vue.extend({
   gap: 4px;
   margin: 4px;
 
-  > span {
+  > span,
+  > select {
     background: #ffffffcc;
     border-radius: 2px;
     border-width: 0 2px 2px 2px;
