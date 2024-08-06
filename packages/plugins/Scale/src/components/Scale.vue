c8e699933af55280a30c40125e1bcbbcec2e363d
bruehlca <carina.bruehl@dataport.de>
2024-08-06 09:19:56 +0200

    adjust styling

--- packages/plugins/Scale/src/components/Scale.vue
+++ packages/plugins/Scale/src/components/Scale.vue
@@ -122,13 +122,13 @@ export default Vue.extend({
 
   .scale-switcher {
     cursor: pointer;
-    font-weight: bold;
     background: #003064;
     color: #ffffff;
     border-radius: 5px;
     box-shadow: 0 2px 4px -1px rgba(0, 0, 0, 0.2),
       0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 1px 10px 0 rgba(0, 0, 0, 0.12);
     border: 3px solid transparent;
+    box-sizing: border-box;
   }
 
   .scale-switcher:hover,
