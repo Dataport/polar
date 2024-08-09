bb1604797cd39b7dcf41d6c66348175af7eb5df6
bruehlca <carina.bruehl@dataport.de>
2024-08-09 10:22:56 +0200

    fix wrongly deleted property

--- packages/plugins/Scale/src/index.ts
+++ packages/plugins/Scale/src/index.ts
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import { ScaleConfiguration } from '@polar/lib-custom-types'
+import { Scale } from './components'
 import language from './language'
 import { makeStoreModule } from './store'
 
 export default (options: ScaleConfiguration) => (instance: Vue) =>
   instance.$store.dispatch('addComponent', {
     name: 'scale',
+    plugin: Scale,
     language,
     storeModule: makeStoreModule(),
     options,
