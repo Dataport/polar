b0ef2d84424875d5e78ccd2cd1641a09c7347857
jedi-of-the-sea <jedi-of-the-sea@users.noreply.github.com>
2024-08-14 13:09:39 +0000

    deploy: 766b681e8ca4f74882122174447a448236b684bd

--- node_modules/@polar/client-generic/node_modules/@polar/plugin-gfi/node_modules/@polar/lib-test-mount-parameters/node_modules/@polar/lib-custom-types/core.ts
+++ node_modules/@polar/client-generic/node_modules/@polar/plugin-gfi/node_modules/@polar/lib-test-mount-parameters/node_modules/@polar/lib-custom-types/core.ts
@@ -431,6 +431,11 @@ export type ToastTypeStyles = {
 /** configuration for toast type styles */
 export type ToastConfiguration = PluginOptions & ToastTypeStyles
 
+export interface ScaleConfiguration extends PluginOptions {
+  showScaleSwitcher?: boolean
+  zoomMethod?: string
+}
+
 export interface ZoomConfiguration extends PluginOptions {
   renderType?: RenderType
   showMobile?: boolean
@@ -592,6 +597,7 @@ export interface MapConfig {
   iconMenu?: IconMenuConfiguration
   pins?: PinsConfiguration
   reverseGeocoder?: ReverseGeocoderConfiguration
+  scale?: ScaleConfiguration
   toast?: ToastConfiguration
   zoom?: ZoomConfiguration
 }
