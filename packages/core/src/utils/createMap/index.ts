be48a73725976dabc0ea1b5c19ae31abd5db0d2e
Pascal Roehling <pas.roehling@gmail.com>
2024-08-19 19:57:43 +0200

    Add another projection to the defaults of namedProjection

--- packages/core/src/utils/createMap/index.ts
+++ packages/core/src/utils/createMap/index.ts
@@ -59,6 +59,10 @@ export default async function createMap({
         'EPSG:31467',
         '+proj=tmerc +lat_0=0 +lon_0=9 +k=1 +x_0=3500000 +y_0=0 +ellps=bessel +nadgrids=BETA2007.gsb +units=m +no_defs +type=crs',
       ],
+      [
+        'EPSG:4647',
+        '+proj=tmerc +lat_0=0 +lon_0=9 +k=0.9996 +x_0=32500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
+      ],
     ],
     startResolution: 15.874991427504629,
   }
