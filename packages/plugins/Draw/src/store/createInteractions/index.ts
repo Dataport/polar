3146f3c78decb6eb4a427574aa8d926f2cf94264
Pascal Roehling <pas.roehling@gmail.com>
2024-09-03 16:46:00 +0200

    Configure Draw directly in createInteractions and use local getter

--- packages/plugins/Draw/src/store/createInteractions/index.ts
+++ packages/plugins/Draw/src/store/createInteractions/index.ts
@@ -1,15 +1,22 @@
 import Interaction from 'ol/interaction/Interaction'
 import { PolarActionContext } from '@polar/lib-custom-types'
+import { Draw, Snap } from 'ol/interaction'
 import { CreateInteractionsPayload, DrawGetters, DrawState } from '../../types'
+import createDrawStyle from '../../utils/createDrawStyle'
 import createDeleteInteractions from './createDeleteInteractions'
 import createTextInteractions from './createTextInteractions'
-import createDrawInteractions from './createDrawInteractions'
 
 export default function (
   {
     dispatch,
-    getters: { drawMode, mode, textInput, textSize, selectedStrokeColor },
-    rootGetters: { configuration },
+    getters: {
+      configuration,
+      drawMode,
+      mode,
+      textInput,
+      textSize,
+      selectedStrokeColor,
+    },
   }: PolarActionContext<DrawState, DrawGetters>,
   { drawSource, drawLayer }: CreateInteractionsPayload
 ): Interaction[] | Promise<Interaction[]> {
@@ -19,15 +26,23 @@ export default function (
         textInput,
         textSize,
         drawSource,
-        configuration.draw
+        configuration
       )
     }
-    return createDrawInteractions(
-      selectedStrokeColor,
-      drawSource,
+    const style = createDrawStyle(
       drawMode,
-      configuration.draw
+      selectedStrokeColor,
+      configuration?.style
     )
+
+    const draw = new Draw({
+      source: drawSource,
+      type: drawMode,
+      style,
+    })
+    draw.on('drawend', (e) => e.feature.setStyle(style))
+
+    return [draw, new Snap({ source: drawSource })]
   } else if (mode === 'edit') {
     return dispatch('createModifyInteractions', { drawSource, drawLayer })
   } else if (mode === 'delete') {
