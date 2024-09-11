79cac2a1ed4e4e60413dc396fc1f09cd1af0a4e1
bruehlca <carina.bruehl@dataport.de>
2024-09-11 15:59:04 +0200

    simplify getter zoomLevel

--- packages/plugins/GeoLocation/src/store/getters.ts
+++ packages/plugins/GeoLocation/src/store/getters.ts
@@ -38,11 +38,7 @@ const getters: PolarGetterTree<GeoLocationState, GeoLocationGetters> = {
     return rootGetters.configuration?.geoLocation?.toastAction
   },
   zoomLevel: (_, __, ___, rootGetters): number => {
-    const zoomLevel = rootGetters.configuration?.geoLocation?.zoomLevel
-    if (zoomLevel !== undefined && zoomLevel !== null) {
-      return zoomLevel
-    }
-    return 7
+    return rootGetters.configuration?.geoLocation?.zoomLevel ?? 7
   },
   geoLocationMarkerLayer(_, __, ___, rootGetters) {
     return rootGetters?.map
