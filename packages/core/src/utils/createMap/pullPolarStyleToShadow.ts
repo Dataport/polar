ec6da0729a35da563e46354256d0d6dd1275fffa
Pascal Roehling <pas.roehling@gmail.com>
2024-09-12 19:45:39 +0200

    Revert some changes to have dev scripts working again
    stylePath is only a parameter needed for production builds, dev builds
    use the previous method that is provided by vite.

--- packages/core/src/utils/createMap/pullPolarStyleToShadow.ts
+++ packages/core/src/utils/createMap/pullPolarStyleToShadow.ts
@@ -4,17 +4,26 @@ export const pullPolarStyleToShadow = (
   shadowRoot: ShadowRoot,
   stylePath = defaultStylePath
 ) => {
-  const link = document.createElement('link')
-  link.href = stylePath
-  link.rel = 'stylesheet'
-  link.onerror = (e) =>
-    console.error(
-      `@polar/core: Couldn't find required stylesheets, map won't render. ${
-        stylePath === defaultStylePath
-          ? `The default stylePath ${defaultStylePath} did not work. Please check @polar/core's documentation of 'stylePath' on how this can be configured.`
-          : `It seems the provided stylePath (${stylePath}) did not work. Please verify its correctness. The value should be given as it would be correct in a link tag's href.`
-      }`,
-      e
+  // @ts-expect-error | 'TS2339: Property 'env' does not exist on type 'ImportMeta'.' - It does since we're using vite as a bundler.
+  if (import.meta.env.DEV) {
+    const stylesheets = [...document.getElementsByTagName('style')]
+    const polarStylesheets = stylesheets.filter((el) =>
+      el.getAttribute('data-vite-dev-id')
     )
-  shadowRoot.appendChild(link)
+    polarStylesheets.forEach((style) => shadowRoot.appendChild(style))
+  } else {
+    const link = document.createElement('link')
+    link.href = stylePath
+    link.rel = 'stylesheet'
+    link.onerror = (e) =>
+      console.error(
+        `@polar/core: Couldn't find required stylesheets, map won't render. ${
+          stylePath === defaultStylePath
+            ? `The default stylePath ${defaultStylePath} did not work. Please check @polar/core's documentation of 'stylePath' on how this can be configured.`
+            : `It seems the provided stylePath (${stylePath}) did not work. Please verify its correctness. The value should be given as it would be correct in a link tag's href.`
+        }`,
+        e
+      )
+    shadowRoot.appendChild(link)
+  }
 }
