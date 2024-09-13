1677a49f00b629ca3ade8caa53f24f26e4d600d0
Pascal Röhling <73653210+dopenguin@users.noreply.github.com>
2024-09-13 11:38:35 +0200

    Remove OS check
    Co-authored-by: Dennis Sen <108349707+warm-coolguy@users.noreply.github.com>

--- scripts/buildPages.sh
+++ scripts/buildPages.sh
@@ -3,11 +3,7 @@ for i in "${array[@]}"
 do
   echo "Building $i docs ..."
 	npm run docs:$i
-  if [[ "$OSTYPE" == "darwin"* ]]; then
-    mkdir -p ./pages/docs/$i
-  else
-    mkdir --parents ./pages/docs/$i
-  fi
+  mkdir -p ./pages/docs/$i
   mv ./packages/clients/$i/docs/* ./pages/docs/$i
 done
 echo "All docs built."
