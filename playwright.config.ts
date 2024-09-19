b223b60dc58000a54760c700490fc1816dae4c61
Dennis Sen <dennis.sen@dataport.de>
2024-09-19 12:14:57 +0200

    add webserver the playwright way

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,13 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
+  webServer: {
+    command: 'npm run snowbox:dev',
+    url: 'http://localhost:1234',
+    reuseExistingServer: !process.env.CI,
+    stdout: 'ignore',
+    stderr: 'pipe',
+  },
 
   /* Configure projects for major browsers */
   projects: [
