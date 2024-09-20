33e3c94bcddc0aadc962db626eb5e2e2599d61b6
Dennis Sen <dennis.sen@dataport.de>
2024-09-20 08:52:18 +0200

    add/update e2e tests

--- e2e/zoom.spec.ts
+++ e2e/zoom.spec.ts
@@ -2,11 +2,13 @@ import { test, expect } from '@playwright/test'
 import { openSnowbox } from './utils/openSnowbox'
 import { clickTimes } from './utils/clickTimes'
 
+const zoomTargetId = '#vuex-target-zoom'
+
 test('zoom in button zooms in until max zoom', async ({ page }) => {
   const zoomInLabel = 'Zoom in'
-  openSnowbox(page)
+  await openSnowbox(page)
 
-  const zoomTarget = page.locator('#vuex-target-zoom')
+  const zoomTarget = page.locator(zoomTargetId)
   await expect(zoomTarget).toHaveText('2')
   await clickTimes({ page, value: zoomInLabel, times: 7 })
   await expect(zoomTarget).toHaveText('9')
@@ -15,9 +17,9 @@ test('zoom in button zooms in until max zoom', async ({ page }) => {
 
 test('zoom out button zooms out until min zoom', async ({ page }) => {
   const zoomOutLabel = 'Zoom out'
-  openSnowbox(page)
+  await openSnowbox(page)
 
-  const zoomTarget = page.locator('#vuex-target-zoom')
+  const zoomTarget = page.locator(zoomTargetId)
   await expect(zoomTarget).toHaveText('2')
   await clickTimes({ page, value: zoomOutLabel, times: 2 })
   await expect(zoomTarget).toHaveText('0')
