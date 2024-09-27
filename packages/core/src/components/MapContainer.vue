a8e8da21159de77c5ae564b9afb963862f682fe0
Pascal Roehling <pas.roehling@gmail.com>
2024-09-27 14:01:52 +0200

    Move computed variable to be a const

--- packages/core/src/components/MapContainer.vue
+++ packages/core/src/components/MapContainer.vue
@@ -58,6 +58,8 @@ import MapUi from './MapUi.vue'
 // NOTE: OpenLayers styles need to be imported as the map resides in the shadow DOM
 import 'ol/ol.css'
 
+const isMacOS = navigator.userAgent.indexOf('Mac') !== -1
+
 export default Vue.extend({
   components: {
     MapUi,
@@ -91,13 +93,8 @@ export default Vue.extend({
       'moveHandle',
       'moveHandleActionButton',
     ]),
-    isMacOS() {
-      return navigator.userAgent.indexOf('Mac') !== -1
-    },
     overlayLocale() {
-      return `common:overlay.${
-        this.isMacOS ? 'noCommandOnZoom' : 'noControlOnZoom'
-      }`
+      return `common:overlay.${isMacOS ? 'noCommandOnZoom' : 'noControlOnZoom'}`
     },
     renderMoveHandle() {
       return (
@@ -213,7 +210,7 @@ export default Vue.extend({
     },
     wheelEffect(event: WheelEvent) {
       clearTimeout(this.noControlOnZoomTimeout)
-      this.noControlOnZoom = this.isMacOS ? !event.metaKey : !event.ctrlKey
+      this.noControlOnZoom = isMacOS ? !event.metaKey : !event.ctrlKey
       this.noControlOnZoomTimeout = setTimeout(
         () => (this.noControlOnZoom = false),
         2000
