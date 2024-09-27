1c1e51eef2d8defea198c32aac3d2c1ecd4e3a30
bruehlca <carina.bruehl@dataport.de>
2024-09-27 16:42:46 +0200

    WIP

--- packages/clients/dish/src/polar-client.ts
+++ packages/clients/dish/src/polar-client.ts
@@ -10,35 +10,32 @@ import './styles.css'
 // eslint-disable-next-line no-console
 console.log(`DISH map client running in version ${packageInfo.version}.`)
 
-addPlugins(client)
+export default {
+  createMap: async ({ containerId, mode }) => {
+    addPlugins(client, mode)
+    client.rawLayerList.initializeLayerList(layerConf)
 
-const containerId = 'polarstern'
+    const instance = await client.createMap({
+      containerId,
+      mapConfiguration: {
+        ...mapConfiguration,
+        layerConf,
+      },
+    })
 
-async function initializeClient() {
-  client.rawLayerList.initializeLayerList(layerConf)
+    const parameters = new URL(document.location as unknown as string)
+      .searchParams
+    // using naming from backend to avoid multiple names for same thing
+    const objektId = parameters.get('ObjektID')
 
-  const instance = await client.createMap({
-    containerId,
-    mapConfiguration: {
-      ...mapConfiguration,
-      layerConf,
-    },
-  })
+    if (typeof objektId === 'string') {
+      navigateToDenkmal(instance, objektId)
+    }
 
-  const parameters = new URL(document.location as unknown as string)
-    .searchParams
-  // using naming from backend to avoid multiple names for same thing
-  const objektId = parameters.get('ObjektID')
-
-  if (typeof objektId === 'string') {
-    navigateToDenkmal(instance, objektId)
-  }
-
-  // @ts-expect-error | intentionally expand window; no environment affected
-  window.openBenutzungshinweise = function () {
-    instance.$store.commit('plugin/modal/setContent', CONTENT_ENUM.HINTS)
-    instance.$store.commit('plugin/modal/setClosed', false)
-  }
+    // @ts-expect-error | intentionally expand window; no environment affected
+    window.openBenutzungshinweise = function () {
+      instance.$store.commit('plugin/modal/setContent', CONTENT_ENUM.HINTS)
+      instance.$store.commit('plugin/modal/setClosed', false)
+    }
+  },
 }
-
-initializeClient()
