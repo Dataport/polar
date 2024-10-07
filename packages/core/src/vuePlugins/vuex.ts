4c76ed581b2a7b76f6091ed775d60d3ba80a1053
Dennis Sen <dennis.sen@dataport.de>
2024-10-07 10:59:05 +0200

    add configuration error warning

--- packages/core/src/vuePlugins/vuex.ts
+++ packages/core/src/vuePlugins/vuex.ts
@@ -241,6 +241,12 @@ export const makeStore = () => {
         }
         if (state.configuration[name].displayComponent) {
           commit('setComponents', [...components, component])
+
+          if (!state.configuration[name].layoutTag) {
+            console.warn(
+              `Component "${name}" was registered as visible ('displayComponent' had a truthy value), but no layout tag was associated. This may be an error in configuration and will lead to the component not being visible in the UI.`
+            )
+          }
         }
       },
       centerOnFeature({ rootGetters: { map } }, feature: Feature) {
