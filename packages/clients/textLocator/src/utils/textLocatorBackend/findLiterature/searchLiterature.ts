35484e658fa5fc9e6633f1ffe1697358ce7c253f
Dennis Sen <dennis.sen@dataport.de>
2024-10-11 08:01:14 +0200

    merge duplicate locales

--- packages/clients/textLocator/src/utils/textLocatorBackend/findLiterature/searchLiterature.ts
+++ packages/clients/textLocator/src/utils/textLocatorBackend/findLiterature/searchLiterature.ts
@@ -52,7 +52,7 @@ export function searchLiterature(
         console.error(error)
         this.dispatch('plugin/toast/addToast', {
           type: 'warning',
-          text: 'common:textLocator.error.findLiterature',
+          text: 'common:textLocator.error.search',
         })
       }
 
