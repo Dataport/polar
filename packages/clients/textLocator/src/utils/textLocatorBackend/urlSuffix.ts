5a80d9b72ae8e8ec3a1b02507417d45c5e7fee42
Dennis Sen <108349707+warm-coolguy@users.noreply.github.com>
2024-10-11 08:15:54 +0200

    Update packages/clients/textLocator/src/utils/textLocatorBackend/urlSuffix.ts
    apply suggestion "remove outdated TODO"
    
    Co-authored-by: Pascal Röhling <73653210+dopenguin@users.noreply.github.com>

--- packages/clients/textLocator/src/utils/textLocatorBackend/urlSuffix.ts
+++ packages/clients/textLocator/src/utils/textLocatorBackend/urlSuffix.ts
@@ -1,6 +1,5 @@
 export default {
   findDocumentsByTitle: '/findDocumentsByTitle',
-  // NOTE unused atm; POST {"id": documentId } => TitleLocationFrequency
   locationsToText: '/locationsToText',
   lookupLocation: '/lookup/location',
 }
