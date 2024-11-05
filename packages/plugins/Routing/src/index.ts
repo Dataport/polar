4790e4011b029250310e57456cd1b2696a25e1bb
datKaMa <katharina.mathea@dataport.de>
2024-11-05 10:11:56 +0100

    updated language file added generateInputQuery and added rendertype

--- packages/plugins/Routing/src/index.ts
+++ packages/plugins/Routing/src/index.ts
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import { RoutingConfiguration } from '@polar/lib-custom-types'
 import { makeStoreModule } from './store'
+import { Routing } from './components'
+import language from './language'
 
-export default (options: RoutingConfiguration) => (instance: Vue) =>
-  instance.$store.dispatch('addComponent', {
+export default (options: RoutingConfiguration) => (instance: Vue) => {
+  console.error('mol kieken', options)
+  return instance.$store.dispatch('addComponent', {
     name: 'routing',
+    plugin: Routing,
+    language,
     storeModule: makeStoreModule(),
     options,
   })
+}
