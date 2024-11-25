42b5617a907d7624e37f047ef12b61424b4d1836
Pascal Roehling <pas.roehling@gmail.com>
2024-11-25 12:42:55 +0100

    Add another script to hopefully fix issue with publishing

---
+++ scripts/publishPackages.js
@@ -0,0 +1,19 @@
+/* eslint-env node */
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const cp = require('child_process')
+
+const packages = process.argv.slice(2).map((tag) => tag.split('@')[0])
+
+for (const packageName of packages) {
+  try {
+    cp.execSync(
+      `npx nx release publish --projects ${packageName} --verbose false`,
+      { stdio: ['pipe', 'ignore', 'pipe'] }
+    )
+  } catch (e) {
+    console.error(e)
+    process.exitCode = 1
+  }
+}
