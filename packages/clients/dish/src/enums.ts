f412420c6b92c7b4dfb3360706f498923a7f8a73
bruehlca <carina.bruehl@dataport.de>
2024-12-20 09:41:17 +0100

    edit comments in MODE

--- packages/clients/dish/src/enums.ts
+++ packages/clients/dish/src/enums.ts
@@ -1,6 +1,6 @@
 export const MODE = {
   // display view for users that visit the map via URL
   EXTERN: 'EXTERN',
-  // display intern view for administrators that work within the map
+  // display internal view for the DISH application
   INTERN: 'INTERN',
 }
