24e386ba5b45962ecc56643acf89071dc84f1887
Pascal Roehling <pas.roehling@gmail.com>
2025-01-23 17:27:14 +0100

    Remove unused subscriptions and html elements

--- packages/clients/snowbox/src/polar-client.ts
+++ packages/clients/snowbox/src/polar-client.ts
@@ -23,8 +23,6 @@ const createMap = (layerConf) => {
       window.mapInstance = map
       addStoreSubscriptions(
         ['plugin/zoom/zoomLevel', 'vuex-target-zoom'],
-        ['plugin/measure/measure', 'vuex-target-measure'],
-        ['plugin/measure/selectedUnit', 'vuex-target-unit'],
         [
           'plugin/gfi/featureInformation',
           'vuex-target-gfi',
