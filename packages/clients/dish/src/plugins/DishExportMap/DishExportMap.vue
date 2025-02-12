9a16370f5c6911f41fc6650d4a8bc1405d4e31d9
bruehlca <carina.bruehl@dataport.de>
2025-02-12 16:05:14 +0100

    edit comment

--- packages/clients/dish/src/plugins/DishExportMap/DishExportMap.vue
+++ packages/clients/dish/src/plugins/DishExportMap/DishExportMap.vue
@@ -171,7 +171,7 @@ export default Vue.extend({
       return {
         NewTab: true,
         objektueberschrift: this.title,
-        // incorrect spelling because of backend requirements
+        // spelling is intentional because of backend requirements
         masssstab: this.scaleValue,
         printApproach: 'scale',
         printRequester: 'client',
