ddc43497d70be152b0354ad7f280566aade407b1
Dennis Sen <dennis.sen@dataport.de>
2025-02-14 12:17:21 +0100

    add warning on snap layers w/o vector source

--- packages/plugins/Draw/src/store/createInteractions/getSnaps.ts
+++ packages/plugins/Draw/src/store/createInteractions/getSnaps.ts
@@ -12,6 +12,10 @@ export const getSchnaps = (map: Map, snapIds: string[]): Snap[] =>
     )?.getSource?.()
     if (source) {
       accumulator.push(new Snap({ source }))
+    } else {
+      console.warn(
+        `@polar/plugin-draw: Layer with ID "${layerId}" configured for 'snapTo', but it has no source to snap to. The layer does probably not hold any vector data.`
+      )
     }
     return accumulator
   }, [] as Snap[])
