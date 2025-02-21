3811c559cc3450810a44c540671a2c8f8cf8170b
bruehlca <carina.bruehl@dataport.de>
2025-02-21 14:27:20 +0100

    add custom search

--- packages/clients/bgw/src/utils/badestellenSearch.ts
+++ packages/clients/bgw/src/utils/badestellenSearch.ts
@@ -1,10 +1,26 @@
-import { SelectResultFunction } from '@polar/lib-custom-types'
+import { Feature } from 'geojson'
+import {
+  CoreGetters,
+  CoreState,
+  PolarStore,
+  SelectResultFunction,
+} from '@polar/lib-custom-types'
 import {
   SearchResultSymbols,
   AddressSearchGetters,
   AddressSearchState,
 } from '@polar/plugin-address-search'
-import { Feature } from 'geojson'
+import { WfsParameters, getWfsFeatures } from '@polar/lib-get-features'
+
+export function badestellenSearch(
+  this: PolarStore<CoreState, CoreGetters>,
+  signal: AbortSignal,
+  url: string,
+  inputValue: string,
+  queryParameters: WfsParameters
+) {
+  return getWfsFeatures(signal, url, inputValue.toUpperCase(), queryParameters)
+}
 
 export const badestellenSearchResult: SelectResultFunction<
   AddressSearchState,
