39d56e3cb17c1e21c78e36042e52b48bb27c70da
warm-coolguy <warm-coolguy@users.noreply.github.com>
2025-02-27 12:44:04 +0000

    deploy: 121e78975b8aade352dfebf6383d9c8ea89ce946

---
+++ docs/diplan/example/dev/setup.js
@@ -0,0 +1,162 @@
+/* eslint-disable max-lines-per-function */
+
+const geoJSON = {
+  type: 'FeatureCollection',
+  features: [
+    {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'Polygon',
+        coordinates: [
+          [
+            [553702.4519707427, 5926504.665153537],
+            [549799.849911481, 5938873.929307467],
+            [582674.3113259386, 5942313.510783426],
+            [572421.7126956752, 5930142.68402234],
+            [553702.4519707427, 5926504.665153537],
+          ],
+        ],
+      },
+    },
+  ],
+}
+
+/* this is an example setup function displaying how POLAR is instantiated
+ * you may do this in any other format as long as all required contents arrive
+ * in `initializeLayerList` and `createMap` */
+export default (client, layerConf, config) => {
+  /* The parameter may be a URL; in that case, a second parameter is a callback
+   * function that provides the `layerConf` object as first parameter.
+   * The code for that would look like this:
+   * client.rawLayerList.initializeLayerList("url", (layerConf) => {
+   *   client.createMap({...})
+   * })
+   * However, we're working with a loaded object here, making the
+   * masterportalapi's `initializeLayerList` synchronous:
+   */
+  client.rawLayerList.initializeLayerList(layerConf)
+
+  // TODO can we encapsulate do the upper statement in the core? people
+  //      don't really need to do that manually, do they?
+
+  client
+    .createMap({
+      // id of div to render in
+      containerId: 'polarstern',
+      /* see API.md for mapConfiguration options, especially the compiled
+       * version at https://dataport.github.io/polar/docs/diplan/client-diplan.html  */
+      mapConfiguration: {
+        stylePath: '../dist/polar-client.css',
+        layerConf,
+        ...config,
+      },
+    })
+    .then((mapInstance) => {
+      /*
+       * This is a binding example for the various data POLAR makes accessible.
+       * The example is not exhaustive and additional fields may be found in
+       * the API.md and nested documents in the compiled docs:
+       * https://dataport.github.io/polar/docs/diplan/client-diplan.html
+       */
+
+      // TODO expand example bindings
+
+      const actionPlus = document.getElementById('action-plus')
+      const actionMinus = document.getElementById('action-minus')
+      const actionToast = document.getElementById('action-toast')
+      const actionLoadGeoJson = document.getElementById('action-load-geojson')
+      const actionZoomToAll = document.getElementById('action-zoom-to-all')
+      const actionFillAddressSearch = document.getElementById(
+        'action-address-search-filler'
+      )
+      const actionLasso = document.getElementById('action-lasso')
+
+      actionPlus.onclick = () =>
+        mapInstance.$store.dispatch('plugin/zoom/increaseZoomLevel')
+      actionMinus.onclick = () =>
+        mapInstance.$store.dispatch('plugin/zoom/decreaseZoomLevel')
+      actionToast.onclick = () =>
+        mapInstance.$store.dispatch('plugin/toast/addToast', {
+          type: 'success',
+          text: 'Dies ist eine Beispielnachricht.',
+          timeout: 3000,
+        })
+      actionLoadGeoJson.onclick = () => {
+        mapInstance.$store.dispatch('plugin/draw/addFeatures', {
+          geoJSON,
+        })
+      }
+      actionZoomToAll.onclick = () =>
+        mapInstance.$store.dispatch('plugin/draw/zoomToAllFeatures', {
+          margin: 10, // defaults to 20
+        })
+      actionFillAddressSearch.addEventListener('input', (e) =>
+        mapInstance.$store.dispatch('plugin/addressSearch/search', {
+          input: e.target.value,
+          autoselect: 'first',
+        })
+      )
+      actionLasso.onclick = () =>
+        mapInstance.$store.dispatch('plugin/draw/setMode', 'lasso')
+
+      const htmlRevisedDrawExport = document.getElementById(
+        'subscribed-revised-draw-export'
+      )
+      const htmlRevisionInProgress = document.getElementById(
+        'subscribed-revision-in-progress'
+      )
+      const htmlSimpleGeometryValidity = document.getElementById(
+        'subscribed-simple-geometry-validity'
+      )
+      const htmlZoom = document.getElementById('subscribed-zoom')
+      const htmlGfi = document.getElementById('subscribed-gfi')
+      const htmlExportA = document.getElementById('subscribed-export-a')
+      const htmlExportImg = document.getElementById('subscribed-export-img')
+      const htmlDraw = document.getElementById('subscribed-draw')
+
+      mapInstance.subscribe(
+        'plugin/zoom/zoomLevel',
+        (zoomLevel) => (htmlZoom.innerHTML = zoomLevel)
+      )
+      mapInstance.subscribe(
+        'plugin/gfi/featureInformation',
+        (v) => (htmlGfi.innerHTML = JSON.stringify(v, null, 2))
+      )
+      mapInstance.subscribe('plugin/export/exportedMap', (screenshot) => {
+        htmlExportImg.setAttribute('src', screenshot)
+        if (navigator.userAgent.toLowerCase().includes('firefox')) {
+          htmlExportImg.onclick = function () {
+            window.open(this.src, '_blank')
+          }
+          htmlExportA.onclick = () => false
+        } else {
+          htmlExportA.setAttribute('href', screenshot)
+        }
+      })
+      mapInstance.subscribe('plugin/draw/featureCollection', (geojson) => {
+        htmlDraw.innerHTML = JSON.stringify(geojson, null, 2)
+      })
+      mapInstance.subscribe('diplan/revisedDrawExport', (revisedDrawExport) => {
+        htmlRevisedDrawExport.innerHTML = JSON.stringify(
+          revisedDrawExport,
+          null,
+          2
+        )
+      })
+      mapInstance.subscribe(
+        'diplan/revisionInProgress',
+        (revisionInProgress) => {
+          htmlRevisionInProgress.innerHTML = revisionInProgress
+        }
+      )
+      mapInstance.subscribe(
+        'diplan/simpleGeometryValidity',
+        (simpleGeometryValidity) => {
+          htmlSimpleGeometryValidity.innerHTML = simpleGeometryValidity
+        }
+      )
+
+      window.mapInstance = mapInstance
+    })
+}
---
+++ docs/diplan/example/polar-ui/config.js
@@ -0,0 +1,193 @@
+// service id map to avoid typos, ease renames
+const basemap = 'basemapde_farbe'
+const xplanwms = 'xplanwms'
+const xplanwfs = 'xplanwfs'
+const flurstuecke = 'flurstuecke'
+const bstgasleitung = 'bst_gasleitung'
+
+export default {
+  // masterportalAPI parameters
+  startResolution: 264.583190458,
+  startCenter: [561210, 5932600],
+  extent: [
+    248651.73157077, 5227198.20287631, 928366.12236557, 6118661.62507136,
+  ],
+  // diplan-specific configuration example (see API.md)
+  diplan: {
+    mergeToMultiGeometries: true,
+    validateGeoJson: true,
+    metaServices: [
+      {
+        id: flurstuecke,
+        propertyNames: ['land', 'gemarkung', 'regbezirk', 'kreis', 'gemeinde'],
+        aggregationMode: 'unequal',
+      },
+    ],
+  },
+  // general POLAR parameters
+  locales: [
+    {
+      type: 'de',
+      resources: {
+        diplan: {
+          layers: {
+            [basemap]: 'BasemapDE',
+            [xplanwms]: 'XPlanWMS',
+            [xplanwfs]: 'XPlanSynWFS',
+            [flurstuecke]: 'Flurstücke',
+            [bstgasleitung]: 'BST Gasleitung',
+          },
+          attributions: {
+            [basemap]: `$t(diplan.layers.${basemap}) © GeoBasis-DE / BKG <YEAR> CC BY 4.0`,
+            [xplanwms]: `$t(diplan.layers.${xplanwms}) © ???`,
+            [xplanwfs]: `$t(diplan.layers.${xplanwfs}) © ???`,
+            [flurstuecke]: `$t(diplan.layers.${flurstuecke}) © ???`,
+            [bstgasleitung]: `$t(diplan.layers.${bstgasleitung}) © ???`,
+          },
+        },
+      },
+    },
+    // TODO should we provide english locales?
+  ],
+  addressSearch: {
+    displayComponent: true,
+    searchMethods: [
+      {
+        queryParameters: {
+          searchAddress: true,
+          searchStreets: true,
+          searchHouseNumbers: true,
+        },
+        type: 'mpapi',
+        url: 'https://geodienste.hamburg.de/HH_WFS_GAGES?service=WFS&request=GetFeature&version=2.0.0',
+      },
+    ],
+    minLength: 3,
+    waitMs: 300,
+  },
+  layers: [
+    {
+      id: basemap,
+      visibility: true,
+      type: 'background',
+      name: `diplan.layers.${basemap}`,
+    },
+    {
+      id: xplanwms,
+      visibility: true,
+      type: 'mask',
+      name: `diplan.layers.${xplanwms}`,
+    },
+    {
+      id: xplanwfs,
+      visibility: false,
+      type: 'mask',
+      name: `diplan.layers.${xplanwfs}`,
+    },
+    {
+      id: flurstuecke,
+      visibility: false,
+      // TODO available from 7, but only starts loading from 8 - bug or skill issue?
+      minZoom: 7,
+      type: 'mask',
+      name: `diplan.layers.${flurstuecke}`,
+    },
+    {
+      id: bstgasleitung,
+      visibility: false,
+      type: 'mask',
+      name: `diplan.layers.${bstgasleitung}`,
+    },
+  ],
+  attributions: {
+    layerAttributions: [
+      {
+        id: basemap,
+        title: `diplan.attributions.${basemap}`,
+      },
+      {
+        id: xplanwms,
+        title: `diplan.attributions.${xplanwms}`,
+      },
+      {
+        id: xplanwfs,
+        title: `diplan.attributions.${xplanwfs}`,
+      },
+      {
+        id: flurstuecke,
+        title: `diplan.attributions.${flurstuecke}`,
+      },
+      {
+        id: bstgasleitung,
+        title: `diplan.attributions.${bstgasleitung}`,
+      },
+    ],
+  },
+  draw: {
+    enableOptions: true,
+    lassos: [
+      {
+        id: flurstuecke,
+      },
+      {
+        id: xplanwfs,
+      },
+    ],
+    measureOptions: {
+      metres: true,
+      kilometres: true,
+      hectares: true,
+    },
+    selectableDrawModes: ['Point', 'LineString', 'Circle', 'Text', 'Polygon'],
+    snapTo: [xplanwfs, flurstuecke],
+    textStyle: {
+      font: {
+        size: [10, 20, 30],
+        family: 'Arial',
+      },
+    },
+    style: {
+      fill: { color: 'rgb(51 117 212 / 50%)' },
+      stroke: {
+        color: '#3375d4',
+        width: 2,
+      },
+      circle: {
+        radius: 7,
+        fillColor: 'rgb(51 117 212 / 50%)',
+      },
+    },
+  },
+  export: {
+    displayComponent: true,
+    showJpg: false,
+    showPdf: false,
+  },
+  gfi: {
+    mode: 'bboxDot',
+    layers: {
+      [xplanwms]: {
+        geometry: true,
+        window: true,
+        properties: ['xpPlanName'],
+      },
+      [xplanwfs]: {
+        geometry: true,
+        window: true,
+        properties: ['name'],
+      },
+    },
+    coordinateSources: [
+      'plugin/pins/transformedCoordinate',
+      'plugin/pins/coordinatesAfterDrag',
+    ],
+  },
+  pins: {
+    toZoomLevel: 9,
+    movable: 'drag',
+    appearOnClick: {
+      show: true,
+      atZoomLevel: 0,
+    },
+  },
+}
