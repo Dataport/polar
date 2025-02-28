d232c50f55ff906da80798cefee0a59a1365929c
Dennis Sen <dennis.sen@dataport.de>
2025-02-28 06:17:47 +0100

    add snap operations, add function outline

--- packages/clients/diplan/src/store/geoEditing/cutPolygons.ts
+++ packages/clients/diplan/src/store/geoEditing/cutPolygons.ts
@@ -1,17 +1,44 @@
 import { PolarActionContext } from '@polar/lib-custom-types'
 import Draw from 'ol/interaction/Draw'
+import { getSnaps } from '@polar/plugin-draw'
+import Snap from 'ol/interaction/Snap'
+import VectorSource from 'ol/source/Vector'
+import Feature from 'ol/Feature'
 import { DiplanGetters, DiplanState } from '../../types'
 
 export const cutPolygons = ({
   dispatch,
+  rootGetters,
 }: PolarActionContext<DiplanState, DiplanGetters>) => {
   dispatch('plugin/draw/setMode', 'none', { root: true })
 
+  const drawSource: VectorSource = rootGetters['plugin/draw/drawSource']
+
   const draw = new Draw({ type: 'LineString' })
 
-  // TODO draw.on drawend → get draw source, modify polygons
+  draw.on('drawend', (e) => {
+    const cutLine = e.feature
+    const features = drawSource.getFeatures()
+    const nextFeatures = features.reduce((accumulator, feature) => {
+      // TODO cut feature with line if it's a polygon, add multiple features thereafter
+      console.error(cutLine)
+      accumulator.push(feature)
+      return accumulator
+    }, [] as Feature[])
+    drawSource.clear()
+    drawSource.addFeatures(nextFeatures)
+  })
 
-  // TODO get Snap interactions
-
-  dispatch('plugin/draw/setInteractions', [draw], { root: true })
+  dispatch(
+    'plugin/draw/setInteractions',
+    [
+      draw,
+      ...getSnaps(
+        rootGetters.map,
+        rootGetters.configuration?.draw?.snapTo || []
+      ),
+      new Snap({ source: drawSource }),
+    ],
+    { root: true }
+  )
 }
