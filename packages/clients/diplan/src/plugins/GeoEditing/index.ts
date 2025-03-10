19b643157fef534786a84013022c3ab5250e4d3b
Pascal Roehling <pas.roehling@gmail.com>
2025-03-10 18:44:44 +0100

    Comment unused locales

--- packages/clients/diplan/src/plugins/GeoEditing/index.ts
+++ packages/clients/diplan/src/plugins/GeoEditing/index.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import { PluginOptions } from '@polar/lib-custom-types'
 import { GeoEditing } from './components'
-import locales from './locales'
+// import locales from './locales'
 // import { makeStoreModule } from './store'
 
 // TODO: Are additional configuration parameters needed?
