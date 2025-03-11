49ab19b369e77a2d839c306b9bda4fbf9de8a917
Pascal Roehling <pas.roehling@gmail.com>
2025-03-11 20:40:46 +0100

    Reorder types alphanumerically

--- packages/plugins/Attributions/src/types.ts
+++ packages/plugins/Attributions/src/types.ts
@@ -1,18 +1,18 @@
 import { Attribution } from '@polar/lib-custom-types'
 
 export interface AttributionsState {
-  layer: string[]
   attributions: Attribution[]
+  layer: string[]
   // Parameter is only relevant if the attributions plugin controls the toggling of the window
   windowIsOpen: boolean
 }
 
 export interface AttributionsGetters extends AttributionsState {
+  initiallyOpen: boolean
   /** Array on store paths to listen to for changes */
   listenToChanges: string[]
   mapInfo: string[]
   mapInfoIcon: string
-  initiallyOpen: boolean
   renderType: 'independent' | 'iconMenu' | 'footer'
   staticAttributions: string[]
   windowWidth: number
