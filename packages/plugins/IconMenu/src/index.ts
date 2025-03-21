2644e6fcfcf23615386ab5133871c39a6930450d
Pascal Roehling <pas.roehling@gmail.com>
2025-03-21 20:30:49 +0100

    Remove IconMenuWrapper.vue

--- packages/plugins/IconMenu/src/index.ts
+++ packages/plugins/IconMenu/src/index.ts
@@ -4,9 +4,6 @@ import { IconMenu } from './components'
 import locales from './locales'
 import { makeStoreModule } from './store'
 
-import IconMenuWrapper from './components/IconMenuWrapper.vue'
-export { IconMenuWrapper }
-
 export default (options: IconMenuConfiguration) => (instance: Vue) =>
   instance.$store.dispatch('addComponent', {
     name: 'iconMenu',
