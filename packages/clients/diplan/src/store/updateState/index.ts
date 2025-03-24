9fc18179a0d9efe798be90333f6e75ebbe30fc45
Dennis Sen <dennis.sen@dataport.de>
2025-03-24 07:59:36 +0100

    add minimalistic edge case handling

--- packages/clients/diplan/src/store/updateState/index.ts
+++ packages/clients/diplan/src/store/updateState/index.ts
@@ -15,6 +15,33 @@ import { enrichWithMetaServices } from './enrichWithMetaServices'
 let abortController: AbortController | null = null
 export const drawFeatureCollectionSource = 'plugin/draw/featureCollection'
 
+const reviseFeatureCollection = (
+  sourceFeatureCollection: FeatureCollection<GeometryType>
+) => {
+  let revisedFeatureCollection = cloneFeatureCollection(sourceFeatureCollection)
+
+  revisedFeatureCollection = {
+    ...revisedFeatureCollection,
+    features: revisedFeatureCollection.features.reduce(
+      (accumulator, feature) => {
+        if (['Polygon', 'MultiPolygon'].includes(feature.geometry.type)) {
+          accumulator.push(
+            ...unkinkPolygon(
+              cleanCoords(feature) as GeoJsonFeature<Polygon | MultiPolygon>
+            ).features
+          )
+        } else {
+          accumulator.push(cleanCoords(feature))
+        }
+        return accumulator
+      },
+      [] as GeoJsonFeature<GeometryType>[]
+    ),
+  }
+
+  return revisedFeatureCollection
+}
+
 // FeatureCollection is compatible to stupid clone
 export const cloneFeatureCollection = (
   // No GeometryCollection from Draw, hence the <GeometryType>
@@ -38,27 +65,15 @@ export const updateState = async ({
   const thisController = (abortController = new AbortController())
 
   // clone to prevent accidentally messing with the draw tool's data
-  let revisedFeatureCollection = cloneFeatureCollection(
-    rootGetters[drawFeatureCollectionSource]
-  )
-
-  revisedFeatureCollection = {
-    ...revisedFeatureCollection,
-    features: revisedFeatureCollection.features.reduce(
-      (accumulator, feature) => {
-        if (['Polygon', 'MultiPolygon'].includes(feature.geometry.type)) {
-          accumulator.push(
-            ...unkinkPolygon(
-              cleanCoords(feature) as GeoJsonFeature<Polygon | MultiPolygon>
-            ).features
-          )
-        } else {
-          accumulator.push(cleanCoords(feature))
-        }
-        return accumulator
-      },
-      [] as GeoJsonFeature<GeometryType>[]
-    ),
+  let revisedFeatureCollection
+  try {
+    revisedFeatureCollection = reviseFeatureCollection(
+      rootGetters[drawFeatureCollectionSource]
+    )
+  } catch {
+    // if this revision fails, errors are catastrophic and unfixable
+    commit('setSimpleGeometryValidity', false)
+    commit('setRevisionInProgress', false)
   }
 
   // merge first; relevant for both follow-up steps
