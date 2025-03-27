529de4aa3b1c8581a2f0db5e479e3db3030260cb
Dennis Sen <dennis.sen@dataport.de>
2025-03-27 13:30:29 +0100

    pull e2e boilerplate code to draw function

--- packages/clients/diplan/e2e/draw.spec.ts
+++ packages/clients/diplan/e2e/draw.spec.ts
@@ -1,6 +1,7 @@
 // it is what it is – a test file, specifically
 /* eslint-disable max-lines-per-function */
 import { test } from '@playwright/test'
+import { draw } from '../../../../e2e/utils/draw'
 import { openDiPlan } from './utils/openDiplan'
 
 const drawTargetId = 'subscribed-draw'
@@ -8,44 +9,22 @@ const drawTargetId = 'subscribed-draw'
 test('drawn polygons can be cut', async ({ page }) => {
   await openDiPlan(page)
 
-  const canvas = await page.locator('canvas')
-  const boundingBox = await canvas.boundingBox()
-  if (boundingBox === null) throw new Error('Canvas not found.')
-  const { width, height } = boundingBox
-  let { x, y } = boundingBox
-
-  x += width / 2
-  y += height / 2
-
   await page.getByLabel('Digitalisierungswerkzeuge').click()
   await page.getByLabel('Neue Fläche einzeichnen').click()
 
-  // canvas not fully visible initially
-  await canvas.scrollIntoViewIfNeeded()
-  const scrollY = await page.evaluate(() => window.scrollY)
-  y -= scrollY
-
-  const moves: [number, number, string][] = [
+  await draw(page, [
     [-40, 0, 'click'],
     [40, -40, 'click'],
     [40, 40, 'click'],
     [-40, 40, 'dblclick'],
-  ]
-
-  for (const [xMove, yMove, method] of moves) {
-    await page.mouse[method]((x += xMove), (y += yMove))
-  }
+  ])
 
   await page.getByLabel('Durchschneiden').click()
 
-  const cut: [number, number, string][] = [
-    [-80, -40, 'click'],
+  await draw(page, [
+    [-80, 0, 'click'],
     [160, 0, 'dblclick'],
-  ]
-
-  for (const [xMove, yMove, method] of cut) {
-    await page.mouse[method]((x += xMove), (y += yMove))
-  }
+  ])
 
   await page.waitForFunction(
     (id) =>
@@ -60,40 +39,18 @@ test('drawn polygons can be cut', async ({ page }) => {
 test('drawn polygons can be duplicated', async ({ page }) => {
   await openDiPlan(page)
 
-  const canvas = await page.locator('canvas')
-  const boundingBox = await canvas.boundingBox()
-  if (boundingBox === null) throw new Error('Canvas not found.')
-  const { width, height } = boundingBox
-  let { x, y } = boundingBox
-
-  x += width / 2
-  y += height / 2
-
   await page.getByLabel('Digitalisierungswerkzeuge').click()
   await page.getByLabel('Neue Fläche einzeichnen').click()
 
-  // canvas not fully visible initially
-  await canvas.scrollIntoViewIfNeeded()
-  const scrollY = await page.evaluate(() => window.scrollY)
-  y -= scrollY
-
-  const moves: [number, number, string][] = [
+  await draw(page, [
     [-40, 0, 'click'],
     [40, -40, 'click'],
     [40, 40, 'dblclick'],
-  ]
-
-  for (const [xMove, yMove, method] of moves) {
-    await page.mouse[method]((x += xMove), (y += yMove))
-  }
+  ])
 
   await page.getByLabel('Duplizieren').click()
 
-  const duplicate: [number, number, string][] = [[-40, -20, 'click']]
-
-  for (const [xMove, yMove, method] of duplicate) {
-    await page.mouse[method]((x += xMove), (y += yMove))
-  }
+  await draw(page, [[0, -20, 'click']])
 
   await page.waitForFunction(
     (id) =>
@@ -108,55 +65,29 @@ test('drawn polygons can be duplicated', async ({ page }) => {
 test('drawn polygons can be merged', async ({ page }) => {
   await openDiPlan(page)
 
-  const canvas = await page.locator('canvas')
-  const boundingBox = await canvas.boundingBox()
-  if (boundingBox === null) throw new Error('Canvas not found.')
-  const { width, height } = boundingBox
-  let { x, y } = boundingBox
-
-  x += width / 2
-  y += height / 2
-
   await page.getByLabel('Digitalisierungswerkzeuge').click()
   await page.getByLabel('Neue Fläche einzeichnen').click()
 
-  // canvas not fully visible initially
-  await canvas.scrollIntoViewIfNeeded()
-  const scrollY = await page.evaluate(() => window.scrollY)
-  y -= scrollY
-
-  const movesOne: [number, number, string][] = [
+  await draw(page, [
     [-40, 0, 'click'],
     [0, -40, 'click'],
     [-40, 40, 'dblclick'],
-  ]
+  ])
 
-  const movesTwo: [number, number, string][] = [
-    [80, 0, 'click'],
+  await draw(page, [
+    [40, 0, 'click'],
     [40, 0, 'click'],
     [-40, -40, 'dblclick'],
-  ]
-
-  for (const [xMove, yMove, method] of movesOne) {
-    await page.mouse[method]((x += xMove), (y += yMove))
-  }
-
-  for (const [xMove, yMove, method] of movesTwo) {
-    await page.mouse[method]((x += xMove), (y += yMove))
-  }
+  ])
 
   await page.getByLabel('Flächen kombinieren').click()
 
-  const merge: [number, number, string][] = [
-    [20, 20, 'click'],
-    [-80, 0, 'click'],
-    [0, 20, 'click'],
-    [80, 0, 'dblclick'],
-  ]
-
-  for (const [xMove, yMove, method] of merge) {
-    await page.mouse[method]((x += xMove), (y += yMove))
-  }
+  await draw(page, [
+    [60, -20, 'click'],
+    [-120, 0, 'click'],
+    [0, 40, 'click'],
+    [120, 0, 'dblclick'],
+  ])
 
   await page.waitForFunction(
     (id) =>
