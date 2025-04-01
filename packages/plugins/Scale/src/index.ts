9e1d20a7f79a62073eae9acc65b6c8df558ddd67
bruehlca <carina.bruehl@dataport.de>
2025-04-01 15:41:16 +0200

    expose thousandsSeparator and getDpi

--- packages/plugins/Scale/src/index.ts
+++ packages/plugins/Scale/src/index.ts
@@ -5,6 +5,8 @@ import locales from './locales'
 import { makeStoreModule } from './store'
 
 export { default as beautifyScale } from './utils/beautifyScale'
+export { default as thousandsSeparator } from './utils/thousandsSeperator'
+export { default as getDpi } from './utils/getDpi'
 
 export default (options: ScaleConfiguration) => (instance: Vue) =>
   instance.$store.dispatch('addComponent', {
