f1a20cc2e3278de4269ce6893719c1cb6acb8986
Dennis Sen <dennis.sen@dataport.de>
2025-04-08 07:28:20 +0200

    move e2e tests

--- e2e/draw.spec.ts
+++ e2e/draw.spec.ts
@@ -12,7 +12,7 @@ test('the draw tool can be used to draw polygons on the map', async ({
 
   await page.getByLabel('Draw tools').click()
   await page.getByText('Draw, write and measure').click()
-  await page.getByText('Polygon').click()
+  await page.getByText('Polygon', { exact: true }).click()
 
   if (isMobile) {
     // menu gets in the way in mobile
@@ -83,5 +83,148 @@ test('two features drawn at the same coordinate can be modified separately', asy
   )
   expect(drawing.features[0].geometry.coordinates[1]).not.toBe(
     drawing.features[1].geometry.coordinates[1]
+  )
+})
+
+test('drawn polygons can be cut', async ({ page, isMobile }) => {
+  await openSnowbox(page)
+
+  await page.getByLabel('Draw tools').click()
+  await page.getByText('Draw, write and measure').click()
+  await page.getByText('Polygon', { exact: true }).click()
+
+  if (isMobile) {
+    // menu gets in the way in mobile
+    await page.getByLabel('Draw tools').click()
+    await expect(page.locator('.polar-draw-menu')).toHaveCount(0)
+  }
+
+  await draw(page, [
+    [-40, 0, 'click'],
+    [40, -40, 'click'],
+    [40, 40, 'click'],
+    [-40, 40, 'dblclick'],
+  ])
+
+  if (isMobile) {
+    await page.getByLabel('Draw tools').click()
+    await expect(page.locator('.polar-draw-menu')).toHaveCount(1)
+  }
+
+  await page.getByLabel('Cut polygons').click()
+
+  if (isMobile) {
+    await page.getByLabel('Draw tools').click()
+    await expect(page.locator('.polar-draw-menu')).toHaveCount(0)
+  }
+
+  await draw(page, [
+    [-80, 0, 'click'],
+    [160, 0, 'dblclick'],
+  ])
+
+  await page.waitForFunction(
+    (id) =>
+      document.getElementById(id)?.innerText !== 'uninitialized' &&
+      JSON.parse(document.getElementById(id)?.innerText || '').features
+        .length === 2,
+    drawTargetId,
+    { timeout: 5000 }
+  )
+})
+
+test('drawn polygons can be duplicated', async ({ page, isMobile }) => {
+  await openSnowbox(page)
+
+  await page.getByLabel('Draw tools').click()
+  await page.getByText('Draw, write and measure').click()
+  await page.getByText('Polygon', { exact: true }).click()
+
+  if (isMobile) {
+    // menu gets in the way in mobile
+    await page.getByLabel('Draw tools').click()
+    await expect(page.locator('.polar-draw-menu')).toHaveCount(0)
+  }
+
+  await draw(page, [
+    [-40, 0, 'click'],
+    [40, -40, 'click'],
+    [40, 40, 'dblclick'],
+  ])
+
+  if (isMobile) {
+    await page.getByLabel('Draw tools').click()
+    await expect(page.locator('.polar-draw-menu')).toHaveCount(1)
+  }
+
+  await page.getByLabel('Duplicate').click()
+
+  if (isMobile) {
+    await page.getByLabel('Draw tools').click()
+    await expect(page.locator('.polar-draw-menu')).toHaveCount(0)
+  }
+
+  await draw(page, [[0, -20, 'click']])
+
+  await page.waitForFunction(
+    (id) =>
+      document.getElementById(id)?.innerText !== 'uninitialized' &&
+      JSON.parse(document.getElementById(id)?.innerText || '').features
+        .length === 2,
+    drawTargetId,
+    { timeout: 5000 }
+  )
+})
+
+test('drawn polygons can be merged', async ({ page, isMobile }) => {
+  await openSnowbox(page)
+
+  await page.getByLabel('Draw tools').click()
+  await page.getByText('Draw, write and measure').click()
+  await page.getByText('Polygon', { exact: true }).click()
+
+  if (isMobile) {
+    await page.getByLabel('Draw tools').click()
+    await expect(page.locator('.polar-draw-menu')).toHaveCount(0)
+  }
+
+  await draw(page, [
+    [-40, 0, 'click'],
+    [0, -40, 'click'],
+    [-40, 40, 'dblclick'],
+  ])
+
+  await draw(page, [
+    [40, 0, 'click'],
+    [40, 0, 'click'],
+    [-40, -40, 'dblclick'],
+  ])
+
+  if (isMobile) {
+    await page.getByLabel('Draw tools').click()
+    await expect(page.locator('.polar-draw-menu')).toHaveCount(1)
+  }
+
+  await page.getByLabel('Merge polygons').click()
+
+  if (isMobile) {
+    await page.getByLabel('Draw tools').click()
+    await expect(page.locator('.polar-draw-menu')).toHaveCount(0)
+  }
+
+  await draw(page, [
+    [60, -20, 'click'],
+    [-120, 0, 'click'],
+    [0, 40, 'click'],
+    [120, 0, 'dblclick'],
+  ])
+
+  await page.waitForFunction(
+    (id) =>
+      document.getElementById(id)?.innerText !== 'uninitialized' &&
+      JSON.parse(document.getElementById(id)?.innerText || '').features
+        .length === 1,
+    drawTargetId,
+    { timeout: 5000 }
   )
 })
