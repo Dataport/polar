a0ab5d26b01adf72cfd504727119277f8197fa08
Pascal Roehling <pas.roehling@gmail.com>
2025-04-22 17:41:30 +0200

    Move mutation generation to store index file

--- packages/plugins/Routing/src/store/index.ts
+++ packages/plugins/Routing/src/store/index.ts
@@ -1,9 +1,11 @@
 import { type PolarModule } from '@polar/lib-custom-types'
-import { generateSimpleGetters } from '@repositoryname/vuex-generators'
+import {
+  generateSimpleGetters,
+  generateSimpleMutations,
+} from '@repositoryname/vuex-generators'
 import { RoutingGetters, RoutingState } from '../types'
 import { getInitialState } from './state'
 import actions from './actions'
-import mutations from './mutations'
 
 /**
  * Creates and returns a Vuex store module with namespacing enabled.
@@ -22,5 +24,33 @@ export const makeStoreModule = (): PolarModule<
   getters: {
     ...generateSimpleGetters(getInitialState()),
   },
-  mutations,
+  mutations: {
+    ...generateSimpleMutations(getInitialState()),
+
+    /**
+     * Updates 'selectableTravelModes' in the state by keeping only the travelModes
+     * whose 'key' is included in 'selectedTravelModes'. The 'key' and 'localKey' properties are retained.
+     *
+     * @param state - The VueX state containing 'selectableTravelModes'.
+     * @param selectedTravelModes - An array of 'key' values that should be retained.
+     */
+    setSelectableTravelModes(state, selectedTravelModes) {
+      state.selectableTravelModes = state.selectableTravelModes
+        .filter((travelMode) => selectedTravelModes.includes(travelMode.key))
+        .map(({ key, localKey }) => ({ key, localKey }))
+    },
+
+    /**
+     * Updates 'selectablePreferences' in the state by keeping only the preferences
+     * whose 'key' is included in 'selectedPreferences'. The 'key' and 'localKey' properties are retained.
+     *
+     * @param state - The VueX state containing 'selectablePreferences'.
+     * @param selectedPreferences - An array of 'key' values that should be retained.
+     */
+    setSelectablePreferences(state, selectedPreferences) {
+      state.selectablePreferences = state.selectablePreferences
+        .filter((preference) => selectedPreferences.includes(preference.key))
+        .map(({ key, localKey }) => ({ key, localKey }))
+    },
+  },
 })
