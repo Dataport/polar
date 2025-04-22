69904af897d8871b2c618e89b5db63429ddce259
Pascal Roehling <pas.roehling@gmail.com>
2025-04-22 17:38:33 +0200

    Remove unused getter renderType

--- packages/plugins/Routing/src/store/getters.ts
+++ packages/plugins/Routing/src/store/getters.ts
@@ -5,17 +5,9 @@ import { getInitialState } from './state'
  * Defines Vuex getters for the routing module.
  *
  * - Includes automatically generated simple getters based on the initial state.
- * - Provides a custom getter 'renderType', which retrieves the 'renderType'
- *   from the 'configuration.routing' state in the root Vuex store.
- *   If not defined, it defaults to 'independent'.
  */
 const getters = {
   ...generateSimpleGetters(getInitialState()),
-  renderType: (_, __, ___, rootGetters) => {
-    return rootGetters.configuration?.routing?.renderType
-      ? rootGetters.configuration.routing.renderType
-      : 'independent'
-  },
 }
 
 export default getters
