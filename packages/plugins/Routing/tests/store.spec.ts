c1d3966126af31404731f51b383212a16279bc91
Pascal Roehling <pas.roehling@gmail.com>
2025-06-06 10:58:37 +0200

    Update routing tests

--- packages/plugins/Routing/tests/store.spec.ts
+++ packages/plugins/Routing/tests/store.spec.ts
@@ -1,27 +1,17 @@
+// NOTE: action tests currently not type-supported, but working
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { PolarActionHandler } from '@polar/lib-custom-types'
-import proj4 from 'proj4'
-import { register } from 'ol/proj/proj4'
-import { makeStoreModule } from '../src/store/index'
+import { makeStoreModule } from '../src/store'
 import { getInitialState } from '../src/store/state'
 import { RoutingState, RoutingGetters } from '../src/types'
-// import { reactive } from 'vue'
 
 describe('plugin-routing', () => {
-  jest.mock('ol/proj', () => ({
-    transform: jest.fn((coord, from, to) => {
-      if (from === 'EPSG:4326' && to === 'EPSG:25832') {
-        return [9.6, 48.1] // example values
-      }
-      return coord
-    }),
-  }))
   jest.mock('ol/source/Vector', () => {
     return jest.fn().mockImplementation(() => ({
       addFeature: jest.fn(),
       clear: jest.fn(),
     }))
   })
-
   describe('store', () => {
     describe('actions', () => {
       describe('setupModule', () => {
@@ -36,27 +26,17 @@ describe('plugin-routing', () => {
         }
 
         let actionContext
+        let addLayer
+        let dispatch
 
         beforeEach(() => {
+          addLayer = jest.fn()
+          dispatch = jest.fn()
           actionContext = {
-            state: getInitialState(),
-            commit: jest.fn((mutation, payload) => {
-              if (mutation === 'setStart') actionContext.state.start = payload
-              if (mutation === 'setEnd') actionContext.state.end = payload
-            }),
-            dispatch: jest.fn(),
-            getters: {},
-            rootState: {},
+            dispatch,
             rootGetters: {
-              configuration: {
-                routing: {
-                  displayPreferences: { avoidTolls: true },
-                  displayRouteTypesToAvoid: ['highways'],
-                },
-              },
               map: {
-                addLayer: jest.fn(),
-                on: jest.fn(),
+                addLayer,
               },
             },
           }
@@ -66,58 +46,13 @@ describe('plugin-routing', () => {
           jest.clearAllMocks()
         })
 
-        it('should commit configuration settings to state', () => {
+        it('should initialize the draw layer and the draw interaction', () => {
           // @ts-ignore
           setupModule(actionContext)
 
-          expect(actionContext.commit).toHaveBeenCalledWith(
-            'setDisplayPreferences',
-            { avoidTolls: true }
-          )
-          expect(actionContext.commit).toHaveBeenCalledWith(
-            'setDisplayRouteTypesToAvoid',
-            ['highways']
-          )
-        })
-
-        it('should set up the draw layer and event listener', () => {
-          // @ts-ignore
-          setupModule(actionContext)
-
-          expect(actionContext.rootGetters.map.addLayer).toHaveBeenCalledTimes(
-            1
-          )
-          expect(actionContext.rootGetters.map.on).toHaveBeenCalledWith(
-            'click',
-            expect.any(Function)
-          )
-        })
-
-        it('should commits start and end points on map click', () => {
-          let clickHandler
-          actionContext.rootGetters.map.on = jest.fn((event, handler) => {
-            if (event === 'click') clickHandler = handler
-          })
-
-          // @ts-ignore
-          setupModule(actionContext)
-
-          if (!clickHandler) throw new Error('Click handler was not set')
-
-          // Simulate first click (start point)
-          clickHandler({ coordinate: [10, 50] })
-          expect(actionContext.commit).toHaveBeenCalledWith(
-            expect.stringContaining('setStart'),
-            expect.arrayContaining([10, 50])
-          )
-          console.error(
-            'Commit history before assertion:',
-            actionContext.commit.mock.calls
-          )
-
-          // Simulate second click (end point)
-          clickHandler({ coordinate: [20, 60] })
-          expect(actionContext.commit).toHaveBeenCalledWith('setEnd', [20, 60])
+          expect(dispatch).toHaveBeenCalledTimes(1)
+          expect(dispatch).toHaveBeenCalledWith('initializeDraw')
+          expect(addLayer).toHaveBeenCalledTimes(1)
         })
       })
       describe('reset', () => {
@@ -134,7 +69,12 @@ describe('plugin-routing', () => {
         }
 
         let actionContext
+        let commit
+        let dispatch
+
         beforeEach(() => {
+          commit = jest.fn()
+          dispatch = jest.fn()
           actionContext = {
             state: {
               start: [1, 2],
@@ -146,97 +86,33 @@ describe('plugin-routing', () => {
               selectedRouteTypesToAvoid: ['toll'],
               routingResponseData: { data: 'some data' },
             },
-            commit: jest.fn(),
-            dispatch: jest.fn(),
+            commit,
+            dispatch,
           }
         })
 
         it('should reset all coordinates and related state properties', () => {
+          // @ts-ignore
           reset(actionContext)
-          expect(actionContext.commit).toHaveBeenCalledWith('setStart', [])
-          expect(actionContext.commit).toHaveBeenCalledWith('setEnd', [])
-          expect(actionContext.commit).toHaveBeenCalledWith(
-            'setStartAddress',
-            ''
-          )
-          expect(actionContext.commit).toHaveBeenCalledWith('setEndAddress', '')
-          expect(actionContext.commit).toHaveBeenCalledWith(
+          expect(commit).toHaveBeenCalledTimes(6)
+          expect(commit).toHaveBeenCalledWith('resetRoute')
+          expect(commit).toHaveBeenCalledWith('setCurrentlyFocusedInput', -1)
+          expect(commit).toHaveBeenCalledWith(
             'setSelectedTravelMode',
-            ''
+            'driving-car'
           )
-          expect(actionContext.commit).toHaveBeenCalledWith(
+          expect(commit).toHaveBeenCalledWith(
             'setSelectedPreference',
-            ''
+            'recommended'
           )
-          expect(actionContext.commit).toHaveBeenCalledWith(
+          expect(commit).toHaveBeenCalledWith(
             'setSelectedRouteTypesToAvoid',
             []
           )
-          expect(actionContext.commit).toHaveBeenCalledWith(
-            'setRoutingResponseData',
-            {}
-          )
-          expect(actionContext.dispatch).toHaveBeenCalledWith(
-            'deleteRouteDrawing'
-          )
+          expect(commit).toHaveBeenCalledWith('setRoutingResponseData', {})
+          expect(dispatch).toHaveBeenCalledWith('clearRoute')
         })
       })
-
-      describe('translateCoordinateToWGS84', () => {
-        const RoutingStore = makeStoreModule()
-        const translateCoordinateToWGS84 = RoutingStore.actions
-          ?.translateCoordinateToWGS84 as PolarActionHandler<
-          RoutingState,
-          RoutingGetters
-        >
-
-        if (typeof translateCoordinateToWGS84 === 'undefined') {
-          throw new Error(
-            'Action translateCoordinateToWGS84 is missing in RoutingStore. Tests could not be run.'
-          )
-        }
-
-        let actionContext
-
-        beforeEach(() => {
-          actionContext = {
-            rootGetters: {
-              configuration: { epsg: 'EPSG:25832' },
-            },
-          }
-
-          // Proj4-Definitionen setzen
-          proj4.defs(
-            'EPSG:25832',
-            '+proj=utm +zone=32 +datum=WGS84 +units=m +no_defs'
-          )
-          proj4.defs('EPSG:4326', '+proj=longlat +datum=WGS84 +no_defs')
-          register(proj4)
-        })
-
-        it('should transform coordinates to WGS84 correctly', () => {
-          const inputCoordinate = [500000, 5400000]
-          const expectedCoordinate = proj4(
-            'EPSG:25832',
-            'EPSG:4326',
-            inputCoordinate
-          )
-
-          const result = translateCoordinateToWGS84(
-            actionContext,
-            inputCoordinate
-          )
-
-          // Erwartung: transformierte Koordinaten sollten mit Proj4-Berechnung übereinstimmen
-          expect(result).toEqual(
-            expect.arrayContaining([
-              expectedCoordinate[0],
-              expectedCoordinate[1],
-            ])
-          )
-        })
-      })
-
       describe('getRoute', () => {
         const RoutingStore = makeStoreModule()
         const getRoute = RoutingStore.actions?.getRoute as PolarActionHandler<
@@ -251,23 +127,30 @@ describe('plugin-routing', () => {
         }
 
         let actionContext
+        let commit
+        let dispatch
 
         beforeEach(() => {
+          commit = jest.fn()
+          dispatch = jest.fn()
           actionContext = {
             state: {
               ...getInitialState(),
-              start: [569779.2478911661, 5936605.1216985425], // Beispielkoordinaten in EPSG:25832
-              end: [564138.3342705927, 5937211.3700576415], // Beispielkoordinaten in EPSG:25832
+              selectedRouteTypesToAvoid: ['tollways'],
+              selectedPreference: 'recommended',
             },
-            commit: jest.fn(),
-            dispatch: jest.fn().mockResolvedValue([9.6, 48.1]), // Beispiel: Umgewandelte Koordinaten in WGS84
-            rootGetters: {
-              configuration: {
-                routing: { url: 'http://example.com', format: 'json' },
-              },
+            commit,
+            dispatch,
+            getters: {
+              url: 'http://example.com/driving-car/json',
+              routeAsWGS84: [
+                [19.6, 48.1],
+                [19.5, 47.1],
+              ],
             },
           }
 
+          // @ts-ignore
           global.fetch = jest.fn(() =>
             Promise.resolve({
               ok: true,
@@ -293,89 +176,55 @@ describe('plugin-routing', () => {
         })
 
         it('should fetch routing data and commit the response', async () => {
-          // Mock for `dispatch` in order to transform the coordinates
-          actionContext.dispatch = jest
-            .fn()
-            .mockImplementation((action, payload) => {
-              if (action === 'translateCoordinateToWGS84') {
-                return payload === actionContext.state.start
-                  ? [19.6, 48.1]
-                  : [19.5, 47.1] // example coordinates in WGS84
-              }
-            })
-
-          // Mock for fetch
-          global.fetch = jest.fn(() =>
-            Promise.resolve({
-              ok: true,
-              json: () =>
-                Promise.resolve({
-                  features: [
-                    {
-                      geometry: {
-                        coordinates: [
-                          [19.6, 48.1],
-                          [19.5, 47.1],
-                        ],
-                      },
-                    },
-                  ],
-                }),
-            })
-          )
-
+          // @ts-ignore
           await getRoute(actionContext)
 
           // Erwartung: fetch wird korrekt aufgerufen
-          expect(fetch).toHaveBeenCalledWith('http://example.com/json', {
-            method: 'POST',
-            headers: {
-              // eslint-disable-next-line @typescript-eslint/naming-convention
-              'Content-Type': 'application/json', // eslint error has to be ignored since name is determined by ORS
-            },
-            body: JSON.stringify({
-              coordinates: [
-                [19.6, 48.1],
-                [19.5, 47.1],
-              ], // Transformierte Koordinaten
-              geometry: true,
-              instructions: true,
-              language: 'en',
-              options: {
-                avoid_features: [],
-              },
-              preference: '',
-              units: 'm',
-            }),
-          })
-
-          // Expectation: commit is called correctly
-          expect(actionContext.commit).toHaveBeenCalledTimes(1)
-          expect(actionContext.commit).toHaveBeenCalledWith(
-            'setRoutingResponseData',
+          expect(fetch).toHaveBeenCalledWith(
+            'http://example.com/driving-car/json',
             {
-              features: [
-                {
-                  geometry: {
-                    coordinates: [
-                      [19.6, 48.1],
-                      [19.5, 47.1],
-                    ],
-                  },
+              method: 'POST',
+              headers: {
+                // eslint-disable-next-line @typescript-eslint/naming-convention
+                'Content-Type': 'application/json', // eslint error has to be ignored since name is determined by ORS
+              },
+              body: JSON.stringify({
+                coordinates: [
+                  [19.6, 48.1],
+                  [19.5, 47.1],
+                ],
+                geometry: true,
+                instructions: true,
+                language: 'en',
+                options: {
+                  avoid_features: ['tollways'],
                 },
-              ],
+                preference: 'recommended',
+                units: 'm',
+              }),
             }
           )
 
-          // Erwartung: drawRoute wird ausgelöst
-          expect(actionContext.dispatch).toHaveBeenCalledWith('drawRoute')
+          expect(commit).toHaveBeenCalledTimes(1)
+          expect(commit).toHaveBeenCalledWith('setRoutingResponseData', {
+            features: [
+              {
+                geometry: {
+                  coordinates: [
+                    [19.6, 48.1],
+                    [19.5, 47.1],
+                  ],
+                },
+              },
+            ],
+          })
+          expect(dispatch).toHaveBeenCalledTimes(2)
+          expect(dispatch).toHaveBeenCalledWith('clearRoute')
+          expect(dispatch).toHaveBeenCalledWith('drawRoute')
         })
 
         it('should handle fetch errors correctly', async () => {
-          const consoleErrorSpy = jest
-            .spyOn(console, 'error')
-            .mockImplementation(() => {})
-
+          // @ts-ignore
           global.fetch = jest.fn(() =>
             Promise.resolve({
               ok: false,
@@ -383,13 +232,18 @@ describe('plugin-routing', () => {
             })
           )
 
+          // @ts-ignore
           await getRoute(actionContext)
 
-          expect(consoleErrorSpy).toHaveBeenCalledWith(
-            expect.stringContaining('Route could not be determined')
+          expect(commit).toHaveBeenCalledTimes(0)
+          expect(dispatch).toHaveBeenCalledTimes(2)
+          expect(dispatch).toHaveBeenCalledWith('clearRoute')
+          expect(dispatch).toHaveBeenCalledWith(
+            'handleErrors',
+            new Error(
+              'Route could not be determined. Try different coordinates.'
+            )
           )
-
-          consoleErrorSpy.mockRestore()
         })
       })
     })
