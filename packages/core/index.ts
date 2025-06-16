3d7fcf575167ad17f36eb08daf49837dbcc6b610
Hendrik Oenings <hendrik.oenings@dataport.de>
2025-06-16 19:50:14 +0200

    Add pinia (VueX successor) support

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -2,11 +2,13 @@ import { defineCustomElement } from 'vue'
 import PolarMapCE from './components/PolarMap.ce.vue'
 import { ComponentLibrary } from './vuePlugins/kolibri'
 import { I18Next } from './vuePlugins/i18next'
+import { Pinia } from './vuePlugins/pinia'
 
 export const PolarMap = defineCustomElement(PolarMapCE, {
 	configureApp (app) {
 		app.use(ComponentLibrary)
 		app.use(I18Next)
+		app.use(Pinia)
 	},
 })
 
---
+++ packages/core/vuePlugins/pinia.ts
@@ -0,0 +1,9 @@
+import type { Plugin } from 'vue'
+import { createPinia } from 'pinia'
+
+export const Pinia: Plugin = {
+	install (app) {
+		const pinia = createPinia()
+		app.use(pinia)
+	},
+}
