cd1f838174c510f90df5ee4ae81e39f4521609b7
Hendrik Oenings <hendrik.oenings@dataport.de>
2025-06-16 18:17:06 +0200

    Use new version of @dataport/eslint-config-geodev

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,7 @@ import mainConfig from '@dataport/eslint-config-geodev'
 import browserConfig from '@dataport/eslint-config-geodev/browser'
 import tsConfig from '@dataport/eslint-config-geodev/typescript'
 import vueConfig from '@dataport/eslint-config-geodev/vue'
-import tseslint from 'typescript-eslint'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import vuePugConfig from '@dataport/eslint-config-geodev/vue-pug'
 
 export default defineConfig([
 	{
@@ -26,29 +25,10 @@ export default defineConfig([
 	},
 	{
 		files: ['**/*.ts'],
-		extends: [mainConfig, browserConfig, tsConfig, tseslint.configs.strictTypeChecked, {
-			languageOptions: {
-				parserOptions: {
-					projectService: true,
-					ecmaVersion: 2021,
-					sourceType: 'module',
-					tsconfigRootDir: import.meta.dirname,
-				},
-			},
-		}],
+		extends: [mainConfig, browserConfig, tsConfig],
 	},
 	{
 		files: ['**/*.vue'],
-		extends: [mainConfig, browserConfig, tsConfig, vueConfig, tseslint.configs.strictTypeChecked, {
-			languageOptions: {
-				parserOptions: {
-					projectService: true,
-					ecmaVersion: 2021,
-					sourceType: 'module',
-					tsconfigRootDir: import.meta.dirname,
-					extraFileExtensions: ['.vue'],
-				},
-			},
-		}, vueTsEslintConfig()],
+		extends: [mainConfig, browserConfig, tsConfig, vueConfig, vuePugConfig],
 	},
 ])
