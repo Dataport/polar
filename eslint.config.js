8b93e30ecac5cebbeba7c69b6990b2009553b55b
Pascal Roehling <pas.roehling@gmail.com>
2025-06-16 21:54:28 +0200

    Fix linting setup
    Some of these options should surely be set in
    @dataport/eslint-config-geodev/typescript.

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,21 @@ import mainConfig from '@dataport/eslint-config-geodev'
 import browserConfig from '@dataport/eslint-config-geodev/browser'
 import tsConfig from '@dataport/eslint-config-geodev/typescript'
 import vueConfig from '@dataport/eslint-config-geodev/vue'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig([
 	{
-		ignores: ['vue2/'],
+		ignores: ['vue2/', '**/build',
+			'**/.cache',
+			'**/coverage',
+			'**/dist',
+			'**/dist-test',
+			'**/docs',
+			'**/node_modules',
+			'**/tests_output',
+			'*.d.ts',
+			'/.nx/',
+		],
 	},
 	{
 		files: ['**/*.js'],
@@ -14,10 +25,29 @@ export default defineConfig([
 	},
 	{
 		files: ['**/*.ts'],
-		extends: [mainConfig, browserConfig, tsConfig],
+		extends: [mainConfig, browserConfig, tsConfig, tseslint.configs.strictTypeChecked, {
+			languageOptions: {
+				parserOptions: {
+					projectService: true,
+					ecmaVersion: 2021,
+					sourceType: 'module',
+					tsconfigRootDir: import.meta.dirname,
+				},
+			},
+		}],
 	},
 	{
 		files: ['**/*.vue'],
-		extends: [mainConfig, browserConfig, tsConfig, vueConfig],
+		extends: [mainConfig, browserConfig, tsConfig, vueConfig, tseslint.configs.strictTypeChecked, {
+			languageOptions: {
+				parserOptions: {
+					projectService: true,
+					ecmaVersion: 2021,
+					sourceType: 'module',
+					tsconfigRootDir: import.meta.dirname,
+					extraFileExtensions: ['.vue'],
+				},
+			},
+		}],
 	},
 ])
