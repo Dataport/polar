7ac2709ac4562fe30a61f4a786a5df6867e3eb13
Hendrik Oenings <hendrik.oenings@dataport.de>
2025-06-24 20:29:21 +0200

    polar: Add multiple entry points for build

--- packages/polar/vite.config.ts
+++ packages/polar/vite.config.ts
@@ -1,7 +1,17 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+import { externalizeDeps } from 'vite-plugin-externalize-deps'
+
+import { globSync } from 'glob'
+
+import pkg from './package.json'
 
 export default defineConfig({
+	root: __dirname,
 	plugins: [
 		vue({
 			template: {
@@ -10,12 +20,34 @@ export default defineConfig({
 				},
 			},
 		}),
+		dts({
+			root: '../../',
+			entryRoot: 'packages/polar/src',
+			include: [ '**/*.ts', '**/*.vue' ],
+			exclude: [ '**/vite.config.ts' ],
+			outDir: 'packages/polar/dist',
+			skipDiagnostics: false,
+		}),
+		externalizeDeps({
+		}),
 	],
 	build: {
+		minify: false,
+		sourcemap: true,
+		outDir: './dist',
+		reportCompressedSize: false,
 		lib: {
 			name: '@polar/polar',
-			fileName: 'polar',
-			entry: 'core/src/index.ts',
+			entry: Object.fromEntries(
+				globSync('src/**/*.{js,ts,vue}').map(file => [
+					path.relative(
+						'src',
+						file.slice(0, file.length - path.extname(file).length)
+					),
+					fileURLToPath(new URL(file, import.meta.url))
+				])
+			),
+			formats: ['es'],
 		},
 	},
 })
