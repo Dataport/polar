42025958525a7e7e26480fb78ec9c1e421bfa553
Pascal Roehling <pas.roehling@gmail.com>
2025-07-15 17:30:48 +0200

    Add lib functionality invisibleStyle

---
+++ packages/polar/lib/invisibleStyle/index.ts
@@ -0,0 +1,32 @@
+import Style from 'ol/style/Style'
+import { Feature } from 'ol'
+
+/*
+ * Exports a style for vector layer features that results in invisibility.
+ * Plugins that work with feature visibility ought to use this lib-functionality
+ * to keep them interoperable.
+ */
+
+/**
+ * Makes feature invisible.
+ * To remove the invisibility, set the style to `undefined` or another style.
+ *
+ * Example usage:
+ * feature.setStyle(InvisibleStyle)
+ *
+ */
+export const InvisibleStyle = new Style()
+/**
+ * Checks if a feature is invisible.
+ *
+ * @param feature - The feature to check.
+ */
+export const isInvisible = (feature: Feature) =>
+	feature.getStyle() === InvisibleStyle
+/**
+ * Checks if a feature is visible.
+ *
+ * @param feature - The feature to check.
+ */
+export const isVisible = (feature: Feature) =>
+	feature.getStyle() !== InvisibleStyle
