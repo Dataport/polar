36415dd215636bf977c883f34450fa9973ce4067
Pascal Roehling <pas.roehling@gmail.com>
2025-07-15 18:21:50 +0200

    Add marker styles and use getPolygonFillHatch of masterportalapi

--- packages/polar/core/src/utils/markers.ts
+++ packages/polar/core/src/utils/markers.ts
@@ -1,7 +1,9 @@
+import PolygonStyle from '@masterportal/masterportalapi/src/vectorStyle/styles/polygon/stylePolygon'
 import Style from 'ol/style/Style'
 import Icon from 'ol/style/Icon'
-import { MarkerStyle } from '@polar/lib-custom-types'
-import { getPolygonFillHatch } from './hatches'
+import type { MarkerStyle } from '../types'
+
+const polygonStyle = new PolygonStyle()
 
 type GetMarkerFunction = (style: MarkerStyle, multi: boolean) => Style
 
@@ -20,15 +22,15 @@ const defaultUnselectableFill = '#333333'
 const prefix = 'data:image/svg+xml,'
 
 const getImagePattern = (fill: Exclude<MarkerStyle['fill'], undefined>) =>
-  typeof fill === 'string'
-    ? ''
-    : `<defs>
+	typeof fill === 'string'
+		? ''
+		: `<defs>
     <pattern id="img" patternUnits="userSpaceOnUse" x="0" y="0" width="${
-      fill.size || 30
-    }" height="${fill.size || 30}">
-      <image href="${getPolygonFillHatch(fill).toDataURL()}" width="${
-        fill.size || 30
-      }" height="${fill.size || 30}"/>
+			fill.size || 30
+		}" height="${fill.size || 30}">
+      <image href="${polygonStyle.getPolygonFillHatch(fill).toDataURL()}" width="${
+				fill.size || 30
+			}" height="${fill.size || 30}"/>
     </pattern>
   </defs>`
 
@@ -36,15 +38,15 @@ const getImagePattern = (fill: Exclude<MarkerStyle['fill'], undefined>) =>
  * @masterportal/masterportalapi/public/marker.svg. */
 
 const makeMarker = ({
-  fill = defaultFill,
-  size = imgSize,
-  stroke = defaultStroke,
-  strokeWidth = defaultStrokeWidth,
+	fill = defaultFill,
+	size = imgSize,
+	stroke = defaultStroke,
+	strokeWidth = defaultStrokeWidth,
 }: MarkerStyle) =>
-  `${prefix}${encodeURIComponent(`
+	`${prefix}${encodeURIComponent(`
 <svg width="${size[0]}" height="${
-    size[1]
-  }" viewBox="0 0 30 43" xmlns="http://www.w3.org/2000/svg">
+		size[1]
+	}" viewBox="0 0 30 43" xmlns="http://www.w3.org/2000/svg">
   <title>DB6C494E-88E8-49F1-89CE-97CBEC3A5240</title>
   ${getImagePattern(fill)}
   <path
@@ -58,20 +60,20 @@ const makeMarker = ({
 `)}`
 
 const makeMultiMarker = ({
-  clusterSize = imgSizeMulti,
-  fill = defaultFill,
-  stroke = defaultStroke,
-  strokeWidth = defaultStrokeWidth,
+	clusterSize = imgSizeMulti,
+	fill = defaultFill,
+	stroke = defaultStroke,
+	strokeWidth = defaultStrokeWidth,
 }: MarkerStyle) =>
-  `${prefix}${encodeURIComponent(`
+	`${prefix}${encodeURIComponent(`
 <svg width="${clusterSize[0]}" height="${
-    clusterSize[1]
-  }" viewBox="0 0 30 43" xmlns="http://www.w3.org/2000/svg">
+		clusterSize[1]
+	}" viewBox="0 0 30 43" xmlns="http://www.w3.org/2000/svg">
   <title>0A6F4952-4A5A-4E86-88E4-4B3D2EA1E3DF</title>
   ${getImagePattern(fill)}
   <g stroke="${stroke}" stroke-width="${strokeWidth}" fill="${
-    typeof fill === 'string' ? fill : 'url(#img)'
-  }" fill-rule="nonzero">
+		typeof fill === 'string' ? fill : 'url(#img)'
+	}" fill-rule="nonzero">
     <path d="M22.584 1C15.101 1 9 7.101 9 14.584c0 8.103 7.865 20.448 11.63 25.93a2.36 2.36 0 0 0 3.908 0c3.766-5.482 11.63-17.922 11.63-25.93C36.168 7.054 30.068 1 22.584 1z"/>
     <path d="M18.584 1C11.101 1 5 7.101 5 14.584c0 8.103 7.865 20.448 11.63 25.93a2.36 2.36 0 0 0 3.908 0c3.766-5.482 11.63-17.922 11.63-25.93C32.168 7.054 26.068 1 18.584 1z"/>
     <path d="M14.584 1C7.101 1 1 7.101 1 14.584c0 8.103 7.865 20.448 11.63 25.93a2.36 2.36 0 0 0 3.908 0c3.766-5.482 11.63-17.922 11.63-25.93C28.168 7.054 22.068 1 14.584 1z"/>
@@ -87,45 +89,45 @@ const anchor = [0.5, 1]
  * This memoization function optimises this issue by reusing styles.
  * */
 const memoizeStyle = (getMarker: GetMarkerFunction): GetMarkerFunction => {
-  const singleCache = new Map()
-  const multiCache = new Map()
-  return (style, multi) => {
-    const cache = multi ? multiCache : singleCache
-    if (cache.has(style)) {
-      return cache.get(style)
-    }
-    const markerStyle = getMarker(style, multi)
-    cache.set(style, markerStyle)
-    if (cache.size > 1000) {
-      console.warn(
-        `@polar/core: 1000+ styles have been created. This is possibly a memory leak. Please mind that the methods exported by this module are memoized. You *may* be calling the methods with constantly newly generated objects, or maybe there's just a lot of styles.`
-      )
-    }
-    return markerStyle
-  }
+	const singleCache = new Map()
+	const multiCache = new Map()
+	return (style, multi) => {
+		const cache = multi ? multiCache : singleCache
+		if (cache.has(style)) {
+			return cache.get(style)
+		}
+		const markerStyle = getMarker(style, multi)
+		cache.set(style, markerStyle)
+		if (cache.size > 1000) {
+			console.warn(
+				`@polar/core: 1000+ styles have been created. This is possibly a memory leak. Please mind that the methods exported by this module are memoized. You *may* be calling the methods with constantly newly generated objects, or maybe there's just a lot of styles.`
+			)
+		}
+		return markerStyle
+	}
 }
 
 const getStyleFunction =
-  (fallbackFill: string): GetMarkerFunction =>
-  (style, multi = false) =>
-    new Style({
-      image: new Icon({
-        src: (multi ? makeMultiMarker : makeMarker)({
-          fill: fallbackFill,
-          ...style,
-        }),
-        anchor,
-      }),
-    })
+	(fallbackFill: string): GetMarkerFunction =>
+	(style, multi = false) =>
+		new Style({
+			image: new Icon({
+				src: (multi ? makeMultiMarker : makeMarker)({
+					fill: fallbackFill,
+					...style,
+				}),
+				anchor,
+			}),
+		})
 
 export const getDefaultStyle = memoizeStyle(getStyleFunction(defaultFill))
 
 export const getHoveredStyle = memoizeStyle(getStyleFunction(defaultHoverFill))
 
 export const getSelectedStyle = memoizeStyle(
-  getStyleFunction(defaultSelectionFill)
+	getStyleFunction(defaultSelectionFill)
 )
 
 export const getUnselectableStyle = memoizeStyle(
-  getStyleFunction(defaultUnselectableFill)
+	getStyleFunction(defaultUnselectableFill)
 )
