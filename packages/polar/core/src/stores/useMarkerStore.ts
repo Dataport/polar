Dataport/polar

commit 94d6586cd6d99c9fa8f3c622c7ef82be0d771b58

Fix feature selection if features are not clustered

packages/polar/core/src/stores/useMarkerStore.ts
@@ -104,21 +104,22 @@ export const useMarkerStore = defineStore('markers', () => {
 			return
 		}
 
-		const selectedValueCluster = getCluster(
-			coreStore.getMap(),
-			feature,
-			'_polarLayerId'
-		)
+		// If clustering is not enabled, the feature can not be part of a cluster
+		// and doesn't have the property 'feature' defined.
+		const selectedCluster =
+			feature.get('features') === undefined
+				? feature
+				: getCluster(coreStore.getMap(), feature, '_polarLayerId')
 
-		selectedValueCluster.setStyle(
+		selectedCluster.setStyle(
 			getMarkerStyle(
 				selectionStyle,
-				selectedValueCluster.get('features')?.length > 1
+				selectedCluster.get('features')?.length > 1
 			)
 		)
 
-		selectedValue = selectedValueCluster
-		selected.value = selectedValueCluster
+		selectedValue = selectedCluster
+		selected.value = selectedCluster
 		if (centerOnFeature) {
 			coreStore.centerOnFeature(selectedValue)
 		}

commit b0c9d76b9752c5c61218bfba64db5382c2777611

Correctly fix feature selection if no clustering is enabled
94d6586c broke selection for clustered features when zooming.

packages/polar/core/src/stores/useMarkerStore.ts
@@ -75,6 +75,14 @@ export const useMarkerStore = defineStore('markers', () => {
 		return layers.includes(layer.get('id'))
 	}
 
+	function findLayer(layerId: string) {
+		return useCoreStore()
+			.getMap()
+			.getLayers()
+			.getArray()
+			.find((layer) => layer.get('id') === layerId) as VectorLayer | undefined
+	}
+
 	function resolveClusterClick(feature: Feature) {
 		const features = feature.get('features') as Feature[]
 
@@ -104,12 +112,12 @@ export const useMarkerStore = defineStore('markers', () => {
 			return
 		}
 
-		// If clustering is not enabled, the feature can not be part of a cluster
-		// and doesn't have the property 'feature' defined.
+		const layerId = feature.get('_polarLayerId') as string
 		const selectedCluster =
-			feature.get('features') === undefined
-				? feature
-				: getCluster(coreStore.getMap(), feature, '_polarLayerId')
+			// @ts-expect-error | Found layers always have a source and getDistance is defined on cluster sources.
+			typeof findLayer(layerId)?.getSource().getDistance === 'function'
+				? getCluster(coreStore.getMap(), feature, '_polarLayerId')
+				: feature
 
 		selectedCluster.setStyle(
 			getMarkerStyle(