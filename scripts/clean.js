19db76ef8d9292c5ad0445c4915bec64165b993d
Hendrik Oenings <hendrik.oenings@dataport.de>
2025-08-01 08:40:24 +0200

    Code-golf scripts/clean.js

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,11 +1,7 @@
-/* eslint-disable no-console */
 import fs from 'node:fs/promises'
 
-await Promise.all([
-	'.cache',
-	'dist',
-	'docs',
-	'node_modules'
-].map(async dir => {
-	await fs.rm(dir, { recursive: true, force: true })
-}))
+await Promise.all(
+	['.cache', 'dist', 'docs', 'node_modules'].map(
+		async (dir) => await fs.rm(dir, { recursive: true, force: true })
+	)
+)
