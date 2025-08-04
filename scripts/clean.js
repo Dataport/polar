3bf026b2d899d740d2fce1ceeb82050da06374db
Pascal Roehling <pas.roehling@gmail.com>
2025-08-04 14:18:12 +0200

    Change clean script file signature to typescript
    Change file according to linting rules.

--- scripts/clean.js
+++
@@ -1,7 +0,0 @@
-import fs from 'node:fs/promises'
-
-await Promise.all(
-	['.cache', 'dist', 'docs-html', 'node_modules'].map(
-		async (dir) => await fs.rm(dir, { recursive: true, force: true })
-	)
-)
---
+++ scripts/clean.ts
@@ -0,0 +1,7 @@
+import fs from 'node:fs/promises'
+
+await Promise.all(
+	['.cache', 'dist', 'docs-html', 'node_modules'].map(async (dir) => {
+		await fs.rm(dir, { recursive: true, force: true })
+	})
+)
