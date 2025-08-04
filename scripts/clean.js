f1d7df18a1acb640e26c7b4d7854d911f6549bed
Pascal Roehling <pas.roehling@gmail.com>
2025-08-04 14:13:29 +0200

    Remove correct docs folder

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,7 +1,7 @@
 import fs from 'node:fs/promises'
 
 await Promise.all(
-	['.cache', 'dist', 'docs', 'node_modules'].map(
+	['.cache', 'dist', 'docs-html', 'node_modules'].map(
 		async (dir) => await fs.rm(dir, { recursive: true, force: true })
 	)
 )
