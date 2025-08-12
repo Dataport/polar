96ea75447c6097af3f645f9e74653dc15c9b61a5
Hendrik Oenings <hendrik.oenings@dataport.de>
2025-08-12 18:35:19 +0200

    plugins/fullscreen: Improve component test

--- src/plugins/fullscreen/components/FullscreenUI.spec.ts
+++ src/plugins/fullscreen/components/FullscreenUI.spec.ts
@@ -1,22 +1,35 @@
-import { expect, test, vi } from 'vitest'
-import { mount } from '@vue/test-utils'
+import { expect, test as _test, vi } from 'vitest'
+import { mount, VueWrapper } from '@vue/test-utils'
 import { createTestingPinia } from '@pinia/testing'
 import { nextTick } from 'vue'
 import { useFullscreenStore } from '../store'
 import { PluginId } from '../types'
 import FullscreenUI from './FullscreenUI.ce.vue'
 
-test('test button label', async () => {
-	const wrapper = mount(FullscreenUI, {
-		global: {
-			plugins: [createTestingPinia({ createSpy: vi.fn })],
-			mocks: {
-				$t: (key, { ns, context }) => `$t(${ns}:${key}_${context})`,
+/* eslint-disable no-empty-pattern */
+const test = _test.extend<{
+	wrapper: VueWrapper
+	store: ReturnType<typeof useFullscreenStore>
+}>({
+	wrapper: async ({}, use) => {
+		const wrapper = mount(FullscreenUI, {
+			global: {
+				plugins: [createTestingPinia({ createSpy: vi.fn })],
+				mocks: {
+					$t: (key, { ns, context }) => `$t(${ns}:${key}_${context})`,
+				},
 			},
-		},
-	})
+		})
+		await use(wrapper)
+	},
+	store: async ({}, use) => {
+		const store = useFullscreenStore()
+		await use(store)
+	},
+})
+/* eslint-enable no-empty-pattern */
 
-	const store = useFullscreenStore()
+test('Component listens to store changes', async ({ wrapper, store }) => {
 	store.fullscreenEnabled = false
 	await nextTick()
 	expect(wrapper.find('.kern-label').text()).toContain(
@@ -28,4 +41,16 @@ test('test button label', async () => {
 	expect(wrapper.find('.kern-label').text()).toContain(
 		`$t(${PluginId}:button.label_off)`
 	)
+})
+
+test('Component triggers store changes', async ({ wrapper, store }) => {
+	store.fullscreenEnabled = false
+	await nextTick()
+
+	await wrapper.find('button').trigger('click')
+	expect(store.fullscreenEnabled).toBeTruthy()
+	await nextTick()
+
+	await wrapper.find('button').trigger('click')
+	expect(store.fullscreenEnabled).toBeFalsy()
 })
