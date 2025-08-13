7589edb5536147f4e30d43ef27815468979e6136
Pascal Roehling <pas.roehling@gmail.com>
2025-08-13 19:07:18 +0200

    Adjust type import

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,5 +1,5 @@
 import type { Resource } from 'i18next'
-import { Feature } from 'ol'
+import type { Feature } from 'ol'
 import type { SetupStoreDefinition } from 'pinia'
 import type { Component, VueElement } from 'vue'
 import type { NineLayoutTag } from '@/core/utils/NineLayoutTag.ts'
