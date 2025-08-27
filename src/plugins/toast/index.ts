ed51b5b0f6f36cfe11cb844eeaf51baf56854f42
Hendrik Oenings <142312676+oeninghe-dataport@users.noreply.github.com>
2025-08-27 12:20:51 +0200

    fix(toast): do not define plugin-specific default options
    Co-authored-by: Pascal Röhling <73653210+dopenguin@users.noreply.github.com>

--- src/plugins/toast/index.ts
+++ src/plugins/toast/index.ts
@@ -23,10 +23,7 @@ export default function pluginToast(
 		component,
 		locales,
 		storeModule: useToastStore as PolarPluginStore,
-		options: {
-			displayComponent: true,
-			...options,
-		},
+		options,
 	}
 }
 
