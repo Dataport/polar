a478d2dd20944218f06506bd99daaf104c6bdfbb
Pascal Roehling <pas.roehling@gmail.com>
2025-08-29 13:11:10 +0200

    refactor: remove redundant if

--- src/lib/getCssColor.ts
+++ src/lib/getCssColor.ts
@@ -1,17 +1,17 @@
 import type { Color } from '@/core'
 
 export function getCssColor(color: Color): string {
-	if (typeof color === 'string') {
-		return color
-	}
-	if ('oklch' in color) {
-		return `oklch(${color.oklch.l}, ${color.oklch.c}, ${color.oklch.h})`
-	}
-	if ('rgba' in color) {
-		if (color.rgba.a) {
-			return `rgba(${color.rgba.r}, ${color.rgba.g}, ${color.rgba.b}, ${color.rgba.a})`
+	if (typeof color === 'object') {
+		if ('oklch' in color) {
+			return `oklch(${color.oklch.l}, ${color.oklch.c}, ${color.oklch.h})`
 		}
-		return `rgb(${color.rgba.r}, ${color.rgba.g}, ${color.rgba.b})`
+		if ('rgba' in color) {
+			if (color.rgba.a) {
+				return `rgba(${color.rgba.r}, ${color.rgba.g}, ${color.rgba.b}, ${color.rgba.a})`
+			}
+			return `rgb(${color.rgba.r}, ${color.rgba.g}, ${color.rgba.b})`
+		}
 	}
+
 	return color
 }
