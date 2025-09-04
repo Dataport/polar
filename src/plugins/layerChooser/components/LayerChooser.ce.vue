d8e232d550e6a804eb7476028cc9abf68955f6bd
Pascal Roehling <pas.roehling@gmail.com>
2025-09-04 14:46:37 +0200

    refactor(layerChooser): implement selectability for background and mask layers
    Tooltip when a layer is disabled is missing and will follow in an upcoming commit.

--- src/plugins/layerChooser/components/LayerChooser.ce.vue
+++ src/plugins/layerChooser/components/LayerChooser.ce.vue
@@ -1,16 +1,13 @@
 <template>
-	<PolarCard>
-		<LayerSelection v-if="displaySelection" />
-		<LayerOptions v-else />
-	</PolarCard>
+	<LayerSelection v-if="displaySelection" />
+	<LayerOptions v-else />
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
+import { useLayerChooserStore } from '../store'
 import LayerSelection from './LayerSelection.ce.vue'
 import LayerOptions from './LayerOptions.ce.vue'
-import PolarCard from '@/components/PolarCard.ce.vue'
-import { useLayerChooserStore } from '@/plugins/layerChooser/store.ts'
 
 const displaySelection = computed(
 	() => useLayerChooserStore().openedOptions.length === 0
--- src/plugins/layerChooser/components/LayerSelection.ce.vue
+++ src/plugins/layerChooser/components/LayerSelection.ce.vue
@@ -1,7 +1,65 @@
 <template>
-	<div>Implement me!</div>
+	<PolarCard>
+		<PolarRadioGroup
+			v-if="backgrounds.length"
+			v-model="activeBackground"
+			legend="backgroundTitle"
+			:legend-ns="PluginId"
+			:values="
+				backgrounds.map(({ name, id }) => ({
+					disabled: disabledBackgrounds[id],
+					label: name,
+					value: id,
+				}))
+			"
+		/>
+		<template v-if="shownMasks.length">
+			<PolarCheckboxes
+				v-for="[type, masks] in Object.entries(masksSeparatedByType)"
+				:key="`polar-layer-chooser-mask-${type}`"
+				v-model="activeMasks"
+				:legend="`${type}Title`"
+				:legend-ns="PluginId"
+				:values="
+					masks.map(({ name, id }) => ({
+						disabled: disabledMasks[id],
+						label: name,
+						value: id,
+					}))
+				"
+			/>
+		</template>
+	</PolarCard>
 </template>
 
 <script setup lang="ts">
-// TODO(dopenguin): Implement me!
+import { computed } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useLayerChooserStore } from '../store'
+import { PluginId } from '../types'
+import PolarCard from '@/components/PolarCard.ce.vue'
+import PolarCheckboxes from '@/components/PolarCheckboxes.ce.vue'
+import PolarRadioGroup from '@/components/PolarRadioGroup.ce.vue'
+
+const layerChooserStore = useLayerChooserStore()
+const {
+	backgrounds,
+	disabledBackgrounds,
+	disabledMasks,
+	masksSeparatedByType,
+	shownMasks,
+} = storeToRefs(layerChooserStore)
+
+const activeBackground = computed({
+	get: () => layerChooserStore.activeBackgroundId,
+	set: (newValue) => {
+		layerChooserStore.setActiveBackgroundId(newValue)
+	},
+})
+const activeMasks = computed({
+	get: () => layerChooserStore.activeMaskIds,
+	set: (newValue) => {
+		layerChooserStore.setActiveMaskIds(newValue)
+	},
+})
 </script>
