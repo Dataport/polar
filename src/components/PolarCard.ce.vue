7de9fd4d3792b9ec72598d53aa2aa36284f6da9e
Pascal Roehling <pas.roehling@gmail.com>
2025-09-04 09:25:40 +0200

    feat(core): implement card component according to design docs

--- src/components/PolarCard.ce.vue
+++ src/components/PolarCard.ce.vue
@@ -1,7 +1,28 @@
 <template>
-	<div><slot /></div>
+	<article class="kern-card kern-card--small">
+		<div class="kern-card__container">
+			<slot />
+		</div>
+	</article>
 </template>
 
 <script setup lang="ts">
-// TODO(dopenguin): Implement me!
+// NOTE: This comment is needed to have the <script>-tag not empty
 </script>
+
+<style scoped>
+.kern-card {
+	height: inherit;
+	min-width: 300px;
+	border-radius: 0.5rem;
+	box-shadow:
+		0 1px 1px 0 rgba(53, 57, 86, 0.16),
+		0 1px 2px 0 rgba(53, 57, 86, 0.25),
+		0 1px 6px 0 rgba(110, 117, 151, 0.25);
+
+	.kern-card__container {
+		padding: var(--kern-metric-space-small);
+		padding-bottom: 0.625rem;
+	}
+}
+</style>
