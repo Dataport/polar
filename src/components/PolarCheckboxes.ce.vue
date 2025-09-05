2cead99df42e0ad0cff1096e0fcc5465e135e538
Pascal Roehling <pas.roehling@gmail.com>
2025-09-05 14:05:33 +0200

    refactor: add a slot for e.g. an option button the checkboxes

--- src/components/PolarCheckboxes.ce.vue
+++ src/components/PolarCheckboxes.ce.vue
@@ -8,6 +8,7 @@
 				v-for="({ label, value, disabled }, index) in $props.values"
 				:key="`${value}-${index}`"
 				class="kern-form-check"
+				:style="horizontalSpacing"
 			>
 				<input
 					:id="`polar-checkbox-${value}-${index}`"
@@ -21,11 +22,14 @@
 				<label class="kern-label" :for="`polar-checkbox-${value}-${index}`">
 					{{ label }}
 				</label>
+				<slot />
 			</div>
 		</div>
 	</fieldset>
 </template>
 <script setup lang="ts">
+import { computed, useSlots } from 'vue'
+
 defineProps<{
 	legend: string
 	legendNs: string
@@ -37,6 +41,11 @@ defineProps<{
 	}[]
 }>()
 const model = defineModel<string[]>({ required: true })
+
+const horizontalSpacing = computed(
+	() =>
+		`justify-content: ${Object.hasOwnProperty.call(useSlots(), 'default') ? 'space-between' : 'flex-start'};`
+)
 </script>
 
 <style scoped>
@@ -45,6 +54,8 @@ const model = defineModel<string[]>({ required: true })
 }
 
 .kern-form-check {
+	display: flex;
+	align-items: center;
 	gap: var(--kern-metric-space-large, 1.5rem);
 	padding-left: 0.75rem;
 	padding-right: 0;
