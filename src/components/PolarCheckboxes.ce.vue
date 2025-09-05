e9ff7b0d0a5171cdebd2ea81267cb5975315ad39
Pascal Roehling <pas.roehling@gmail.com>
2025-09-05 14:29:54 +0200

    refactor: add the possibility to add a button directly to component instead of a slot

--- src/components/PolarCheckboxes.ce.vue
+++ src/components/PolarCheckboxes.ce.vue
@@ -8,7 +8,9 @@
 				v-for="({ label, value, disabled }, index) in $props.values"
 				:key="`${value}-${index}`"
 				class="kern-form-check"
-				:style="horizontalSpacing"
+				:class="{
+					'polar-form-check-has-options': $props.options !== undefined,
+				}"
 			>
 				<input
 					:id="`polar-checkbox-${value}-${index}`"
@@ -22,14 +24,24 @@
 				<label class="kern-label" :for="`polar-checkbox-${value}-${index}`">
 					{{ label }}
 				</label>
-				<slot />
+				<button
+					v-if="$props.options"
+					class="kern-btn kern-btn--tertiary"
+					:disabled="disabled"
+				>
+					<span
+						:class="`kern-icon ${$props.options.icon}`"
+						aria-hidden="true"
+					/>
+					<span class="kern-label kern-sr-only">
+						{{ $t($props.options.label, { ns: $props.localeNs }) }}
+					</span>
+				</button>
 			</div>
 		</div>
 	</fieldset>
 </template>
 <script setup lang="ts">
-import { computed, useSlots } from 'vue'
-
 defineProps<{
 	legend: string
 	localeNs: string
@@ -39,13 +51,12 @@ defineProps<{
 		value: string
 		disabled?: boolean
 	}[]
+	options?: {
+		icon: string
+		label: string
+	}
 }>()
 const model = defineModel<string[]>({ required: true })
-
-const horizontalSpacing = computed(
-	() =>
-		`justify-content: ${Object.hasOwnProperty.call(useSlots(), 'default') ? 'space-between' : 'flex-start'};`
-)
 </script>
 
 <style scoped>
@@ -55,9 +66,21 @@ const horizontalSpacing = computed(
 
 .kern-form-check {
 	display: flex;
+	justify-content: flex-start;
 	align-items: center;
 	gap: var(--kern-metric-space-large, 1.5rem);
 	padding-left: 0.75rem;
 	padding-right: 0;
+}
+
+.polar-form-check-has-options {
+	justify-content: space-between;
+}
+
+.kern-btn {
+	width: var(--kern-metric-dimension-large);
+	min-height: var(--kern-metric-dimension-large);
+	border: none;
+	border-radius: 50%;
 }
 </style>
