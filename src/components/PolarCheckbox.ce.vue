8dcad0d2502f20b33366a46aee966ee93e728df3
Pascal Roehling <pas.roehling@gmail.com>
2025-09-09 15:02:35 +0200

    refactor(core): update reusable components to use less redundant code
    This also eases the possibility of adding additional functionality without bloating the components.

---
+++ src/components/PolarCheckbox.ce.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="kern-form-check">
+		<input
+			:id="`polar-checkbox-${value}-${idSuffix}`"
+			v-model="model"
+			class="kern-form-check__checkbox"
+			:name="value"
+			:value="value"
+			type="checkbox"
+			:disabled="disabled"
+		/>
+		<label class="kern-label" :for="`polar-checkbox-${value}-${idSuffix}`">
+			{{ label }}
+		</label>
+	</div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+	idSuffix: string
+	label: string
+	value: string
+	disabled?: boolean
+}>()
+
+const model = defineModel<string[]>({ required: true })
+</script>
---
+++ src/components/PolarInputGroup.ce.vue
@@ -0,0 +1,31 @@
+<template>
+	<fieldset class="kern-fieldset">
+		<legend class="kern-label">
+			{{ $t(legend, legendOptions) }}
+		</legend>
+		<div class="kern-fieldset__body">
+			<slot />
+		</div>
+	</fieldset>
+</template>
+<script setup lang="ts">
+import { type TOptions } from 'i18next'
+
+defineProps<{
+	legend: string
+	legendOptions?: TOptions
+}>()
+</script>
+
+<style scoped>
+.kern-label {
+	padding-left: 0.75rem;
+}
+
+:deep(.kern-form-check) {
+	align-items: center;
+	gap: var(--kern-metric-space-large, 1.5rem);
+	margin-left: 0.75rem;
+	padding-right: 0;
+}
+</style>
