d482ba0681d03490104bb8c436c3d0d5041fafde
Pascal Roehling <pas.roehling@gmail.com>
2025-09-10 19:58:33 +0200

    chore: remove todo meant for another branch

--- src/core/components/PolarMap.ce.vue
+++ src/core/components/PolarMap.ce.vue
@@ -105,7 +105,6 @@ function wheelEffect(event: WheelEvent) {
 
 onMounted(async () => {
 	if (typeof mainStore.serviceRegister === 'string') {
-		// TODO: Add a general loader that shows until this is resolved - dem LLamas need food
 		mainStore.serviceRegister = await new Promise<Record<string, unknown>[]>(
 			(resolve) =>
 				rawLayerList.initializeLayerList(mainStore.serviceRegister, resolve)
