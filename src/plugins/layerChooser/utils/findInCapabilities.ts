d6518e0eeb956aafd9df2b7de576aa05f261e836
Pascal Roehling <pas.roehling@gmail.com>
2025-09-10 17:27:19 +0200

    refactor(layerChooser): move findCapabilities

--- src/plugins/layerChooser/utils/findInCapabilities.ts
+++ src/plugins/layerChooser/utils/findInCapabilities.ts
@@ -2,6 +2,8 @@
  * https://portal.ogc.org/files/?artifact_id=14416
  * OL currently has no TS support for its return object, hence :any'ing here */
 
+// TODO(dopenguin): Check if this can be written anew
+
 /**
  * Finds a named layer from a root layer (array). First-found is returned,
  * assuming that not multiple layers will have the same name, since they're a
@@ -13,20 +15,20 @@
  * @returns capabilities layer with matching name
  */
 const deepLayerFind = (layer, name: string) => {
-  if (Array.isArray(layer)) {
-    return (
-      layer.map((l) => deepLayerFind(l, name)).find((l) => l !== null) || null
-    )
-  } else if (typeof layer === 'object') {
-    if (layer.Name === name) {
-      return layer
-    } else if (layer.Layer) {
-      return deepLayerFind(layer.Layer, name)
-    }
-  }
+	if (Array.isArray(layer)) {
+		return (
+			layer.map((l) => deepLayerFind(l, name)).find((l) => l !== null) || null
+		)
+	} else if (typeof layer === 'object') {
+		if (layer.Name === name) {
+			return layer
+		} else if (layer.Layer) {
+			return deepLayerFind(layer.Layer, name)
+		}
+	}
 
-  // layer is minOccurs="0", so we may always end up empty-handed
-  return null
+	// layer is minOccurs="0", so we may always end up empty-handed
+	return null
 }
 
 /**
@@ -34,16 +36,16 @@ const deepLayerFind = (layer, name: string) => {
  * @returns array of all found legend URLs
  */
 const getAllLegendURLs = (style): string[] =>
-  (Array.isArray(style) ? style : [style])
-    .map((styleObject) =>
-      (Array.isArray(styleObject.LegendURL)
-        ? styleObject.LegendURL
-        : typeof styleObject.LegendURL === 'object'
-        ? [styleObject.LegendURL]
-        : []
-      ).map((legendUrl) => legendUrl.OnlineResource)
-    )
-    .flat(1)
+	(Array.isArray(style) ? style : [style])
+		.map((styleObject) =>
+			(Array.isArray(styleObject.LegendURL)
+				? styleObject.LegendURL
+				: typeof styleObject.LegendURL === 'object'
+					? [styleObject.LegendURL]
+					: []
+			).map((legendUrl) => legendUrl.OnlineResource)
+		)
+		.flat(1)
 
 /**
  * @param capabilities - capabilities from ol/format/WMSCapabilities
@@ -51,11 +53,11 @@ const getAllLegendURLs = (style): string[] =>
  * @returns title, or empty string if not found
  */
 export const findLayerTitleInCapabilitiesByName = (
-  capabilities,
-  name: string
+	capabilities,
+	name: string
 ): string => {
-  const layer = deepLayerFind(capabilities.Capability.Layer, name)
-  return layer?.Title || ''
+	const layer = deepLayerFind(capabilities.Capability.Layer, name)
+	return layer?.Title || ''
 }
 
 /**
@@ -64,15 +66,15 @@ export const findLayerTitleInCapabilitiesByName = (
  * @returns legend URL as string, or empty string if not found
  */
 export const findLegendUrlInCapabilitiesByName = (
-  capabilities,
-  name: string
+	capabilities,
+	name: string
 ): string => {
-  const layer = deepLayerFind(capabilities.Capability.Layer, name)
-  const style = layer?.Style
-  if (!style) {
-    return ''
-  }
-  const urls: string[] = getAllLegendURLs(style)
-  // NOTE: choosing URL is more complex when supporting layer styles
-  return urls[0] || ''
+	const layer = deepLayerFind(capabilities.Capability.Layer, name)
+	const style = layer?.Style
+	if (!style) {
+		return ''
+	}
+	const urls: string[] = getAllLegendURLs(style)
+	// NOTE: choosing URL is more complex when supporting layer styles
+	return urls[0] || ''
 }
