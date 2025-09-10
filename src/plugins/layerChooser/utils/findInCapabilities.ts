8738579d50b81a91533ee31d1f50c80a2735cccd
Pascal Roehling <pas.roehling@gmail.com>
2025-09-10 18:01:10 +0200

    chore(layerChooser): update function signatures

--- src/plugins/layerChooser/utils/findInCapabilities.ts
+++ src/plugins/layerChooser/utils/findInCapabilities.ts
@@ -1,20 +1,20 @@
 /* NOTE: dig up from Capabilities by OGC WMS Capabilities specification E.1 in
  * https://portal.ogc.org/files/?artifact_id=14416
- * OL currently has no TS support for its return object, hence :any'ing here */
-
-// TODO(dopenguin): Check if this can be written anew
+ * OL currently has no TS support for its return object, hence :any'ing here
+ */
 
 /**
  * Finds a named layer from a root layer (array). First-found is returned,
  * assuming that not multiple layers will have the same name, since they're a
  * distinguishing feature for layer enabling/disabling via URL. Layers can be
  * nested arbitrarily deep.
- * NOTE Should we start doing this a lot, consider memoization
- * @param layer - layer from ol/format/WMSCapabilities
- * @param name - name to search for
- * @returns capabilities layer with matching name
+ * NOTE: Should we start doing this a lot, consider memoization.
+ *
+ * @param layer - layer from ol/format/WMSCapabilities.
+ * @param name - name to search for.
+ * @returns capabilities layer with matching name.
  */
-const deepLayerFind = (layer, name: string) => {
+function deepLayerFind(layer, name: string) {
 	if (Array.isArray(layer)) {
 		return (
 			layer.map((l) => deepLayerFind(l, name)).find((l) => l !== null) || null
@@ -32,8 +32,8 @@ const deepLayerFind = (layer, name: string) => {
 }
 
 /**
- * @param style - style of a layer from ol/format/WMSCapabilities
- * @returns array of all found legend URLs
+ * @param style - style of a layer from ol/format/WMSCapabilities.
+ * @returns array of all found legend URLs.
  */
 const getAllLegendURLs = (style): string[] =>
 	(Array.isArray(style) ? style : [style])
@@ -48,27 +48,21 @@ const getAllLegendURLs = (style): string[] =>
 		.flat(1)
 
 /**
- * @param capabilities - capabilities from ol/format/WMSCapabilities
- * @param name - name of the layer to find title for
- * @returns title, or empty string if not found
+ * @param capabilities - capabilities from ol/format/WMSCapabilities.
+ * @param name - name of the layer to find title for.
+ * @returns title, or empty string if not found.
  */
-export const findLayerTitleInCapabilitiesByName = (
-	capabilities,
-	name: string
-): string => {
+export function findLayerTitleInCapabilitiesByName(capabilities, name: string) {
 	const layer = deepLayerFind(capabilities.Capability.Layer, name)
 	return layer?.Title || ''
 }
 
 /**
- * @param capabilities - capabilities from ol/format/WMSCapabilities
- * @param name - name of the layer to find legendURL for
- * @returns legend URL as string, or empty string if not found
+ * @param capabilities - capabilities from ol/format/WMSCapabilities.
+ * @param name - name of the layer to find legendURL for.
+ * @returns legend URL as string, or empty string if not found.
  */
-export const findLegendUrlInCapabilitiesByName = (
-	capabilities,
-	name: string
-): string => {
+export function findLegendUrlInCapabilitiesByName(capabilities, name: string) {
 	const layer = deepLayerFind(capabilities.Capability.Layer, name)
 	const style = layer?.Style
 	if (!style) {
