9e2e34ac46eaa14db4d9eb1da1ebc0465fe149f9
Pascal Roehling <pas.roehling@gmail.com>
2025-09-19 20:37:55 +0200

    refactor(loadingIndicator): add locales

--- src/plugins/loadingIndicator/locales.ts
+++ src/plugins/loadingIndicator/locales.ts
@@ -1,8 +1,12 @@
 import type { Locale } from '@/core'
 
-export const resourcesDe = {} as const
+export const resourcesDe = {
+	loading: 'Lädt...',
+} as const
 
-export const resourcesEn = {} as const
+export const resourcesEn = {
+	loading: 'Loading...',
+} as const
 
 // first type will be used as fallback language
 const locales: Locale[] = [
