899e38d026699f467b1c840e4949927482dbab58
Hendrik Oenings <hendrik.oenings@dataport.de>
2025-09-22 12:54:49 +0200

    refactor(iconMenu): remove pluginStore from store

--- src/plugins/iconMenu/store.ts
+++ src/plugins/iconMenu/store.ts
@@ -9,7 +9,6 @@ import { defineStore } from 'pinia'
 import { type Component, computed, markRaw, ref } from 'vue'
 import type { Menu } from './types.ts'
 import { useCoreStore } from '@/core/stores/export.ts'
-import { usePluginStore } from '@/core/stores/plugin.ts'
 
 /* eslint-disable tsdoc/syntax */
 /**
@@ -20,7 +19,6 @@ import { usePluginStore } from '@/core/stores/plugin.ts'
 /* eslint-enable tsdoc/syntax */
 export const useIconMenuStore = defineStore('plugins/iconMenu', () => {
 	const coreStore = useCoreStore()
-	const pluginStore = usePluginStore()
 
 	const menus = ref<Menu[]>([])
 	const open = ref(-1)
@@ -39,7 +37,7 @@ export const useIconMenuStore = defineStore('plugins/iconMenu', () => {
 			}
 		)
 		menus.value.forEach(({ plugin }) => {
-			pluginStore.addPlugin(toMerged(plugin, { independent: false }))
+			coreStore.addPlugin(toMerged(plugin, { independent: false }))
 		})
 		// Otherwise, the component itself is made reactive
 		menus.value.map((menuItem) =>
