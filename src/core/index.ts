fafeae0163b9501a2e07ebea7702b23c6cf74697
Hendrik Oenings <hendrik.oenings@dataport.de>
2025-10-07 14:09:16 +0200

    refactor(core): split exported utils to separate files

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,7 +2,7 @@
 /**
  * This is the main export for the NPM package \@polar/polar.
  *
- * Lost? You probably want to start at {@link register} and {@link createMap}.
+ * Lost? You probably want to start at {@link createMap}.
  *
  * @packageDocumentation
  * @module \@polar/polar
@@ -10,85 +10,12 @@
 /* eslint-enable tsdoc/syntax */
 
 import '@kern-ux/native/dist/fonts/fira-sans.css'
-import { defineCustomElement, watch, type WatchOptions } from 'vue'
+import { defineCustomElement } from 'vue'
 import PolarContainer from './components/PolarContainer.ce.vue'
 import { I18Next } from './vuePlugins/i18next'
 import { Pinia } from './vuePlugins/pinia'
-import type {
-	BundledPluginId,
-	BundledPluginStores,
-	MapConfiguration,
-	MasterportalApiServiceRegister,
-	PluginContainer,
-	PluginId,
-	PolarPluginStore,
-} from './types'
 
 import './monkeyHeaderLoader'
-import type { useCoreStore } from './stores/export'
-import { fetchServiceRegister } from './utils/fetchServiceRegister'
-
-/**
- * Calls `addPlugin` for each plugin in the array.
- *
- * @param map - Map to add the plugin to.
- * @param plugins - Plugins to be added.
- */
-export function addPlugins(
-	map: typeof PolarContainer,
-	plugins: PluginContainer[]
-) {
-	plugins.forEach((plugin) => {
-		addPlugin(map, plugin)
-	})
-}
-
-/**
- * Before instantiating the map, all required plugins have to be added. Depending on how you use POLAR, this may
- * already have been done. Ready-made clients (that is, packages prefixed `@polar/client-`) come with plugins prepared.
- *
- * You may add further plugins or proceed with `createMap`.
- *
- * Please note that the order of certain plugins is relevant when other plugins are referenced,
- * e.g. `@polar/plugin-gfi`'s `coordinateSources` requires the configured sources to have previously been set up.
- *
- * In case you're integrating new plugins, call `addPlugin` with a plugin instance.
- *
- * If you want to add multiple plugins at once, you can use `addPlugins` instead.
- *
- * @example
- * ```
- * addPlugin(Plugin(pluginOptions: PluginOptions))
- * ```
- *
- * @remarks
- * In case you're writing a new plugin, it must fulfill the following API:
- * ```
- * const Plugin = (options: PluginOptions): PluginContainer => ({
- * 	id,
- * 	component,
- * 	locales,
- * 	options,
- * 	storeModule,
- * })
- * ```
- *
- * @param map - Map to add the plugin to.
- * @param plugin - Plugin to be added.
- */
-export function addPlugin(map: typeof PolarContainer, plugin: PluginContainer) {
-	map.store.addPlugin(plugin)
-}
-
-/**
- * Remove a plugin from a map by its ID.
- *
- * @param map - Map to remove the plugin from.
- * @param pluginId - ID of the plugin to be removed.
- */
-export function removePlugin(map: typeof PolarContainer, pluginId: string) {
-	map.store.removePlugin(pluginId)
-}
 
 /**
  * Custom element of the POLAR map.
@@ -112,146 +39,10 @@ export function register() {
 	customElements.define('polar-map', PolarMap)
 }
 
-/**
- * Creates an HTML map element with a given configuration.
- *
- * Instead of calling this function, you may also create the element yourself.
- * Creating the element yourself yields benefits especially when you use a framework
- * that is more used to creating elements itself and adding properties to them.
- *
- * Remember to always call `register` first.
- *
- * @privateRemarks
- * In earlier versions of POLAR, this function did a lot of magic.
- * However, the magic moved to the custom element itself, therefore, you may create the element by yourself now.
- *
- * @param mapConfiguration - Configuration options.
- * @param serviceRegister - Service register.
- */
-export function createMapElement(
-	mapConfiguration: MapConfiguration,
-	serviceRegister: MasterportalApiServiceRegister
-) {
-	// @ts-expect-error | We trust that the element is registered
-	const map = document.createElement('polar-map') as typeof PolarContainer
-	map.mapConfiguration = mapConfiguration
-	map.serviceRegister = serviceRegister
-	return map
-}
-
-/**
- * Creates an HTML map element with a given configuration and inserts this at a given ID.
- *
- * This is a convenience function that combines `register`, `createMap` and `fetchServiceRegister`.
- *
- * It inserts the map element by replacing the element with the given ID.
- * The ID and the classes of the container are transferred to the map element.
- *
- * @param mapConfiguration - Configuration options.
- * @param serviceRegister - Service register given as an array, or an URL to fetch this from.
- */
-export async function createMap(
-	containerId: string,
-	mapConfiguration: MapConfiguration,
-	serviceRegister: MasterportalApiServiceRegister | string
-) {
-	if (!customElements.get('polar-map')) {
-		register()
-	}
-
-	if (typeof serviceRegister === 'string') {
-		serviceRegister = await fetchServiceRegister(serviceRegister)
-	}
-
-	const map = createMapElement(mapConfiguration, serviceRegister)
-
-	const container = document.getElementById(containerId)
-	if (!container) {
-		throw new Error(`Container with ID '${containerId}' not found`)
-	}
-	map.id = container.id
-	container.classList.forEach((c) => map.classList.add(c))
-	container.replaceWith(map as unknown as HTMLElement)
-	return map
-}
-
-export type SubscribeCallback = (value: unknown, oldValue: unknown) => void
-
-type StoreType<T extends 'core' | BundledPluginId> = T extends BundledPluginId
-	? ReturnType<BundledPluginStores<T>>
-	: ReturnType<typeof useCoreStore>
-type ExtractStateAndGetters<T extends { $state: unknown }> =
-	| keyof T['$state']
-	| {
-			[K in keyof T]: T[K] extends (..._args) => unknown
-				? never
-				: K extends `$${string}` | `_${string}`
-					? never
-					: K
-	  }[keyof T]
-type StoreId = 'core' | PluginId
-type StoreParameter<T extends StoreId> = T extends 'core' | BundledPluginId
-	? ExtractStateAndGetters<StoreType<T>>
-	: string
-
-/**
- * Returns the store module for the core or for an active plugin.
- *
- * @param map - Map to get the corresponding store for.
- * @param storeName - Either `'core'` for the core store or the plugin ID for a plugin's store.
- * @returns Core store for the map if `'core'` is given, or the plugin's store else.
- */
-export function getStore<T extends StoreId>(
-	map: typeof PolarContainer,
-	storeName: T
-): T extends 'core' | BundledPluginId ? StoreType<T> : PolarPluginStore {
-	return storeName === 'core' ? map.store : map.store.getPluginStore(storeName)
-}
-
-/**
- * Subscribe to a store value of the core store or any plugin's store.
- *
- * @param map - Map to subscribe the value at.
- * @param storeName - Either `'core'` for the core store or the plugin ID for a plugin's store.
- * @param parameterName - Name of the parameter to update.
- * @param callback - Function to call on updates.
- * @param options - Additional options to give to `watch`.
- */
-export function subscribe<T extends StoreId>(
-	map: typeof PolarContainer,
-	storeName: T,
-	parameterName: StoreParameter<T>,
-	callback: SubscribeCallback,
-	options?: WatchOptions
-) {
-	const store = getStore(map, storeName)
-	// @ts-expect-error | Parameter name is checked, but TS does not infer this
-	return watch(() => store[parameterName], callback, {
-		immediate: true,
-		...options,
-	})
-}
-
-/**
- * Updates the parameter {@link parameterName | parameter} in the {@link storeName | store} with the {@link payload}.
- *
- * @param map - Map to update the value at.
- * @param storeName - Either `'core'` for the core store or the plugin ID for a plugin's store.
- * @param parameterName - Name of the parameter to update.
- * @param payload - The payload to update the given parameter with.
- */
-export function updateState<T extends StoreId>(
-	map: typeof PolarContainer,
-	storeName: T,
-	parameterName: StoreParameter<T>,
-	payload: unknown
-) {
-	const store = getStore(map, storeName)
-	// @ts-expect-error | Parameter name is checked, but TS does not infer this
-	store[parameterName] = payload
-}
-
 export { fetchServiceRegister } from './utils/fetchServiceRegister'
+export * from './utils/export/createMap'
+export * from './utils/export/plugin'
+export * from './utils/export/store'
 
 export type * from './types'
 export { type PolarContainer }
