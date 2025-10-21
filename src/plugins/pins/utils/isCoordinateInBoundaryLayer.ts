2e2ced579c0773405b9e8254e5618e7c1877e578
Pascal Roehling <pas.roehling@gmail.com>
2025-10-21 11:21:25 +0200

    refactor(pins): add implementation to check whether a coordinate is in the boundary of a layer

--- src/plugins/pins/utils/isCoordinateInBoundaryLayer.ts
+++ src/plugins/pins/utils/isCoordinateInBoundaryLayer.ts
@@ -1,13 +1,16 @@
+import { t } from 'i18next'
 import type { Map } from 'ol'
 import type { Coordinate } from 'ol/coordinate'
 import type { PinBoundary } from '../types'
+import { notifyUser } from '@/lib/notifyUser'
+import { passesBoundaryCheck } from '@/lib/passesBoundaryCheck'
 
 /**
  * Checks if boundary layer conditions are met; returns false if not and
  * toasts to the user about why the action was blocked, if `toastAction` is
  * configured. If no boundaryLayer configured, always returns true.
  */
-export function isCoordinateInBoundaryLayer(
+export async function isCoordinateInBoundaryLayer(
 	coordinate: Coordinate,
 	map: Map,
 	boundary?: PinBoundary
@@ -15,10 +18,7 @@ export function isCoordinateInBoundaryLayer(
 	if (!boundary) {
 		return true
 	}
-	console.warn('coordinate', coordinate, map)
-	return true
-	// TODO(dopenguin): passesBoundaryCheck is being migrated on vue3/migration-plugin-geo-location. Uncomment afterwards.
-	/* const boundaryCheckResult = await passesBoundaryCheck(
+	const boundaryCheckResult = await passesBoundaryCheck(
 		map,
 		boundary.layerId,
 		coordinate
@@ -32,12 +32,12 @@ export function isCoordinateInBoundaryLayer(
 	}
 
 	if (typeof boundaryCheckResult === 'symbol') {
-		notifyUser('error', 'boundaryError', { ns: 'pins' })
+		notifyUser('error', () => t(($) => $.boundaryError, { ns: 'pins' }))
 		console.error('Checking boundary layer failed.')
 	} else {
-		notifyUser('info', 'notInBoundary', { ns: 'pins' })
+		notifyUser('info', () => t(($) => $.notInBoundary, { ns: 'pins' }))
 		// eslint-disable-next-line no-console
 		console.info('Pin position outside of boundary layer:', coordinate)
 	}
-	return false */
+	return false
 }
