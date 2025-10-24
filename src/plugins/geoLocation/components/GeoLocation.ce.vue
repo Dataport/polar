78b4fbf752191c8cb07d5ac05bb86a2e3d8e1750
Pascal Roehling <pas.roehling@gmail.com>
2025-10-24 10:16:08 +0200

    refactor(geoLocation): update tooltipPosition to change depending on configuration

--- src/plugins/geoLocation/components/GeoLocation.ce.vue
+++ src/plugins/geoLocation/components/GeoLocation.ce.vue
@@ -2,7 +2,6 @@
 	<PolarIconButton
 		v-if="state !== 'DISABLED'"
 		:action="action"
-		:class="layout === 'standard' ? 'polar-plugin-geoLocation-standard' : ''"
 		hint="button.tooltip"
 		:hint-namespace="PluginId"
 		:icon="icon"
@@ -11,21 +10,27 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
 import { storeToRefs } from 'pinia'
+import { computed } from 'vue'
 import { useGeoLocationStore } from '../store'
 import { PluginId } from '../types'
-import { useCoreStore } from '@/core/stores/export.ts'
 import PolarIconButton from '@/components/PolarIconButton.ce.vue'
+import { useCoreStore } from '@/core/stores/export'
 
 const { layout } = storeToRefs(useCoreStore())
-const { action, state } = storeToRefs(useGeoLocationStore())
-
-// TODO: this can't be a fixed value, but depends on the environment
-const tooltipPosition = 'left'
+const geoLocationStore = useGeoLocationStore()
+const { action, state } = storeToRefs(geoLocationStore)
+// TODO: Place plugin in IconMenu; make it work if NineRegions if it is independent
 
 const icon = computed(() =>
 	state.value === 'LOCATED' ? 'kern-icon-fill--near-me' : 'kern-icon--near-me'
+)
+const tooltipPosition = computed(() =>
+	layout.value === 'standard' ||
+	(geoLocationStore.configuration.renderType === 'independent' &&
+		geoLocationStore.configuration.layoutTag?.includes('RIGHT'))
+		? 'left'
+		: 'right'
 )
 </script>
 
