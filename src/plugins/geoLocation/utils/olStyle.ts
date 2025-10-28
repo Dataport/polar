3ec76249367e565413ede3b5476999a760895a58
Pascal Roehling <pas.roehling@gmail.com>
2025-10-28 22:22:21 +0100

    refactor(geoLocation): shorten olStyle util

--- src/plugins/geoLocation/utils/olStyle.ts
+++ src/plugins/geoLocation/utils/olStyle.ts
@@ -1,18 +1,18 @@
-import type { Feature } from 'ol'
-import { Circle, Fill, RegularShape, Stroke, Style } from 'ol/style.js'
+import type { FeatureLike } from 'ol/Feature'
+import Circle from 'ol/style/Circle'
+import Fill from 'ol/style/Fill'
+import RegularShape from 'ol/style/RegularShape'
+import Stroke from 'ol/style/Stroke'
+import Style from 'ol/style/Style'
 
-/**
- * All those years of academy training ... finally put to good use.
- * @internal
- */
 function createLinearGradient(radians: number, radius: number) {
 	const sideLength = (radius / 2) * Math.sqrt(3)
 
-	const canvas = document.createElement('canvas')
-	const context = canvas.getContext('2d')
-	// It was revealed to me in a dream.
-	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-	const gradient = context!.createLinearGradient(
+	const gradient = (
+		document
+			.createElement('canvas')
+			.getContext('2d') as CanvasRenderingContext2D
+	).createLinearGradient(
 		0,
 		-sideLength,
 		Math.sin(radians),
@@ -24,65 +24,44 @@ function createLinearGradient(radians: number, radius: number) {
 	return gradient
 }
 
-/*
-	// I tried so hard and got so far
-	// But in the end, it didn't stop to jitter
-	const sizes = Array.from({ length: 17 }, (_, i) => 9 + i * 0.0625)
-	sizes.push(...[...sizes].reverse())
-	let pointer = 0
+function dropDirectionalShadow(feature: FeatureLike) {
+	if (typeof feature.get('heading') === 'undefined') {
+		return new Style()
+	}
 
-	setInterval(() => {
-		const inner = sizes[pointer++] ?? 0
-		image.setRadius(inner)
-		stroke.setWidth(fullRadius - inner)
-		layer.setStyle(style)
-		if (pointer >= sizes.length) {
-			pointer = 0
-		}
-	}, 100)
-	// when retrying, you also need to rerender somehow (e.g. re-setting feature style)
-	*/
+	const radius = 42
+	const heading = Math.PI - feature.get('heading')
 
-export const getGeoLocationStyle = () => {
-	const strokeWidth = 2
-	const circleRadius = 8
+	return new Style({
+		image: new RegularShape({
+			points: 3,
+			radius,
+			rotation: heading,
+			fill: new Fill({ color: createLinearGradient(heading, radius) }),
+			displacement: [0, -(radius - 8)],
+		}),
+	})
+}
 
+export function getGeoLocationStyle() {
 	const fill = new Fill({
 		color: '#0078D4', // polar-blue/500 – TODO: take from uhhh style system?
 	})
 	const stroke = new Stroke({
 		color: '#FFFFFF',
-		width: strokeWidth,
-	})
-	const image = new Circle({
-		fill,
-		stroke,
-		radius: circleRadius,
-	})
-	const style = new Style({
-		image,
-		fill,
-		stroke,
+		width: 2,
 	})
 
-	const dropDirectionalShadow = (feature: Feature, radius = 42) => {
-		if (typeof feature.get('heading') === 'undefined') {
-			return new Style()
-		}
-
-		const heading = Math.PI - feature.get('heading')
-
-		return new Style({
-			// it would be so cool if i had a trapezoid and not just a triangle
-			image: new RegularShape({
-				points: 3,
-				radius,
-				rotation: heading,
-				fill: new Fill({ color: createLinearGradient(heading, radius) }),
-				displacement: [0, -(radius - circleRadius)],
+	return (feature: FeatureLike) => [
+		dropDirectionalShadow(feature),
+		new Style({
+			image: new Circle({
+				fill,
+				stroke,
+				radius: 8,
 			}),
-		})
-	}
-
-	return (feature) => [dropDirectionalShadow(feature), style]
+			fill,
+			stroke,
+		}),
+	]
 }
