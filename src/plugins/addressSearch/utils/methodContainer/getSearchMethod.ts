06ba0e81832e6192ecbdda87d08b2bf8b7315892
Pascal Roehling <pas.roehling@gmail.com>
2025-11-13 20:32:51 +0100

    refactor(addressSearch): move methodContainer

---
+++ src/plugins/addressSearch/utils/methodContainer/getSearchMethod.ts
@@ -0,0 +1,30 @@
+import type { SearchMethodFunction } from '../../types'
+
+export function getMethodContainer() {
+	const methods = {}
+
+	return {
+		registerSearchMethods: (
+			additionalMethods: Record<string, SearchMethodFunction>
+		) => {
+			Object.entries(additionalMethods).forEach(([type, searchMethod]) => {
+				if (methods[type]) {
+					console.error(
+						`Method "${type}" already exists. Please choose a different name. Overrides are not allowed.`
+					)
+					return
+				}
+				methods[type] = searchMethod
+			})
+		},
+		getSearchMethod: (type: string): SearchMethodFunction => {
+			const method = methods[type]
+			if (method) {
+				return method
+			}
+			throw new Error(
+				`The given type "${type}" does not define a valid searchMethod.`
+			)
+		},
+	}
+}
