66f48afd8e7ed0456e0869383c0a8d1cbf2e1a29
Pascal Roehling <pas.roehling@gmail.com>
2025-11-24 17:58:23 +0100

    feat(footer): add entries including assignment on setup

--- src/plugins/footer/store.ts
+++ src/plugins/footer/store.ts
@@ -5,6 +5,9 @@
 /* eslint-enable tsdoc/syntax */
 
 import { acceptHMRUpdate, defineStore } from 'pinia'
+import { ref } from 'vue'
+import type { PluginContainer } from '@/core'
+import { useCoreStore } from '@/core/stores/export'
 
 /* eslint-disable tsdoc/syntax */
 /**
@@ -14,9 +17,23 @@ import { acceptHMRUpdate, defineStore } from 'pinia'
  */
 /* eslint-enable tsdoc/syntax */
 export const useFooterStore = defineStore('plugins/footer', () => {
-	function setupPlugin() {}
+	const coreStore = useCoreStore()
+
+	const entries = ref<PluginContainer[]>([])
+
+	function setupPlugin() {
+		entries.value = (coreStore.configuration.footer?.entries || []).filter(
+			({ id }) => {
+				const display = coreStore.configuration[id]?.displayComponent
+				return typeof display === 'boolean' ? display : true
+			}
+		)
+	}
 	function teardownPlugin() {}
 	return {
+		/** @internal */
+		entries,
+
 		/** @internal */
 		setupPlugin,
 
