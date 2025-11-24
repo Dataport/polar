f78529d6f3ab53fd9455214496497c9c8414941d
FinnDarge <finndarge@googlemail.com>
2025-11-24 11:07:01 +0100

    add dynamic date to staticAttributions

--- packages/clients/dish/src/mapConfigurations/mapConfigIntern.ts
+++ packages/clients/dish/src/mapConfigurations/mapConfigIntern.ts
@@ -68,7 +68,7 @@ export const mapConfigIntern = (urlParams: DishUrlParams): DishMapConfig => ({
       attributionsAlkisWms,
     ],
     staticAttributions: [
-      `<span>Geobasisdaten: © GeoBasis-DE / <a href="https://www.bkg.bund.de/">BKG</a> 2024 <a href="http://sg.geodatenzentrum.de/web_public/nutzungsbedingungen.pdf">Nutzungsbedingungen</a></span>`,
+      `<span>Geobasisdaten: © GeoBasis-DE / <a href="https://www.bkg.bund.de/">BKG</a> <YEAR> <a href="http://sg.geodatenzentrum.de/web_public/nutzungsbedingungen.pdf">Nutzungsbedingungen</a></span>`,
       '<a href="#" onclick="window.openBenutzungshinweise(true)">Benutzungshinweise</a>',
     ],
   },
