1b375b1d61074e86c5de9089398d229ca1c02a71
Pascal Roehling <pas.roehling@gmail.com>
2025-11-24 17:33:54 +0100

    feat(footer): add required configuration parameter

--- src/plugins/footer/types.ts
+++ src/plugins/footer/types.ts
@@ -1,8 +1,13 @@
-import type { PluginOptions } from '@/core'
+import type { PluginContainer, PluginOptions } from '@/core'
 
 export const PluginId = 'footer'
 
 /**
  * Plugin options for footer plugin.
  */
-export interface FooterPluginOptions extends PluginOptions {}
+export interface FooterPluginOptions extends PluginOptions {
+	/**
+	 * Plugins that are going to be directly rendered as part of the footer.
+	 */
+	entries: PluginContainer[]
+}
