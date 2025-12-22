2a937c0123530fdd7f6a75a7392e78c87da2cdd6
Pascal Roehling <pas.roehling@gmail.com>
2025-12-22 14:07:14 +0100

    refactor: add geojson related types that reflect the existence of title

--- src/core/types/utils.ts
+++ src/core/types/utils.ts
@@ -1,3 +1,10 @@
+import type {
+	Feature,
+	GeoJsonObject,
+	GeoJsonProperties,
+	Geometry,
+} from 'geojson'
+
 /**
  * Copied from https://stackoverflow.com/a/54178819.
  *
@@ -6,3 +13,21 @@
  * @example `PartialBy<LayerConfiguration, 'id' | 'name'>`
  */
 export type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+
+type UsedGeometry = Exclude<Geometry, 'GeometryCollection'>
+
+export interface PolarFeature<
+	G extends UsedGeometry | null = UsedGeometry,
+	P = GeoJsonProperties,
+> extends Omit<Feature<G, P>, 'type'> {
+	title: string
+	type: 'Feature' | 'reverse_geocoded'
+}
+
+export interface PolarFeatureCollection<
+	G extends UsedGeometry | null = UsedGeometry,
+	P = GeoJsonProperties,
+> extends GeoJsonObject {
+	features: Array<PolarFeature<G, P>>
+	type: 'FeatureCollection'
+}
