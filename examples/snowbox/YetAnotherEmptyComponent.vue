15972bfff219673bb5a3ba109cbaae995271fcf1
Hendrik Oenings <hendrik.oenings@dataport.de>
2026-01-13 17:24:02 +0100

    test(snowbox): allow jumping to Hamburg via button

--- examples/snowbox/YetAnotherEmptyComponent.vue
+++ examples/snowbox/YetAnotherEmptyComponent.vue
@@ -1,7 +1,17 @@
 <template>
-	<div style="background: #fff">Awesome</div>
+	<div style="background: #fff; z-index: 99999; pointer-events: all">
+		Awesome
+		<button @click="goHamburg()">Go to Hamburg</button>
+	</div>
 </template>
 
 <script setup lang="ts">
-// keep this block to enforce language
+// eslint-disable-next-line
+// @ts-nocheck
+
+const coreStore = document.getElementById('snowbox').store
+
+function goHamburg() {
+	coreStore.center = [561210, 5932600]
+}
 </script>
