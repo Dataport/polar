8c4e9c991e617210435d22c519d6574c54ef6f6a
bruehlca <carina.bruehl@dataport.de>
2026-01-16 12:23:55 +0100

    prevent linting error

--- packages/clients/dish/src/utils/watchSearchResultForAlkisSearch.ts
+++ packages/clients/dish/src/utils/watchSearchResultForAlkisSearch.ts
@@ -3,25 +3,25 @@ import { alkisWms } from '../servicesConstants'
 import { alkisMinZoom } from '../mapConfigurations/layerConfigIntern'
 
 export function watchSearchResultForAlkisSearch(instance: MapInstance) {
-  instance.subscribe('plugin/addressSearch/chosenAddress', (chosenAddress) => {
-    if (chosenAddress === null) {
-      return
-    }
-    if (chosenAddress.properties?.idflurst) {
-      const zoomLevel = instance.$store.getters['plugin/zoom/zoomLevel']
+  instance.subscribe(
+    'plugin/addressSearch/chosenAddress',
+    (chosenAddress: any) => {
+      if (chosenAddress?.properties?.idflurst) {
+        const zoomLevel = instance.$store.getters['plugin/zoom/zoomLevel']
 
-      if (zoomLevel <= alkisMinZoom) {
-        instance.$store.getters.map.getView().setZoom(alkisMinZoom)
-      }
+        if (zoomLevel <= alkisMinZoom) {
+          instance.$store.getters.map.getView().setZoom(alkisMinZoom)
+        }
 
-      const activeMaskIds =
-        instance.$store.getters['plugin/layerChooser/activeMaskIds']
-      if (!activeMaskIds.includes(alkisWms)) {
-        instance.$store.dispatch('plugin/layerChooser/setActiveMaskIds', [
-          ...activeMaskIds,
-          alkisWms,
-        ])
+        const activeMaskIds =
+          instance.$store.getters['plugin/layerChooser/activeMaskIds']
+        if (!activeMaskIds.includes(alkisWms)) {
+          instance.$store.dispatch('plugin/layerChooser/setActiveMaskIds', [
+            ...activeMaskIds,
+            alkisWms,
+          ])
+        }
       }
     }
-  })
+  )
 }
