d0b4729d436c0e5481f930e0551c6fb0fc5e7dd1
bruehlca <carina.bruehl@dataport.de>
2026-01-20 11:29:34 +0100

    delete searchResult function

--- packages/clients/dish/src/utils/alkisSearch.ts
+++ packages/clients/dish/src/utils/alkisSearch.ts
@@ -1,15 +1,4 @@
-import { Feature } from 'geojson'
-import {
-  CoreGetters,
-  CoreState,
-  PolarStore,
-  SelectResultFunction,
-} from '@polar/lib-custom-types'
-import {
-  SearchResultSymbols,
-  AddressSearchGetters,
-  AddressSearchState,
-} from '@polar/plugin-address-search'
+import { CoreGetters, CoreState, PolarStore } from '@polar/lib-custom-types'
 import { WfsParameters, getWfsFeatures } from '@polar/lib-get-features'
 
 export function alkisSearch(
@@ -19,26 +8,7 @@ export function alkisSearch(
   inputValue: string,
   queryParameters: WfsParameters
 ) {
-  const searchString = inputValue[0].toUpperCase() + inputValue.slice(1)
+  const searchString =
+    inputValue[0].toUpperCase() + inputValue.slice(1).toLowerCase()
   return getWfsFeatures(signal, url, `${searchString}*`, queryParameters)
-}
-
-export const alkisSearchResult: SelectResultFunction<
-  AddressSearchState,
-  AddressSearchGetters
-> = (
-  { commit },
-  {
-    feature,
-  }: {
-    feature: Feature & {
-      title: string
-      epsg?: string
-    }
-  }
-) => {
-  console.log('Alkis selected feature:', feature)
-  commit('setChosenAddress', feature)
-  commit('setInputValue', feature.title)
-  commit('setSearchResults', SearchResultSymbols.NO_SEARCH)
 }
