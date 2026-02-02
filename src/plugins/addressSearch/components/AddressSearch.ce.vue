9b0df8210eb55bc3a7837bd1791156d42475dbba
Pascal Roehling <pas.roehling@gmail.com>
2026-02-02 17:10:48 +0100

    refactor(addressSearch): move loader to a separate file

--- src/plugins/addressSearch/components/AddressSearch.ce.vue
+++ src/plugins/addressSearch/components/AddressSearch.ce.vue
@@ -32,12 +32,7 @@
 					@focusout="updateStatus"
 				/>
 				<!-- TODO: May be replaced with the KERN-Loader. -->
-				<div v-if="isLoading" class="loader" role="status">
-					<div />
-					<div />
-					<div />
-					<div />
-				</div>
+				<SmallLoader v-if="isLoading" />
 				<button
 					class="kern-btn kern-btn--tertiary polar-plugin-address-search-input-button"
 					@click="addressSearchStore.clear"
@@ -70,6 +65,7 @@ import { PluginId } from '../types'
 import { focusFirstResult } from '../utils/focusFirstResult'
 import GroupSelect from './GroupSelect.ce.vue'
 import SearchResults from './SearchResults.ce.vue'
+import SmallLoader from './SmallLoader.ce.vue'
 
 const coreStore = useCoreStore()
 const addressSearchStore = useAddressSearchStore()
@@ -163,46 +159,6 @@ function inputDown(event: KeyboardEvent) {
 				margin-top: var(--kern-metric-space-small);
 			}
 		}
-	}
-}
-
-.loader,
-.loader div {
-	box-sizing: border-box;
-}
-.loader {
-	position: absolute;
-	right: 3rem;
-	width: var(--kern-metric-dimension-large);
-	height: var(--kern-metric-dimension-large);
-	margin: var(--kern-metric-space-small);
-}
-.loader div {
-	box-sizing: border-box;
-	display: block;
-	position: absolute;
-	width: var(--kern-metric-dimension-large);
-	height: var(--kern-metric-dimension-large);
-	border: 4px solid currentColor;
-	border-radius: 50%;
-	animation: loader 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-	border-color: currentColor transparent transparent transparent;
-}
-.loader div:nth-child(1) {
-	animation-delay: -0.45s;
-}
-.loader div:nth-child(2) {
-	animation-delay: -0.3s;
-}
-.loader div:nth-child(3) {
-	animation-delay: -0.15s;
-}
-@keyframes loader {
-	0% {
-		transform: rotate(0deg);
-	}
-	100% {
-		transform: rotate(360deg);
 	}
 }
 </style>
---
+++ src/plugins/addressSearch/components/SmallLoader.ce.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class="loader" role="status">
+		<div />
+		<div />
+		<div />
+		<div />
+	</div>
+</template>
+
+<script setup lang="ts">
+// Appease linter
+</script>
+
+<style scoped>
+.loader,
+.loader div {
+	box-sizing: border-box;
+}
+.loader {
+	position: absolute;
+	right: 3rem;
+	width: var(--kern-metric-dimension-large);
+	height: var(--kern-metric-dimension-large);
+	margin: var(--kern-metric-space-small);
+}
+.loader div {
+	box-sizing: border-box;
+	display: block;
+	position: absolute;
+	width: var(--kern-metric-dimension-large);
+	height: var(--kern-metric-dimension-large);
+	border: 4px solid currentColor;
+	border-radius: 50%;
+	animation: loader 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+	border-color: currentColor transparent transparent transparent;
+}
+.loader div:nth-child(1) {
+	animation-delay: -0.45s;
+}
+.loader div:nth-child(2) {
+	animation-delay: -0.3s;
+}
+.loader div:nth-child(3) {
+	animation-delay: -0.15s;
+}
+@keyframes loader {
+	0% {
+		transform: rotate(0deg);
+	}
+	100% {
+		transform: rotate(360deg);
+	}
+}
+</style>
