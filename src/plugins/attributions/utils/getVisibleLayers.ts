22c243ef77cd097ffd580f6f1af93dfe24c5ccb9
Pascal Roehling <pas.roehling@gmail.com>
2026-03-05 16:10:08 +0100

    refactor(attributions): code-golf getVisibleLayers
    Co-authored-by: oeninghe-dataport <oeninghe-dataport@users.noreply.github.com>

--- src/plugins/attributions/utils/getVisibleLayers.ts
+++ src/plugins/attributions/utils/getVisibleLayers.ts
@@ -2,17 +2,17 @@ import type { Collection } from 'ol'
 import type BaseLayer from 'ol/layer/Base'
 
 /**
- * looks for all Layers that are currently visible
+ * Looks for all Layers that are currently visible.
+ *
  * @param layers - contains all Layers
- * @returns an array of LayerIDs
+ * @returns an array of LayerIDs.
+ *
+ * @remarks
+ * Only layers added through the services include the id property.
  */
 export function getVisibleLayers(layers: Collection<BaseLayer>) {
-	const layerIDs: string[] = []
-	layers.forEach((layer) => {
-		// Only layers added through the services include the id property
-		if (layer.getVisible() && layer.get('id')) {
-			layerIDs.push(layer.get('id'))
-		}
-	})
-	return layerIDs
+	return layers
+		.getArray()
+		.filter((layer) => layer.getVisible() && layer.get('id'))
+		.map((layer) => layer.get('id'))
 }
