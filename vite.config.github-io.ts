efd605f6d588bce1ff2d81e19f54260442296548
Hendrik Oenings <hendrik.oenings@dataport.de>
2026-03-24 15:38:05 +0100

    build: deploy built github-io to live folder

---
+++ vite.config.github-io.ts
@@ -0,0 +1,35 @@
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import commonJs from 'vite-plugin-commonjs'
+import kernExtraIcons from 'vite-plugin-kern-extra-icons'
+
+import enrichedConsole from './vitePlugins/enrichedConsole.js'
+
+export default defineConfig({
+	root: resolve(__dirname, 'examples', 'github-io'),
+	base: './',
+	plugins: [
+		// @ts-expect-error | commonJs dts is broken
+		commonJs(),
+		vue({
+			template: {
+				compilerOptions: {
+					isCustomElement: (tag) => tag.includes('-'),
+				},
+			},
+		}),
+		kernExtraIcons({
+			cssLayer: 'kern-ux-icons',
+			ignoreFilename: (filename) => !filename.includes('/examples/github-io/'),
+		}),
+		enrichedConsole(),
+	],
+	build: {
+		outDir: '../../.dist.github-io',
+		chunkSizeWarningLimit: 1536,
+	},
+	preview: {
+		port: 1236,
+	},
+})
