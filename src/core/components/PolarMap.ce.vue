65d9e37ebb1978d1a9944a3f9a27ef23e03cbe45
Pascal Roehling <pas.roehling@gmail.com>
2026-03-24 23:24:18 +0100

    fix(core): update listeners if window size changes

--- src/core/components/PolarMap.ce.vue
+++ src/core/components/PolarMap.ce.vue
@@ -152,6 +152,7 @@ function updateListeners() {
 }
 
 watch(hasWindowSize, updateListeners)
+watch(hasSmallDisplay, updateListeners)
 </script>
 
 <!-- eslint-disable-next-line vue/enforce-style-attribute -->
