14a36aa30fabd2b9315ec88789692e8ceed151cb
Zirkelbach, Christian <christian.zirkelbach@dataport.de>
2026-03-24 15:36:44 +0100

    docs(github-io): added vite config to compile vue website app for gh-pages action

---
+++ vite.config.github-io.ts
@@ -0,0 +1,34 @@
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+
+import kernExtraIcons from 'vite-plugin-kern-extra-icons'
+
+export default defineConfig({
+	plugins: [
+		vue({
+			template: {
+				compilerOptions: {
+					isCustomElement: (tag) => tag.includes('-'),
+				},
+			},
+		}),
+		kernExtraIcons({
+			cssLayer: 'kern-ux-icons',
+			ignoreFilename: (filename) =>
+				!filename.includes('/examples/github-io/'),
+		}),
+	],
+	build: {
+		outDir: resolve(__dirname, 'examples', 'github-io', 'dist'),
+		emptyOutDir: true,
+		rollupOptions: {
+			input: resolve(__dirname, 'examples', 'github-io', 'index.html'),
+			output: {
+				entryFileNames: '[name].js',
+				chunkFileNames: '[name].js',
+				assetFileNames: '[name].[ext]',
+			},
+		},
+	},
+})
