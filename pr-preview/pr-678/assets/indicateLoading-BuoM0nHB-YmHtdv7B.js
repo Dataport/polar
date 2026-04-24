bc49fab40f8c8a658862a897042c4e9b46ba8047
dopenguin <dopenguin@users.noreply.github.com>
2026-04-24 10:33:23 +0000

    Deploy preview for PR 678 🛫

---
+++ pr-preview/pr-678/assets/indicateLoading-BuoM0nHB-YmHtdv7B.js
@@ -0,0 +1,1 @@
+import{En as e}from"./stores-BXiuMBcL-DijoXJP6.js";var t=0;function n(){let n=e().getPluginStore(`loadingIndicator`);if(!n)return()=>{};let r=`lib-indicate-loading-${t++}`;return n.addLoadingKey(r),()=>{n.removeLoadingKey(r)}}export{n as t};
