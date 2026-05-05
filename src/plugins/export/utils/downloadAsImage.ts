b820e6894c9ab0d4f71816b7dd7b7492f198116c
Dennis Sen <dennis.sen@dataport.de>
2026-05-05 15:59:12 +0200

    refactor(export): rename img export

--- src/plugins/export/utils/downloadAsImage.ts
+++ src/plugins/export/utils/downloadAsImage.ts
@@ -2,7 +2,7 @@ import { type ExportFormat } from '../types'
 
 export const downloadAsImage = (base64String: string, type: ExportFormat) => {
 	const link = document.createElement('a')
-	link.download = `map.${type}`
+	link.download = `polar-map.${type}`
 	link.href = base64String
 	link.style.display = 'none'
 	document.body.appendChild(link)
