f53023d04ea591026716cee7aabe9df105d0579b
Dennis Sen <dennis.sen@dataport.de>
2026-05-12 14:21:56 +0200

    refactor: scrap new feature

--- src/plugins/zoom/components/ZoomSlider.ce.vue
+++ src/plugins/zoom/components/ZoomSlider.ce.vue
@@ -6,7 +6,6 @@
 		:max="zoomStore.maximumZoomLevel"
 		:step="1"
 		:aria-label="$t(($) => $.slider, { ns: PluginId })"
-		:class="{ vertical: zoomStore.renderType === 'vertical' }"
 	/>
 </template>
 
@@ -20,11 +19,8 @@ const zoomStore = useZoomStore()
 <style scoped>
 input {
 	margin: var(--kern-metric-space-small);
+	margin-left: var(--kern-metric-space-large);
 	width: 12em;
-
-	&.vertical {
-		margin-left: var(--kern-metric-space-large);
-		transform: translateX(-50%) translateY(-50%) rotate(-90deg) translateX(-50%);
-	}
+	transform: translateX(-50%) translateY(-50%) rotate(-90deg) translateX(-50%);
 }
 </style>
