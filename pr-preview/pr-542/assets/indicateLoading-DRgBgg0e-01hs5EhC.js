6aaa22a0f650d797eb8b3deb61c7853aa1e71ed4
warm-coolguy <warm-coolguy@users.noreply.github.com>
2026-05-12 10:32:09 +0000

    Deploy preview for PR 542 🛫

--- pr-preview/pr-542/assets/indicateLoading-DRgBgg0e-01hs5EhC.js
+++
@@ -1,1 +0,0 @@
-import{mt as e}from"./stores-DROJ6de1-Dxvjjnhl.js";var t=0;function n(){let n=e().getPluginStore(`loadingIndicator`);if(!n)return()=>{};let r=`lib-indicate-loading-${t++}`;return n.addLoadingKey(r),()=>{n.removeLoadingKey(r)}}export{n as t};
