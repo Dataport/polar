612c70ee4f623fa8ca3878afd91c3d654c472a6f
dopenguin <73653210+dopenguin@users.noreply.github.com>
2026-05-12 10:50:50 +0000

    Deploying to gh-pages from @ Dataport/polar@e7198634f613838621e9a1d8e513fee01f223788 🚀

---
+++ live/indicateLoading2.js
@@ -0,0 +1,1 @@
+import{t as e}from"./stores.js";var t=0;function n(){let n=e().getPluginStore(`loadingIndicator`);if(!n)return()=>{};let r=`lib-indicate-loading-${t++}`;return n.addLoadingKey(r),()=>{n.removeLoadingKey(r)}}export{n as t};
---
+++ live/reverseGeocodeWps.js
@@ -0,0 +1,1 @@
+import{t as e}from"./reverseGeocodeWps2.js";export{e as reverseGeocodeWps};
