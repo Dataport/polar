1130bad7e8c77d1464dfc1080e1950f63577d726
oeninghe-dataport <oeninghe-dataport@users.noreply.github.com>
2026-05-26 09:16:25 +0000

    Remove preview for PR 742 🛬

--- pr-preview/pr-742/assets/indicateLoading-BhzP3i-y-BcYf7TB8.js
+++
@@ -1,1 +0,0 @@
-import{en as e}from"./stores-BfwxYCaF-BABq9bJ-.js";var t=0;function n(){let n=e().getPluginStore(`loadingIndicator`);if(!n)return()=>{};let r=`lib-indicate-loading-${t++}`;return n.addLoadingKey(r),()=>{n.removeLoadingKey(r)}}export{n as t};
