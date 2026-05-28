c5b1994a570f43b7291a5d3d9da0af6dbec36dc5
Pascal Roehling <pas.roehling@gmail.com>
2026-05-28 20:03:56 +0200

    refactor: remove redundant dom elements
    By using "role" the semantic list elements are no longer needed.

--- src/core/components/ContextMenu.ce.vue
+++ src/core/components/ContextMenu.ce.vue
@@ -1,26 +1,21 @@
 <template>
 	<PolarCard
 		:style="`left: ${left}; top: ${top}`"
+		role="menu"
 		@pointerdown.stop
 		@keydown.escape="close"
 	>
 		<section class="kern-card__body">
-			<ul role="menu">
-				<li
-					v-for="{ id, icon, text, callback } in buttons.values()"
-					:key="id"
-					role="none"
-				>
-					<KernButton
-						class="kern-btn--block kern-btn--tertiary"
-						:icon="icon"
-						role="menuitem"
-						@click="ring(callback)"
-					>
-						{{ text }}
-					</KernButton>
-				</li>
-			</ul>
+			<KernButton
+				v-for="{ id, icon, text, callback } in buttons.values()"
+				:key="id"
+				class="kern-btn--block kern-btn--tertiary"
+				:icon="icon"
+				role="menuitem"
+				@click="ring(callback)"
+			>
+				{{ text }}
+			</KernButton>
 		</section>
 	</PolarCard>
 </template>
@@ -68,15 +63,12 @@ function close() {
 		padding: 0;
 	}
 
-	button {
-		justify-content: start;
-	}
+	.kern-card__body {
+		gap: 0;
 
-	ul {
-		list-style: none;
-		margin: 0;
-		padding: 0;
-		width: 100%;
+		button {
+			justify-content: start;
+		}
 	}
 }
 </style>
