31b092a72673a6c10e5ca288cf5f130b0b5d115e
Pascal Roehling <pas.roehling@gmail.com>
2026-06-01 19:26:42 +0200

    feat: add description for context menu configuration

--- src/core/types/contextMenu.ts
+++ src/core/types/contextMenu.ts
@@ -2,11 +2,29 @@ import type { Coordinate } from 'ol/coordinate'
 
 import type { Color, Icon } from './theme'
 
-// TODO(dopenguin): Add proper description
 export interface ContextMenuEntry {
+	/**
+	 * Function to be called on click.
+	 */
 	callback: (coordinate: Coordinate) => void
+
+	/**
+	 * Icon to be displayed for the menu entry.
+	 */
 	icon: Icon
+
+	/**
+	 * Unique identifier. Relevant to be able to remove an entry.
+	 */
 	id: string
+
+	/**
+	 * Text to be displayed for the menu entry.
+	 */
 	text: string
+
+	/**
+	 * Override for the color of the {@link ContextMenuEntry.icon | icon} and {@link ContextMenuEntry.text | text}.
+	 */
 	color?: Color
 }
