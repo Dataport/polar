7de947c5461fb9202cb1d93d79e36073d130e868
dopenguin <dopenguin@users.noreply.github.com>
2026-06-08 10:45:55 +0000

    Remove preview for PR 775 🛬

--- pr-preview/pr-775/assets/indicateLoading-DF5zKWdh-CSdC9q_w.js
+++
@@ -1,1 +0,0 @@
-import{Gt as e}from"./stores-ZOL5Hh9n-BRrx4fUw.js";var t=0;function n(){let n=e().getPluginStore(`loadingIndicator`);if(!n)return()=>{};let r=`lib-indicate-loading-${t++}`;return n.addLoadingKey(r),()=>{n.removeLoadingKey(r)}}export{n as t};
