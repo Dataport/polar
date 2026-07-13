ad673714b51e9f467eea93093a252dc55bcb9f47
Hendrik Oenings <hendrik.oenings@dataport.de>
2026-07-13 08:42:48 +0200

    refactor(reverseGeocoder): type nominatim response

--- src/plugins/reverseGeocoder/utils/reverseGeocodeNominatim.ts
+++ src/plugins/reverseGeocoder/utils/reverseGeocodeNominatim.ts
@@ -2,6 +2,42 @@ import type { ReverseGeocoderFeature } from '../types'
 
 import { transform as transformCoordinate } from 'ol/proj'
 
+interface NominatimReverseGeocodeResponse {
+	address: {
+		house_number?: string
+		road?: string
+		hamlet?: string
+		village?: string
+		town?: string
+		suburb?: string
+		city_district?: string
+		city?: string
+		county?: string
+		state_district?: string
+		state?: string
+		// eslint-disable-next-line @typescript-eslint/naming-convention
+		'ISO3166-2-lvl4'?: string
+		postcode?: string
+		country?: string
+		country_code?: string
+	}
+	boundingbox: [string, string, string, string]
+	category: string
+	display_name: string
+	importance: number
+	lat: string
+	licence: string
+	lon: string
+	name: string
+	osm_id: string
+	osm_type: string
+	place_id: number
+	type: string
+	extratags?: Record<string, unknown>
+	icon?: string
+	place_rank?: number
+}
+
 export async function reverseGeocodeNominatim({
 	url,
 	coordinate,
@@ -24,8 +60,8 @@ export async function reverseGeocodeNominatim({
 	fetchUrl.searchParams.set('lon', searchCoordinate[0].toString())
 	fetchUrl.searchParams.set('format', 'jsonv2')
 
-	const result = await fetch(fetchUrl, { signal }).then((response) =>
-		response.json()
+	const result = await fetch(fetchUrl, { signal }).then(
+		(response) => <Promise<NominatimReverseGeocodeResponse>>response.json()
 	)
 
 	return {
@@ -38,7 +74,7 @@ export async function reverseGeocodeNominatim({
 		]
 			.filter((x) => x)
 			.join(', '),
-		properties: result.properties,
+		properties: {},
 		geometry: {
 			// as clicked by user - usually want to keep this since user is pointing at something
 			coordinates: coordinate,
@@ -47,7 +83,7 @@ export async function reverseGeocodeNominatim({
 		addressGeometry: {
 			// as returned by reverse geocoder
 			coordinates: transformCoordinate(
-				[result.lon, result.lat],
+				[Number(result.lon), Number(result.lat)],
 				'EPSG:4326',
 				epsg
 			),
