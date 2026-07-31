af11964e40fee4370227b42d3fa8368880864c54
JoselinGuevaraHoppe <joselin.guevarahoppe@dataport.de>
2026-07-31 10:48:35 +0200

    fix(initialView): move comment to variable in types.ts

--- src/plugins/initialView/types.ts
+++ src/plugins/initialView/types.ts
@@ -1,14 +1,13 @@
-/**
- * Defines if the geoLocation button is rendered independent or as part of the
- * icon menu.
- * @defaultValue `'independent'`
- */
-
 import type { PluginOptions } from '@/core'
 
 export const PluginId = 'initialView' as const
 
 export interface InitialViewPluginOptions extends PluginOptions {
+	/**
+	 * Defines if the initialView button is rendered independent or as part of the
+	 * icon menu.
+	 * @defaultValue `'independent'`
+	 */
 	renderType?: 'independent' | 'iconMenu'
 	startCenter?: [number, number]
 	startResolution?: number
