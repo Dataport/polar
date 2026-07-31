949c299fb626f93077e2e87e214bd6c2eeb41438
JoselinGuevaraHoppe <joselin.guevarahoppe@dataport.de>
2026-07-31 11:24:42 +0200

    fix(initialView): delete unused value
     Co-authored-by: LivMoeser <liv.moeser@dataport.de>

--- src/plugins/initialView/locales.ts
+++ src/plugins/initialView/locales.ts
@@ -7,14 +7,12 @@
 import type { Locale } from '@/core'
 
 export const resourcesDe = {
-	title: 'Zur Startansicht',
 	label: {
 		return: 'Zurück zur Startansicht',
 	},
 } as const
 
 export const resourcesEn = {
-	title: 'Return to Start View',
 	label: {
 		return: 'Return to start view',
 	},
