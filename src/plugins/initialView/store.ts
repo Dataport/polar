e67cbc41a315aa8139834626a64f51e410043d9f
JoselinGuevaraHoppe <joselin.guevarahoppe@dataport.de>
2026-08-03 07:35:00 +0200

    fix(initialView): comment @aplha in store
    Co-authored-by: LivMoeser <liv.moeser@dataport.de>

--- src/plugins/initialView/store.ts
+++ src/plugins/initialView/store.ts
@@ -24,6 +24,9 @@ export const useInitialViewStore = defineStore('plugins/initialView', () => {
 	return {
 		startCenter,
 		startResolution,
+
+		/** @alpha  */
+
 		returnToInitialView,
 		setupPlugin,
 		teardownPlugin,
