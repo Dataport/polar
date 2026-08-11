4c024b7d6553e0600632bf8f02bfce7989ed9c4f
Melanie Michaela Klamp <MelanieMichaela.klamp@dataport.de>
2026-08-11 09:11:06 +0200

    refactor: use the svgConfig in the makeMultiMarker-function

--- src/core/utils/markers.ts
+++ src/core/utils/markers.ts
@@ -39,24 +39,23 @@ const makeMarker = ({ fill, size, stroke, strokeWidth }: MarkerStyle) =>
 </svg>
 `)}`
 
-const makeMultiMarker = ({
-	fill,
-	clusterSize,
-	stroke,
-	strokeWidth,
-}: MarkerStyle) =>
+const makeMultiMarker = (
+	{ fill, clusterSize, stroke, strokeWidth, displayedText }: MarkerStyle,
+	displayFeatureCount: boolean,
+	svgConfig: MarkerSVGConfig
+) =>
 	`${prefix}${encodeURIComponent(`
 <svg width="${clusterSize[0]}" height="${
 		clusterSize[1]
-	}" viewBox="0 0 30 43" xmlns="http://www.w3.org/2000/svg">
+	}" viewBox=${svgConfig.viewBox} xmlns="http://www.w3.org/2000/svg">
   <title>0A6F4952-4A5A-4E86-88E4-4B3D2EA1E3DF</title>
   ${getImagePattern(fill)}
   <g stroke="${stroke}" stroke-width="${strokeWidth}" fill="${
 		typeof fill === 'string' ? fill : 'url(#img)'
 	}" fill-rule="nonzero">
-    <path d="M22.584 1C15.101 1 9 7.101 9 14.584c0 8.103 7.865 20.448 11.63 25.93a2.36 2.36 0 0 0 3.908 0c3.766-5.482 11.63-17.922 11.63-25.93C36.168 7.054 30.068 1 22.584 1z"/>
-    <path d="M18.584 1C11.101 1 5 7.101 5 14.584c0 8.103 7.865 20.448 11.63 25.93a2.36 2.36 0 0 0 3.908 0c3.766-5.482 11.63-17.922 11.63-25.93C32.168 7.054 26.068 1 18.584 1z"/>
-    <path d="M14.584 1C7.101 1 1 7.101 1 14.584c0 8.103 7.865 20.448 11.63 25.93a2.36 2.36 0 0 0 3.908 0c3.766-5.482 11.63-17.922 11.63-25.93C28.168 7.054 22.068 1 14.584 1z"/>
+	<path ${svgConfig.path}/>
+	<path ${svgConfig.stackedPath1}/>
+	<path ${svgConfig.stackedPath2}/>
   </g>
 </svg>
 `)}`
