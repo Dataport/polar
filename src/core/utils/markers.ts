fd31cf0ba37d2e5fb41367af1defb4a8dff201e7
Melanie Michaela Klamp <MelanieMichaela.klamp@dataport.de>
2026-08-14 07:57:51 +0200

    refactor: adjust skale

--- src/core/utils/markers.ts
+++ src/core/utils/markers.ts
@@ -24,9 +24,7 @@ const getImagePattern = (fill: MarkerStyle['fill']) =>
 
 const makeMarker = ({ fill, size, stroke }: MarkerStyle) =>
 	`${prefix}${encodeURIComponent(`
-<svg fill="none" width="${size[0]}" height="${
-		size[1]
-	}" viewBox="${circlePin.viewBox}" xmlns="http://www.w3.org/2000/svg">
+<svg fill="none" width="${size[0] * 1.8}" height="${size[1] * 1.8}" viewBox="${circlePin.viewBox}" xmlns="http://www.w3.org/2000/svg">
   <title>DB6C494E-88E8-49F1-89CE-97CBEC3A5240</title>
   ${getImagePattern(fill)}
   <g class="feature-pin-default">
@@ -47,7 +45,7 @@ const makeMultiMarker = (
 	svgConfig: MarkerSVGConfig
 ) =>
 	`${prefix}${encodeURIComponent(`
-<svg width="${clusterSize[0]}" height="${clusterSize[1]}"
+<svg width="${clusterSize[0] * 1.5}" height="${clusterSize[1] * 1.5}"
 	viewBox="${svgConfig.viewBox}" xmlns="http://www.w3.org/2000/svg">
   <title>0A6F4952-4A5A-4E86-88E4-4B3D2EA1E3DF</title>
   ${getImagePattern(fill)}
