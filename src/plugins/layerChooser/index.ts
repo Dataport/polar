4bf0599289d2178522f3750aa28bd0905f9ac9ec
Pascal Roehling <pas.roehling@gmail.com>
2026-08-26 15:05:15 +0200

    refactor: remove unused options object

--- src/plugins/layerChooser/index.ts
+++ src/plugins/layerChooser/index.ts
@@ -4,7 +4,7 @@
  */
 /* eslint-enable tsdoc/syntax */
 
-import type { PluginContainer, PluginOptions, PolarPluginStore } from '@/core'
+import type { PluginContainer, PolarPluginStore } from '@/core'
 
 import component from './components/LayerChooser.ce.vue'
 import locales from './locales'
@@ -24,16 +24,13 @@ import { PluginId } from './types'
  *
  * @returns Plugin for use with {@link addPlugin}.
  */
-export default function pluginLayerChooser(
-	options: PluginOptions
-): PluginContainer {
+export default function pluginLayerChooser(): PluginContainer {
 	return {
 		id: PluginId,
 		component,
 		icon: 'kern-icon-fill--layers',
 		locales,
 		storeModule: useLayerChooserStore as PolarPluginStore,
-		options,
 	}
 }
 
